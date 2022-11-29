a7ccc654f963f9c7e7b24f75ef02f383cfaa419b
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2022-11-29 18:40:41 +0530

    refactor

--- src/pet/mapping.py
+++ src/pet/mapping.py
@@ -3,7 +3,7 @@ from .tuners import PrefixTuningConfig, PromptEncoderConfig, PromptTuningConfig
 from .utils import PETConfig
 
 
-MODEL_TYPE_TO_PROMPT_MODEL_MAPPING = {
+MODEL_TYPE_TO_PET_MODEL_MAPPING = {
     "SEQ_CLS": PETModelForSequenceClassification,
     "SEQ_2_SEQ_LM": PETModelForSeq2SeqLM,
     "CAUSAL_LM": PETModelForCausalLM,
@@ -15,3 +15,11 @@ PET_TYPE_TO_CONFIG_MAPPING = {
     "P_TUNING": PromptEncoderConfig,
     "LORA": PETConfig,
 }
+
+
+def get_pet_config(config_dict):
+    return PET_TYPE_TO_CONFIG_MAPPING[config_dict["pet_type"]](**config_dict)
+
+
+def get_pet_model(model, pet_config):
+    return MODEL_TYPE_TO_PET_MODEL_MAPPING[pet_config.task_type](model, pet_config)
