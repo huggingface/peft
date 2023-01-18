246b2cffbad5d061095b1822696da1bcf3a598d2
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2023-01-19 03:23:57 +0530

    fixing doc strings to follow hf doc-builder format

--- src/peft/utils/config.py
+++ src/peft/utils/config.py
@@ -38,8 +38,8 @@ class PeftConfig:
     This is the base configuration class to store the configuration of a :class:`~peft.PeftModel`.
 
     Args:
-        peft_type (Union[:class:`~peft.utils.config.PeftType`, `str`]): The type of Peft method to use.
-        task_type (Union[:class:`~peft.utils.config.TaskType`, `str`]): The type of task to perform.
+        peft_type (Union[[`~peft.utils.config.PeftType`], `str`]): The type of Peft method to use.
+        task_type (Union[[`~peft.utils.config.TaskType`], `str`]): The type of task to perform.
         inference_mode (`bool`, defaults to `False`): Whether to use the Peft model in inference mode.
     """
 
@@ -51,8 +51,8 @@ class PeftConfig:
 @dataclass
 class PromptLearningConfig(PeftConfig):
     """
-    This is the base configuration class to store the configuration of a Union[:class:`~peft.PrefixTuning`,
-    :class:`~peft.PromptEncoder`, :class:`~peft.PromptTuning`].
+    This is the base configuration class to store the configuration of a Union[[`~peft.PrefixTuning`],
+    [`~peft.PromptEncoder`], [`~peft.PromptTuning`]].
 
     Args:
         num_virtual_tokens (`int`): The number of virtual tokens to use.
