8d77d1df44c46c2fe3c1c801b543e2662ad261bf
younesbelkada <younesbelkada@gmail.com>
2023-01-25 22:43:22 +0000

    v1 working
    - from_pretrained support for config
    - from_pretrained support for loramodel
    - todo: tests
    - todo: push_to_hub

--- src/peft/utils/adapters_utils.py
+++ src/peft/utils/adapters_utils.py
@@ -1,7 +1,3 @@
-# flake8: noqa
-# There's no way to ignore "F401 '...' imported but unused" warnings in this
-# module, but to preserve other warnings. So, don't check this module at all
-
 # coding=utf-8
 # Copyright 2023-present the HuggingFace Inc. team.
 #
@@ -17,7 +13,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .config import PeftConfig, PeftType, PromptLearningConfig, TaskType
-from .other import _set_trainable, bloom_model_postprocess_past_key_value, shift_tokens_right, transpose
-from .save_and_load import get_peft_model_state_dict, peft_model_load_and_dispatch, set_peft_model_state_dict
-from .adapters_utils import WEIGHTS_NAME, CONFIG_NAME
+WEIGHTS_NAME = "adapter_model.bin"
+CONFIG_NAME = "adapter_config.json"
