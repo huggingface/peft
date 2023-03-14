ae7c42f0d678b0d84e591d7954869cc0e5bd08e5
younesbelakda <younesbelkada@gmail.com>
2023-03-14 11:13:14 +0000

    style

--- src/peft/tuners/lora.py
+++ src/peft/tuners/lora.py
@@ -344,10 +344,6 @@ class Linear(nn.Linear, LoraLayer):
         self.lora_B.eval()
 
     def forward(self, x: torch.Tensor):
-        # previous_device = x.device
-
-        # if previous_device != self.weight.device:
-        #     x = x.to(self.weight.device)
 
         if self.disable_adapters:
             if self.r > 0 and self.merged:
@@ -356,15 +352,15 @@ class Linear(nn.Linear, LoraLayer):
                 )
                 self.merged = False
 
-            return F.linear(x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias) # .to(previous_device)
+            return F.linear(x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias)
         elif self.r > 0 and not self.merged:
             result = F.linear(x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias)
             if self.r > 0:
                 result += self.lora_B(self.lora_A(self.lora_dropout(x))) * self.scaling
-            result = result # .to(previous_device)
+            result = result
             return result
         else:
-            return F.linear(x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias) # .to(previous_device)
+            return F.linear(x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias)
 
 
 class MergedLinear(nn.Linear, LoraLayer):
