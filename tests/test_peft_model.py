c9174ba1827231a4df50e03d73195bb40bdb18df
Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
2023-03-23 12:38:40 +0100

    [`CI`] Add ci tests (#203)
    * add ci tests
    
    * fix some tests
    
    * fix tests
    
    * rename
    
    * fix
    
    * update tests
    
    * try
    
    * temp hotfix
    
    * refactor tests
    
    * Update .github/workflows/tests.yml
    
    * fix test

--- tests/test_peft_model.py
+++ tests/test_peft_model.py
@@ -17,157 +17,140 @@ import tempfile
 import unittest
 
 import torch
+from parameterized import parameterized
 from transformers import AutoModelForCausalLM
 
 from peft import (
-    LoraConfig,
     PeftModel,
-    PrefixTuningConfig,
-    PromptEncoderConfig,
-    PromptTuningConfig,
     get_peft_model,
     get_peft_model_state_dict,
-    prepare_model_for_training,
+    prepare_model_for_int8_training,
 )
+
+from .testing_common import PeftTestConfigManager
+
+
+# This has to be in the order: model_id, lora_kwargs, prefix_tuning_kwargs, prompt_encoder_kwargs, prompt_tuning_kwargs
+PEFT_MODELS_TO_TEST = [
+    ("hf-internal-testing/tiny-random-OPTForCausalLM", {"target_modules": ["q_proj", "v_proj"]}, {}, {}, {}),
+]
 
 
 class PeftTestMixin:
-    checkpoints_to_test = [
-        "hf-internal-testing/tiny-random-OPTForCausalLM",
-    ]
-    config_classes = (
-        LoraConfig,
-        PrefixTuningConfig,
-        PromptEncoderConfig,
-        PromptTuningConfig,
-    )
-    config_kwargs = (
-        {
-            "r": 8,
-            "lora_alpha": 32,
-            "target_modules": ["q_proj", "v_proj"],
-            "lora_dropout": 0.05,
-            "bias": "none",
-            "task_type": "CAUSAL_LM",
-        },
-        {
-            "num_virtual_tokens": 10,
-            "task_type": "CAUSAL_LM",
-        },
-        {
-            "num_virtual_tokens": 10,
-            "encoder_hidden_size": 32,
-            "task_type": "CAUSAL_LM",
-        },
-        {
-            "num_virtual_tokens": 10,
-            "task_type": "CAUSAL_LM",
-        },
-    )
+    torch_device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 class PeftModelTester(unittest.TestCase, PeftTestMixin):
     r"""
     Test if the PeftModel behaves as expected. This includes:
     - test if the model has the expected methods
+
+    We use parametrized.expand for debugging purposes to test each model individually.
     """
 
-    def test_attributes_model(self):
-        for model_id in self.checkpoints_to_test:
-            for i, config_cls in enumerate(self.config_classes):
-                model = AutoModelForCausalLM.from_pretrained(model_id)
-                config = config_cls(
-                    base_model_name_or_path=model_id,
-                    **self.config_kwargs[i],
+    @parameterized.expand(PeftTestConfigManager.get_grid_parameters(PEFT_MODELS_TO_TEST))
+    def test_attributes_parametrized(self, test_name, model_id, config_cls, config_kwargs):
+        self._test_model_attr(model_id, config_cls, config_kwargs)
+
+    def _test_model_attr(self, model_id, config_cls, config_kwargs):
+        model = AutoModelForCausalLM.from_pretrained(model_id)
+        config = config_cls(
+            base_model_name_or_path=model_id,
+            **config_kwargs,
+        )
+        model = get_peft_model(model, config)
+
+        self.assertTrue(hasattr(model, "save_pretrained"))
+        self.assertTrue(hasattr(model, "from_pretrained"))
+        self.assertTrue(hasattr(model, "push_to_hub"))
+
+    def _test_prepare_for_training(self, model_id, config_cls, config_kwargs):
+        model = AutoModelForCausalLM.from_pretrained(model_id).to(self.torch_device)
+        config = config_cls(
+            base_model_name_or_path=model_id,
+            **config_kwargs,
+        )
+        model = get_peft_model(model, config)
+
+        dummy_input = torch.LongTensor([[1, 1, 1]]).to(self.torch_device)
+        dummy_output = model.get_input_embeddings()(dummy_input)
+
+        self.assertTrue(not dummy_output.requires_grad)
+
+        # load with `prepare_model_for_int8_training`
+        model = AutoModelForCausalLM.from_pretrained(model_id).to(self.torch_device)
+        model = prepare_model_for_int8_training(model)
+
+        for param in model.parameters():
+            self.assertTrue(not param.requires_grad)
+
+        config = config_cls(
+            base_model_name_or_path=model_id,
+            **config_kwargs,
+        )
+        model = get_peft_model(model, config)
+
+        # For backward compatibility
+        if hasattr(model, "enable_input_require_grads"):
+            model.enable_input_require_grads()
+        else:
+
+            def make_inputs_require_grad(module, input, output):
+                output.requires_grad_(True)
+
+            model.get_input_embeddings().register_forward_hook(make_inputs_require_grad)
+
+        dummy_input = torch.LongTensor([[1, 1, 1]]).to(self.torch_device)
+        dummy_output = model.get_input_embeddings()(dummy_input)
+
+        self.assertTrue(dummy_output.requires_grad)
+
+    @parameterized.expand(PeftTestConfigManager.get_grid_parameters(PEFT_MODELS_TO_TEST))
+    def test_prepare_for_training_parametrized(self, test_name, model_id, config_cls, config_kwargs):
+        self._test_prepare_for_training(model_id, config_cls, config_kwargs)
+
+    def _test_save_pretrained(self, model_id, config_cls, config_kwargs):
+        model = AutoModelForCausalLM.from_pretrained(model_id)
+        config = config_cls(
+            base_model_name_or_path=model_id,
+            **config_kwargs,
+        )
+        model = get_peft_model(model, config)
+        model = model.to(self.torch_device)
+
+        with tempfile.TemporaryDirectory() as tmp_dirname:
+            model.save_pretrained(tmp_dirname)
+
+            model_from_pretrained = AutoModelForCausalLM.from_pretrained(model_id)
+            model_from_pretrained = PeftModel.from_pretrained(model_from_pretrained, tmp_dirname)
+
+            # check if the state dicts are equal
+            state_dict = get_peft_model_state_dict(model)
+            state_dict_from_pretrained = get_peft_model_state_dict(model_from_pretrained)
+
+            # check if same keys
+            self.assertEqual(state_dict.keys(), state_dict_from_pretrained.keys())
+
+            # check if tensors equal
+            for key in state_dict.keys():
+                self.assertTrue(
+                    torch.allclose(
+                        state_dict[key].to(self.torch_device), state_dict_from_pretrained[key].to(self.torch_device)
+                    )
                 )
-                model = get_peft_model(model, config)
 
-                self.assertTrue(hasattr(model, "save_pretrained"))
-                self.assertTrue(hasattr(model, "from_pretrained"))
-                self.assertTrue(hasattr(model, "push_to_hub"))
+            # check if `adapter_model.bin` is present
+            self.assertTrue(os.path.exists(os.path.join(tmp_dirname, "adapter_model.bin")))
 
-    def test_prepare_for_training(self):
-        r"""
-        A test that checks if `prepare_for_training` behaves as expected
-        """
-        for model_id in self.checkpoints_to_test:
-            for i, config_cls in enumerate(self.config_classes):
-                model = AutoModelForCausalLM.from_pretrained(model_id)
-                config = config_cls(
-                    base_model_name_or_path=model_id,
-                    **self.config_kwargs[i],
-                )
-                model = get_peft_model(model, config)
+            # check if `adapter_config.json` is present
+            self.assertTrue(os.path.exists(os.path.join(tmp_dirname, "adapter_config.json")))
 
-                dummy_input = torch.LongTensor([[1, 1, 1]])
-                dummy_output = model.get_input_embeddings()(dummy_input)
+            # check if `pytorch_model.bin` is not present
+            self.assertFalse(os.path.exists(os.path.join(tmp_dirname, "pytorch_model.bin")))
 
-                self.assertTrue(not dummy_output.requires_grad)
+            # check if `config.json` is not present
+            self.assertFalse(os.path.exists(os.path.join(tmp_dirname, "config.json")))
 
-                # load with `prepare_model_for_training`
-                model = AutoModelForCausalLM.from_pretrained(model_id)
-                model = prepare_model_for_training(model)
-
-                for param in model.parameters():
-                    self.assertTrue(not param.requires_grad)
-
-                config = config_cls(
-                    base_model_name_or_path=model_id,
-                    **self.config_kwargs[i],
-                )
-                model = get_peft_model(model, config)
-
-                dummy_input = torch.LongTensor([[1, 1, 1]])
-                dummy_output = model.get_input_embeddings()(dummy_input)
-
-                self.assertTrue(dummy_output.requires_grad)
-
-    def test_save_pretrained(self):
-        r"""
-        A test to check if `save_pretrained` behaves as expected. This function should only save the state dict of the
-        adapter model and not the state dict of the base model. Hence inside each saved directory you should have:
-
-        - README.md (that contains an entry `base_model`)
-        - adapter_config.json
-        - adapter_model.bin
-
-        """
-        for model_id in self.checkpoints_to_test:
-            for i, config_cls in enumerate(self.config_classes):
-                model = AutoModelForCausalLM.from_pretrained(model_id)
-                config = config_cls(
-                    base_model_name_or_path=model_id,
-                    **self.config_kwargs[i],
-                )
-                model = get_peft_model(model, config)
-                model.to(model.device)
-
-                with tempfile.TemporaryDirectory() as tmp_dirname:
-                    model.save_pretrained(tmp_dirname)
-
-                    model_from_pretrained = AutoModelForCausalLM.from_pretrained(model_id)
-                    model_from_pretrained = PeftModel.from_pretrained(model_from_pretrained, tmp_dirname)
-                    model_from_pretrained.to(model.device)
-
-                    # check if the state dicts are equal
-                    state_dict = get_peft_model_state_dict(model)
-                    state_dict_from_pretrained = get_peft_model_state_dict(model_from_pretrained)
-
-                    # check if same keys
-                    self.assertEqual(state_dict.keys(), state_dict_from_pretrained.keys())
-
-                    # check if tensors equal
-                    for key in state_dict.keys():
-                        self.assertTrue(torch.allclose(state_dict[key], state_dict_from_pretrained[key]))
-
-                    # check if `adapter_model.bin` is present
-                    self.assertTrue(os.path.exists(os.path.join(tmp_dirname, "adapter_model.bin")))
-
-                    # check if `adapter_config.json` is present
-                    self.assertTrue(os.path.exists(os.path.join(tmp_dirname, "adapter_config.json")))
-
-                    # check if `pytorch_model.bin` is not present
-                    self.assertFalse(os.path.exists(os.path.join(tmp_dirname, "pytorch_model.bin")))
-
-                    # check if `config.json` is not present
-                    self.assertFalse(os.path.exists(os.path.join(tmp_dirname, "config.json")))
+    @parameterized.expand(PeftTestConfigManager.get_grid_parameters(PEFT_MODELS_TO_TEST))
+    def test_save_pretrained(self, test_name, model_id, config_cls, config_kwargs):
+        self._test_save_pretrained(model_id, config_cls, config_kwargs)
