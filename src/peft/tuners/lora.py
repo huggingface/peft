fix half precision forward

src/peft/tuners/lora.py
@@ -394,21 +394,26 @@ class Linear(nn.Linear, LoraLayer):
         self.lora_B.eval()
 
     def forward(self, x: torch.Tensor):
+        previous_dtype = self.weight.dtype
+
         if self.disable_adapters:
             if self.r > 0 and self.merged:
-                self.weight.data -= (
-                    transpose(self.lora_B.weight @ self.lora_A.weight, self.fan_in_fan_out) * self.scaling
-                )
+                matmul_output = self.lora_B.weight @ self.lora_A.weight
+                self.weight.data -= transpose(matmul_output.to(previous_dtype), self.fan_in_fan_out) * self.scaling
                 self.merged = False
 
-            return F.linear(x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias)
+            result = F.linear(x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias)
         elif self.r > 0 and not self.merged:
             result = F.linear(x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias)
             if self.r > 0:
-                result += self.lora_B(self.lora_A(self.lora_dropout(x))) * self.scaling
-            return result
+                result += self.lora_B(self.lora_A(self.lora_dropout(x.to(self.lora_A.weight.dtype)))) * self.scaling
         else:
-            return F.linear(x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias)
+            result = F.linear(x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias)
+
+        if result.dtype != previous_dtype:
+            result = result.to(previous_dtype)
+
+        return result
 
 
 class MergedLinear(nn.Linear, LoraLayer):
@@ -508,6 +513,8 @@ class MergedLinear(nn.Linear, LoraLayer):
         self.lora_B.eval()
 
     def forward(self, x: torch.Tensor):
+        previous_dtype = x.dtype
+
         if self.disable_adapters:
             if self.r > 0 and self.merged and any(self.enable_lora):
                 delta_w = (
@@ -519,18 +526,24 @@ class MergedLinear(nn.Linear, LoraLayer):
                     .squeeze(0)
                     .transpose(-2, -1)
                 )
+
+                delta_w = delta_w.to(self.weight.dtype)
+
                 self.weight.data -= transpose(self.zero_pad(delta_w * self.scaling), not self.fan_in_fan_out)
                 self.merged = False
-            return F.linear(x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias)
+            result = F.linear(x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias)
         elif self.merged:
-            return F.linear(x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias)
+            result = F.linear(x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias)
         else:
             result = F.linear(x, transpose(self.weight, self.fan_in_fan_out), bias=self.bias)
             if self.r > 0:
-                after_A = self.lora_A(self.lora_dropout(x))
+                after_A = self.lora_A(self.lora_dropout(x.to(self.lora_A.weight.dtype)))
                 after_B = self.lora_B(after_A.transpose(-2, -1)).transpose(-2, -1)
                 result += self.zero_pad(after_B) * self.scaling
-            return result
+
+        result = result.to(previous_dtype)
+
+        return result
 
 
 if is_bnb_available():