b7030bfd04e971026cbece9d6e7c00d6e4208300
0x000011b <0x000011b@proton.me>
2023-05-11 16:27:10 +0000

    fix merge_and_unload when LoRA targets embedding layer

--- src/peft/tuners/lora.py
+++ src/peft/tuners/lora.py
@@ -345,8 +345,11 @@ class LoraModel(torch.nn.Module):
             except AttributeError:
                 continue
             if isinstance(target, LoraLayer):
-                bias = target.bias is not None
-                new_module = torch.nn.Linear(target.in_features, target.out_features, bias=bias)
+                if isinstance(target, nn.Embedding):
+                    new_module = torch.nn.Embedding(target.in_features, target.out_features)
+                else:
+                    bias = target.bias is not None
+                    new_module = torch.nn.Linear(target.in_features, target.out_features, bias=bias)
                 target.merge()
                 self._replace_module(parent, target_name, new_module, target)
 
