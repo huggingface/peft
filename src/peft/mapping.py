06f672467f3170f3d063a8cba53b65566268f0be
Thomas Schillaci <thomas.schillaci@gmail.com>
2023-06-01 11:44:06 +0200

    Fix a minor typo where a non-default token_dim would crash prompt tuning (#459)

--- src/peft/mapping.py
+++ src/peft/mapping.py
@@ -96,7 +96,7 @@ def _prepare_prompt_learning_config(peft_config, model_config):
         peft_config.num_attention_heads = num_attention_heads
 
     if getattr(peft_config, "encoder_hidden_size", None) is None:
-        setattr(peft_config, "encoder_hidden_size", token_dim)
+        setattr(peft_config, "encoder_hidden_size", peft_config.token_dim)
 
     return peft_config
 
