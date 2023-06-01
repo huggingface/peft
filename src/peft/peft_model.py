af2c37950f2fb185d6aebdab5f9fa1cec71af7f8
Thomas SCHILLACI <tschilla@px101.prod.exalead.com>
2023-06-01 11:27:14 +0200

    Style formatting

--- src/peft/peft_model.py
+++ src/peft/peft_model.py
@@ -998,26 +998,26 @@ class PeftModelForSeq2SeqLM(PeftModel):
                 elif peft_config.peft_type == PeftType.PROMPT_TUNING:
                     kwargs = deepcopy(kwargs)
 
-                    if 'encoder_outputs' in kwargs:
-                        del kwargs['encoder_ouputs']
+                    if "encoder_outputs" in kwargs:
+                        del kwargs["encoder_ouputs"]
                         warnings.warn(
                             "`encoder_outputs` should not be passed to `generate` when using prompt tuning. Ignoring it."
                         )
 
-                    input_ids = kwargs['input_ids']
+                    input_ids = kwargs["input_ids"]
                     inputs_embeds = self.word_embeddings(input_ids)
-                    del kwargs['input_ids']
-                    batch_size  = inputs_embeds.shape[0]
+                    del kwargs["input_ids"]
+                    batch_size = inputs_embeds.shape[0]
                     prompts = self.get_prompt(batch_size=batch_size)
                     prompts = prompts.to(inputs_embeds.dtype)
 
                     inputs_embeds = torch.cat((prompts[:, : peft_config.num_virtual_tokens], inputs_embeds), dim=1)
                     encoder_outputs = self.base_model.get_encoder()(inputs_embeds=inputs_embeds)
-                    kwargs['encoder_outputs'] = encoder_outputs
+                    kwargs["encoder_outputs"] = encoder_outputs
 
-                    if 'attention_mask' in kwargs:
+                    if "attention_mask" in kwargs:
                         prefix_attention_mask = torch.ones(batch_size, peft_config.num_virtual_tokens).to(
-                            kwargs['attention_mask'].device
+                            kwargs["attention_mask"].device
                         )
                         kwargs["attention_mask"] = torch.cat((prefix_attention_mask, kwargs["attention_mask"]), dim=1)
 
