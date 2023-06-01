[`Llama-Adapter`] fix half precision inference + add tests (#456)
* fix + add tests

* forward contrib credits from discussions

---------

Co-authored-by: HamidShojanazeri <HamidShojanazeri@users.noreply.github.com>

src/peft/tuners/adaption_prompt.py
@@ -294,10 +294,12 @@ class AdaptedAttention(nn.Module):
         # https://github.com/ZrrSkywalker/LLaMA-Adapter/blob/41c3546fe1997ab8a65809dc8d8f9252b19d9faf/llama/model.py#L234
         # (bsz, adapter_len, hidden_size)
         self.adaption_prompt = nn.Parameter(
-            torch.empty(1, adapter_len, self.model.hidden_size, device=device).normal_()
+            torch.empty(
+                1, adapter_len, self.model.hidden_size, device=device, dtype=model.q_proj.weight.dtype
+            ).normal_()
         )
         # Initialize the gate to 0 as this is "zero-init".
-        self.adaption_gate = nn.Parameter(torch.zeros(1, device=device))
+        self.adaption_gate = nn.Parameter(torch.zeros(1, device=device, dtype=model.q_proj.weight.dtype))
 
     def forward(self, **kwargs):
         """
@@ -343,11 +345,12 @@ class AdaptedAttention(nn.Module):
         # (bsz, num_heads, q_len, head_dim)
         query_states = compute_query_states(model=self.model, **kwargs)
 
+        previous_dtype = query_states.dtype
         # (bsz, num_heads, q_len, adapter_len)
         scores = torch.matmul(query_states, adapter_k.transpose(2, 3)) / math.sqrt(self.model.head_dim)
         # Upcast attention to fp32
         # (bsz, num_heads, q_len, adapter_len)
-        scores = self.adaption_gate * F.softmax(scores, dim=-1, dtype=torch.float32).to(query_states.dtype)
+        scores = self.adaption_gate * F.softmax(scores, dim=-1, dtype=torch.float32).to(previous_dtype)
         # (bsz, q_len, num_heads * head_dim)
         adapter_output = torch.matmul(scores, adapter_v).transpose(1, 2).reshape(bsz, q_len, -1)
         # (bsz, q_len, hidden_size)
@@ -356,4 +359,7 @@ class AdaptedAttention(nn.Module):
 
         # Add adaption prompt output to original output.
         output = output + adapter_output
+
+        # Restore original dtype.
+        output = output.to(previous_dtype)
         return output, None, past_key_value