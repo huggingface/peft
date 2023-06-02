6e96e229962855a2a11f18a0461acd05dbdbdf73
Marc Sun <57196510+SunMarc@users.noreply.github.com>
2023-06-02 04:07:46 -0400

    Remove device_map when training 4,8-bit model. (#534)
    * Remove device_map when training 4,8-bit model.
    
    * Fix style

--- examples/fp4_finetuning/finetune_fp4_opt_bnb_peft.py
+++ examples/fp4_finetuning/finetune_fp4_opt_bnb_peft.py
@@ -45,7 +45,6 @@ max_memory = {i: max_memory for i in range(n_gpus)}
 
 model = AutoModelForCausalLM.from_pretrained(
     "facebook/opt-350m",
-    device_map="auto",
     max_memory=max_memory,
     quantization_config=BitsAndBytesConfig(
         load_in_4bit=True,
