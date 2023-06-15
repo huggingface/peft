c2c8bf7a7a70ebefac069c5f65667450d583edff
younesbelkada <younesbelkada@gmail.com>
2023-06-15 08:24:54 +0000

    add tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -16,7 +16,14 @@ import os
 import tempfile
 import unittest
 
-from peft import AdaptionPromptConfig, LoraConfig, PrefixTuningConfig, PromptEncoderConfig, PromptTuningConfig
+from peft import (
+    AdaptionPromptConfig,
+    LoraConfig,
+    PeftConfig,
+    PrefixTuningConfig,
+    PromptEncoderConfig,
+    PromptTuningConfig,
+)
 
 
 PEFT_MODELS_TO_TEST = [("lewtun/tiny-random-OPTForCausalLM-delta", "v1")]
@@ -96,6 +103,24 @@ class PeftConfigTester(unittest.TestCase, PeftConfigTestMixin):
             config = config_class()
             self.assertEqual(config.to_dict(), config.__dict__)
             self.assertTrue(isinstance(config.to_dict(), dict))
+
+    def test_from_pretrained_cache_dir(self):
+        r"""
+        Test if the config is correctly loaded with extra kwargs
+        """
+        with tempfile.TemporaryDirectory() as tmp_dirname:
+            for config_class in self.all_config_classes:
+                for model_name, revision in PEFT_MODELS_TO_TEST:
+                    # Test we can load config from delta
+                    _ = config_class.from_pretrained(model_name, revision=revision, cache_dir=tmp_dirname)
+
+    def test_from_pretrained_cache_dir_remote(self):
+        r"""
+        Test if the config is correctly loaded with a checkpoint from the hub
+        """
+        with tempfile.TemporaryDirectory() as tmp_dirname:
+            _ = PeftConfig.from_pretrained("ybelkada/test-st-lora", cache_dir=tmp_dirname)
+            self.assertTrue("models--ybelkada--test-st-lora" in os.listdir(tmp_dirname))
 
     def test_set_attributes(self):
         # manually set attributes and check if they are correctly written
--- tests/test_decoder_models.py
+++ tests/test_decoder_models.py
@@ -103,3 +103,7 @@ class PeftDecoderModelTester(unittest.TestCase, PeftCommonTester):
     @parameterized.expand(PeftTestConfigManager.get_grid_parameters(FULL_GRID))
     def test_inference_safetensors(self, test_name, model_id, config_cls, config_kwargs):
         self._test_inference_safetensors(model_id, config_cls, config_kwargs)
+
+    @parameterized.expand(PeftTestConfigManager.get_grid_parameters(FULL_GRID))
+    def test_peft_model_device_map(self, test_name, model_id, config_cls, config_kwargs):
+        self._test_peft_model_device_map(model_id, config_cls, config_kwargs)
