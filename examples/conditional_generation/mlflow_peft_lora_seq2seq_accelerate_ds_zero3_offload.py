13f26502b9b3b36361eedd0944ce440c519846b3
Kim Moreh <kim.nguyen@moreh.com.vn>
2023-07-07 13:32:05 +0900

    add for condi generation, single  model

---
+++ examples/conditional_generation/mlflow_peft_lora_seq2seq_accelerate_ds_zero3_offload.py
@@ -0,0 +1,323 @@
+import gc
+import os
+import sys
+import threading
+
+import numpy as np
+import psutil
+import torch
+from accelerate import Accelerator
+from datasets import load_dataset
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, get_linear_schedule_with_warmup, set_seed
+
+from peft import LoraConfig, TaskType, get_peft_model
+import mlflow
+
+
+def levenshtein_distance(str1, str2):
+    # TC: O(N^2)
+    # SC: O(N^2)
+    if str1 == str2:
+        return 0
+    num_rows = len(str1) + 1
+    num_cols = len(str2) + 1
+    dp_matrix = np.empty((num_rows, num_cols))
+    dp_matrix[0, :] = range(num_cols)
+    dp_matrix[:, 0] = range(num_rows)
+
+    for i in range(1, num_rows):
+        for j in range(1, num_cols):
+            if str1[i - 1] == str2[j - 1]:
+                dp_matrix[i, j] = dp_matrix[i - 1, j - 1]
+            else:
+                dp_matrix[i, j] = min(dp_matrix[i - 1, j - 1], dp_matrix[i - 1, j], dp_matrix[i, j - 1]) + 1
+
+    return dp_matrix[num_rows - 1, num_cols - 1]
+
+
+def get_closest_label(eval_pred, classes):
+    min_id = sys.maxsize
+    min_edit_distance = sys.maxsize
+    for i, class_label in enumerate(classes):
+        edit_distance = levenshtein_distance(eval_pred.strip(), class_label)
+        if edit_distance < min_edit_distance:
+            min_id = i
+            min_edit_distance = edit_distance
+    return classes[min_id]
+
+
+# Converting Bytes to Megabytes
+def b2mb(x):
+    return int(x / 2**20)
+
+
+# This context manager is used to track the peak memory usage of the process
+class TorchTracemalloc:
+    def __enter__(self):
+        gc.collect()
+        torch.cuda.empty_cache()
+        torch.cuda.reset_max_memory_allocated()  # reset the peak gauge to zero
+        self.begin = torch.cuda.memory_allocated()
+        self.process = psutil.Process()
+
+        self.cpu_begin = self.cpu_mem_used()
+        self.peak_monitoring = True
+        peak_monitor_thread = threading.Thread(target=self.peak_monitor_func)
+        peak_monitor_thread.daemon = True
+        peak_monitor_thread.start()
+        return self
+
+    def cpu_mem_used(self):
+        """get resident set size memory for the current process"""
+        return self.process.memory_info().rss
+
+    def peak_monitor_func(self):
+        self.cpu_peak = -1
+
+        while True:
+            self.cpu_peak = max(self.cpu_mem_used(), self.cpu_peak)
+
+            # can't sleep or will not catch the peak right (this comment is here on purpose)
+            # time.sleep(0.001) # 1msec
+
+            if not self.peak_monitoring:
+                break
+
+    def __exit__(self, *exc):
+        self.peak_monitoring = False
+
+        gc.collect()
+        torch.cuda.empty_cache()
+        self.end = torch.cuda.memory_allocated()
+        self.peak = torch.cuda.max_memory_allocated()
+        self.used = b2mb(self.end - self.begin)
+        self.peaked = b2mb(self.peak - self.begin)
+
+        self.cpu_end = self.cpu_mem_used()
+        self.cpu_used = b2mb(self.cpu_end - self.cpu_begin)
+        self.cpu_peaked = b2mb(self.cpu_peak - self.cpu_begin)
+        # print(f"delta used/peak {self.used:4d}/{self.peaked:4d}")
+
+
+def main():
+    accelerator = Accelerator()
+    # model_name_or_path = "bigscience/T0_3B"
+    model_name_or_path = "facebook/bart-large"
+    dataset_name = "twitter_complaints"
+    peft_config = LoraConfig(
+        task_type=TaskType.SEQ_2_SEQ_LM, inference_mode=False, r=8, lora_alpha=32, lora_dropout=0.1
+    )
+    text_column = "Tweet text"
+    label_column = "text_label"
+    lr = 3e-3
+    num_epochs = 5
+    batch_size = 8
+    seed = 42
+    do_test = False
+    set_seed(seed)
+
+    dataset = load_dataset("ought/raft", dataset_name)
+    classes = [k.replace("_", " ") for k in dataset["train"].features["Label"].names]
+    dataset = dataset.map(
+        lambda x: {"text_label": [classes[label] for label in x["Label"]]},
+        batched=True,
+        num_proc=1,
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+    target_max_length = max([len(tokenizer(class_label)["input_ids"]) for class_label in classes])
+
+    # mlflow initial
+    experiment_id = mlflow.create_experiment('conditional_generation-{}'.format(model_name_or_path))
+    experiment = mlflow.get_experiment(experiment_id)
+    mlflow_runner = mlflow.start_run(run_name=model_name_or_path, experiment_id=experiment.experiment_id)
+
+    def preprocess_function(examples):
+        inputs = examples[text_column]
+        targets = examples[label_column]
+        model_inputs = tokenizer(inputs, truncation=True)
+        labels = tokenizer(
+            targets, max_length=target_max_length, padding="max_length", truncation=True, return_tensors="pt"
+        )
+        labels = labels["input_ids"]
+        labels[labels == tokenizer.pad_token_id] = -100
+        model_inputs["labels"] = labels
+        return model_inputs
+
+    with accelerator.main_process_first():
+        processed_datasets = dataset.map(
+            preprocess_function,
+            batched=True,
+            num_proc=1,
+            remove_columns=dataset["train"].column_names,
+            load_from_cache_file=True,
+            desc="Running tokenizer on dataset",
+        )
+    accelerator.wait_for_everyone()
+
+    train_dataset = processed_datasets["train"]
+    eval_dataset = processed_datasets["train"]
+    test_dataset = processed_datasets["test"]
+
+    def collate_fn(examples):
+        return tokenizer.pad(examples, padding="longest", return_tensors="pt")
+
+    train_dataloader = DataLoader(
+        train_dataset, shuffle=True, collate_fn=collate_fn, batch_size=batch_size, pin_memory=True
+    )
+    eval_dataloader = DataLoader(eval_dataset, collate_fn=collate_fn, batch_size=batch_size, pin_memory=True)
+    test_dataloader = DataLoader(test_dataset, collate_fn=collate_fn, batch_size=batch_size, pin_memory=True)
+
+    # creating model
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name_or_path)
+    model = get_peft_model(model, peft_config)
+    model.print_trainable_parameters()
+
+    # optimizer
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+
+    # lr scheduler
+    lr_scheduler = get_linear_schedule_with_warmup(
+        optimizer=optimizer,
+        num_warmup_steps=0,
+        num_training_steps=(len(train_dataloader) * num_epochs),
+    )
+
+    model, train_dataloader, eval_dataloader, test_dataloader, optimizer, lr_scheduler = accelerator.prepare(
+        model, train_dataloader, eval_dataloader, test_dataloader, optimizer, lr_scheduler
+    )
+    accelerator.print(model)
+
+    is_ds_zero_3 = False
+    if getattr(accelerator.state, "deepspeed_plugin", None):
+        is_ds_zero_3 = accelerator.state.deepspeed_plugin.zero_stage == 3
+
+    with mlflow_runner:
+        for epoch in range(num_epochs):
+            with TorchTracemalloc() as tracemalloc:
+                model.train()
+                total_loss = 0
+                for step, batch in enumerate(tqdm(train_dataloader)):
+                    outputs = model(**batch)
+                    loss = outputs.loss
+                    total_loss += loss.detach().float()
+                    accelerator.backward(loss)
+                    optimizer.step()
+                    lr_scheduler.step()
+                    optimizer.zero_grad()
+                    mlflow.log_metric('loss', loss)
+                    mlflow.log_metric('total loss', total_loss)
+
+
+            # Printing the GPU memory usage details such as allocated memory, peak memory, and total memory usage
+            accelerator.print("GPU Memory before entering the train : {}".format(b2mb(tracemalloc.begin)))
+            accelerator.print("GPU Memory consumed at the end of the train (end-begin): {}".format(tracemalloc.used))
+            accelerator.print("GPU Peak Memory consumed during the train (max-begin): {}".format(tracemalloc.peaked))
+            accelerator.print(
+                "GPU Total Peak Memory consumed during the train (max): {}".format(
+                    tracemalloc.peaked + b2mb(tracemalloc.begin)
+                )
+            )
+
+            accelerator.print("CPU Memory before entering the train : {}".format(b2mb(tracemalloc.cpu_begin)))
+            accelerator.print("CPU Memory consumed at the end of the train (end-begin): {}".format(tracemalloc.cpu_used))
+            accelerator.print("CPU Peak Memory consumed during the train (max-begin): {}".format(tracemalloc.cpu_peaked))
+            accelerator.print(
+                "CPU Total Peak Memory consumed during the train (max): {}".format(
+                    tracemalloc.cpu_peaked + b2mb(tracemalloc.cpu_begin)
+                )
+            )
+            train_epoch_loss = total_loss / len(train_dataloader)
+            train_ppl = torch.exp(train_epoch_loss)
+            accelerator.print(f"{epoch=}: {train_ppl=} {train_epoch_loss=}")
+
+            model.eval()
+            eval_preds = []
+            with TorchTracemalloc() as tracemalloc:
+                for _, batch in enumerate(tqdm(eval_dataloader)):
+                    batch = {k: v for k, v in batch.items() if k != "labels"}
+                    with torch.no_grad():
+                        outputs = accelerator.unwrap_model(model).generate(
+                            **batch, synced_gpus=is_ds_zero_3
+                        )  # synced_gpus=True for DS-stage 3
+                    outputs = accelerator.pad_across_processes(outputs, dim=1, pad_index=tokenizer.pad_token_id)
+                    preds = accelerator.gather_for_metrics(outputs).detach().cpu().numpy()
+                    eval_preds.extend(tokenizer.batch_decode(preds, skip_special_tokens=True))
+
+            # Printing the GPU memory usage details such as allocated memory, peak memory, and total memory usage
+            accelerator.print("GPU Memory before entering the eval : {}".format(b2mb(tracemalloc.begin)))
+            accelerator.print("GPU Memory consumed at the end of the eval (end-begin): {}".format(tracemalloc.used))
+            accelerator.print("GPU Peak Memory consumed during the eval (max-begin): {}".format(tracemalloc.peaked))
+            accelerator.print(
+                "GPU Total Peak Memory consumed during the eval (max): {}".format(
+                    tracemalloc.peaked + b2mb(tracemalloc.begin)
+                )
+            )
+
+            accelerator.print("CPU Memory before entering the eval : {}".format(b2mb(tracemalloc.cpu_begin)))
+            accelerator.print("CPU Memory consumed at the end of the eval (end-begin): {}".format(tracemalloc.cpu_used))
+            accelerator.print("CPU Peak Memory consumed during the eval (max-begin): {}".format(tracemalloc.cpu_peaked))
+            accelerator.print(
+                "CPU Total Peak Memory consumed during the eval (max): {}".format(
+                    tracemalloc.cpu_peaked + b2mb(tracemalloc.cpu_begin)
+                )
+            )
+
+            correct = 0
+            total = 0
+            assert len(eval_preds) == len(
+                dataset["train"][label_column]
+            ), f"{len(eval_preds)} != {len(dataset['train'][label_column])}"
+            for pred, true in zip(eval_preds, dataset["train"][label_column]):
+                if pred.strip() == true.strip():
+                    correct += 1
+                total += 1
+            accuracy = correct / total * 100
+            accelerator.print(f"{accuracy=}")
+            accelerator.print(f"{eval_preds[:10]=}")
+            accelerator.print(f"{dataset['train'][label_column][:10]=}")
+        mlflow.end_run()
+
+    if do_test:
+        model.eval()
+        test_preds = []
+        for _, batch in enumerate(tqdm(test_dataloader)):
+            batch = {k: v for k, v in batch.items() if k != "labels"}
+            with torch.no_grad():
+                outputs = accelerator.unwrap_model(model).generate(
+                    **batch, synced_gpus=is_ds_zero_3
+                )  # synced_gpus=True for DS-stage 3
+            outputs = accelerator.pad_across_processes(outputs, dim=1, pad_index=tokenizer.pad_token_id)
+            preds = accelerator.gather(outputs).detach().cpu().numpy()
+            test_preds.extend(tokenizer.batch_decode(preds, skip_special_tokens=True))
+
+        test_preds_cleaned = []
+        for _, pred in enumerate(test_preds):
+            test_preds_cleaned.append(get_closest_label(pred, classes))
+
+        test_df = dataset["test"].to_pandas()
+        assert len(test_preds_cleaned) == len(test_df), f"{len(test_preds_cleaned)} != {len(test_df)}"
+        test_df[label_column] = test_preds_cleaned
+        test_df["text_labels_orig"] = test_preds
+        accelerator.print(test_df[[text_column, label_column]].sample(20))
+
+        pred_df = test_df[["ID", label_column]]
+        pred_df.columns = ["ID", "Label"]
+
+        os.makedirs(f"data/{dataset_name}", exist_ok=True)
+        pred_df.to_csv(f"data/{dataset_name}/predictions.csv", index=False)
+
+    accelerator.wait_for_everyone()
+    model.push_to_hub(
+        "smangrul/"
+        + f"{dataset_name}_{model_name_or_path}_{peft_config.peft_type}_{peft_config.task_type}".replace("/", "_"),
+        state_dict=accelerator.get_state_dict(model),
+        use_auth_token=True,
+    )
+    accelerator.wait_for_everyone()
+
+
+if __name__ == "__main__":
+    main()
---
+++ examples/conditional_generation/mlflow_peft_lora_seq2seq_accelerate_fsdp.py
@@ -0,0 +1,148 @@
+import os
+
+import torch
+from accelerate import Accelerator
+from datasets import load_dataset
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, default_data_collator, get_linear_schedule_with_warmup
+
+from peft import LoraConfig, TaskType, get_peft_model
+from peft.utils.other import fsdp_auto_wrap_policy
+import mlflow
+
+
+def main():
+    accelerator = Accelerator()
+    model_name_or_path = "t5-base"
+    batch_size = 8
+    text_column = "sentence"
+    label_column = "label"
+    max_length = 64
+    lr = 1e-3
+    num_epochs = 1
+    base_path = "temp/data/FinancialPhraseBank-v1.0"
+
+    peft_config = LoraConfig(
+        task_type=TaskType.SEQ_2_SEQ_LM, inference_mode=False, r=8, lora_alpha=32, lora_dropout=0.1
+    )
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name_or_path)
+    model = get_peft_model(model, peft_config)
+    accelerator.print(model.print_trainable_parameters())
+
+    dataset = load_dataset(
+        "json",
+        data_files={
+            "train": os.path.join(base_path, "financial_phrase_bank_train.jsonl"),
+            "validation": os.path.join(base_path, "financial_phrase_bank_val.jsonl"),
+        },
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+
+    def preprocess_function(examples):
+        inputs = examples[text_column]
+        targets = examples[label_column]
+        model_inputs = tokenizer(
+            inputs, max_length=max_length, padding="max_length", truncation=True, return_tensors="pt"
+        )
+        labels = tokenizer(targets, max_length=2, padding="max_length", truncation=True, return_tensors="pt")
+        labels = labels["input_ids"]
+        labels[labels == tokenizer.pad_token_id] = -100
+        model_inputs["labels"] = labels
+        return model_inputs
+
+    # mlflow initial
+    experiment_id = mlflow.create_experiment('conditional_generation-{}'.format(model_name_or_path))
+    experiment = mlflow.get_experiment(experiment_id)
+    mlflow_runner = mlflow.start_run(run_name=model_name_or_path, experiment_id=experiment.experiment_id)
+
+    with accelerator.main_process_first():
+        processed_datasets = dataset.map(
+            preprocess_function,
+            batched=True,
+            num_proc=1,
+            remove_columns=dataset["train"].column_names,
+            load_from_cache_file=False,
+            desc="Running tokenizer on dataset",
+        )
+
+    train_dataset = processed_datasets["train"]
+    eval_dataset = processed_datasets["validation"]
+
+    train_dataloader = DataLoader(
+        train_dataset, shuffle=True, collate_fn=default_data_collator, batch_size=batch_size, pin_memory=True
+    )
+    eval_dataloader = DataLoader(
+        eval_dataset, collate_fn=default_data_collator, batch_size=batch_size, pin_memory=True
+    )
+
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+    lr_scheduler = get_linear_schedule_with_warmup(
+        optimizer=optimizer,
+        num_warmup_steps=0,
+        num_training_steps=(len(train_dataloader) * num_epochs),
+    )
+
+    if getattr(accelerator.state, "fsdp_plugin", None) is not None:
+        accelerator.state.fsdp_plugin.auto_wrap_policy = fsdp_auto_wrap_policy(model)
+
+    model, train_dataloader, eval_dataloader, optimizer, lr_scheduler = accelerator.prepare(
+        model, train_dataloader, eval_dataloader, optimizer, lr_scheduler
+    )
+    accelerator.print(model)
+
+    with mlflow_runner:
+        for epoch in range(num_epochs):
+            model.train()
+            total_loss = 0
+            for step, batch in enumerate(tqdm(train_dataloader)):
+                outputs = model(**batch)
+                loss = outputs.loss
+                total_loss += loss.detach().float()
+                loss.backward()
+                optimizer.step()
+                lr_scheduler.step()
+                optimizer.zero_grad()
+                mlflow.log_metric('loss', loss)
+                mlflow.log_metric('total loss', total_loss)
+
+            model.eval()
+            eval_loss = 0
+            eval_preds = []
+            for step, batch in enumerate(tqdm(eval_dataloader)):
+                with torch.no_grad():
+                    outputs = model(**batch)
+                loss = outputs.loss
+                eval_loss += loss.detach().float()
+                preds = accelerator.gather_for_metrics(torch.argmax(outputs.logits, -1)).detach().cpu().numpy()
+                eval_preds.extend(tokenizer.batch_decode(preds, skip_special_tokens=True))
+            eval_epoch_loss = eval_loss / len(eval_dataloader)
+            eval_ppl = torch.exp(eval_epoch_loss)
+            train_epoch_loss = total_loss / len(train_dataloader)
+            train_ppl = torch.exp(train_epoch_loss)
+            accelerator.print(f"{epoch=}: {train_ppl=} {train_epoch_loss=} {eval_ppl=} {eval_epoch_loss=}")
+
+            correct = 0
+            total = 0
+            for pred, true in zip(eval_preds, dataset["validation"][label_column]):
+                if pred.strip() == true.strip():
+                    correct += 1
+                total += 1
+            accuracy = correct / total * 100
+            accelerator.print(f"{accuracy=}")
+            accelerator.print(f"{eval_preds[:10]=}")
+            accelerator.print(f"{dataset['validation'][label_column][:10]=}")
+            accelerator.wait_for_everyone()
+            model.push_to_hub(
+                "smangrul/" + f"{model_name_or_path}_{peft_config.peft_type}_{peft_config.task_type}".replace("/", "_"),
+                state_dict=accelerator.get_state_dict(model),
+                use_auth_token=True,
+            )
+            accelerator.wait_for_everyone()
+        mlflow.end_run()
+        
+
+
+if __name__ == "__main__":
+    main()
