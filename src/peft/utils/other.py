cf16a9c8b58e3aa25a24f959155939733e854b39
Trapper4888 <133875445+Trapper4888@users.noreply.github.com>
2023-07-25 19:29:01 +0200

    Add btlm to officially supported LoRA

--- src/peft/utils/other.py
+++ src/peft/utils/other.py
@@ -285,6 +285,7 @@ TRANSFORMERS_MODELS_TO_LORA_TARGET_MODULES_MAPPING = {
     "RefinedWebModel": ["query_key_value"],
     "RefinedWeb": ["query_key_value"],
     "falcon": ["query_key_value"],
+    "btlm": ["c_proj", "c_attn"],
 }
 
 TRANSFORMERS_MODELS_TO_IA3_TARGET_MODULES_MAPPING = {
