cacee957e605605f390bc1904fc8b303bba6f798
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2023-09-29 09:44:30 +0530

    [tests] add multiple active adapters tests (#961)
    * add tests for multiple active adapters
    
    * add multiple active adapter tests
    
    * fix tests
    
    * fix the device error
    
    * fix typo
    
    * fix the variables
    
    * fix the `adalora` config
    
    * add util function for proper naming of tests
    
    * fix bugs
    
    1. fix `add_weighted_adapter` when working with adapters targeting different layers
    2. fix `ia3` model and layer to handle adapters targeting different layers
    3. fix the multiple active adapter tests
    
    * fix `ia3` issue
    
    * remove debug statements
    
    * fix test
    
    * fix bug
    
    * address comments
    
    * address comments
    
    Co-Authored-By: Benjamin Bossan <BenjaminBossan@users.noreply.github.com>
    
    * fix tests
    
    * remove unused code
    
    * Update test_custom_models.py
    
    * increasing tolerance for a test
    
    ---------
    
    Co-authored-by: Benjamin Bossan <BenjaminBossan@users.noreply.github.com>

--- src/peft/tuners/lora/model.py
+++ src/peft/tuners/lora/model.py
@@ -167,7 +167,7 @@ class LoraModel(BaseTuner):
             raise ValueError("Current Key shouldn't be `None`")
         # Regexp matching - Find key which matches current target_name in patterns provided
         pattern_keys = list(chain(lora_config.rank_pattern.keys(), lora_config.alpha_pattern.keys()))
-        target_name_key = next(filter(lambda key: re.match(f".*\.{key}$", current_key), pattern_keys), target_name)
+        target_name_key = next(filter(lambda key: re.match(f".*\.{key}$", current_key), pattern_keys), current_key)
 
         r = lora_config.rank_pattern.get(target_name_key, lora_config.r)
         alpha = lora_config.alpha_pattern.get(target_name_key, lora_config.lora_alpha)
@@ -518,7 +518,27 @@ class LoraModel(BaseTuner):
         else:
             raise ValueError(f"Invalid combination_type: {combination_type}")
 
-        self.peft_config[adapter_name] = replace(self.peft_config[adapters[0]], r=new_rank, lora_alpha=new_rank)
+        target_modules_type = type(self.peft_config[adapters[0]].target_modules)
+        new_target_modules = set() if target_modules_type == list else ""
+        for adapter in adapters:
+            if type(self.peft_config[adapter].target_modules) != target_modules_type:
+                raise ValueError(
+                    "all adapter configs should follow the same target modules type. "
+                    "Combining adapters with `target_modules` type being a mix of list and string is not supported."
+                )
+            if target_modules_type == list:
+                new_target_modules |= set(self.peft_config[adapter].target_modules)
+            else:
+                new_target_modules += f"({self.peft_config[adapter].target_modules})|"
+
+        new_target_modules = list(new_target_modules) if target_modules_type == list else new_target_modules[:-1]
+
+        self.peft_config[adapter_name] = replace(
+            self.peft_config[adapters[0]],
+            r=new_rank,
+            lora_alpha=new_rank,
+            target_modules=new_target_modules,
+        )
         self.inject_adapter(self.model, adapter_name)
 
         # Do we really need that?
@@ -534,6 +554,8 @@ class LoraModel(BaseTuner):
                 elif adapter_name in target.lora_embedding_A:
                     target_lora_A = target.lora_embedding_A[adapter_name]
                     target_lora_B = target.lora_embedding_B[adapter_name]
+                else:
+                    continue
 
                 target_lora_A.data = target_lora_A.data * 0.0
                 target_lora_B.data = target_lora_B.data * 0.0
@@ -545,6 +567,8 @@ class LoraModel(BaseTuner):
                         elif adapter in target.lora_embedding_A:
                             current_adapter_lora_A = target.lora_embedding_A[adapter]
                             current_adapter_lora_B = target.lora_embedding_B[adapter]
+                        else:
+                            continue
                         target_lora_A.data += current_adapter_lora_A.data * weight * target.scaling[adapter]
                         target_lora_B.data += current_adapter_lora_B.data
                 elif combination_type == "cat":
@@ -556,10 +580,17 @@ class LoraModel(BaseTuner):
                         elif adapter in target.lora_embedding_A:
                             current_adapter_lora_A = target.lora_embedding_A[adapter]
                             current_adapter_lora_B = target.lora_embedding_B[adapter]
+                        else:
+                            continue
                         loras_A.append(current_adapter_lora_A.data * weight * target.scaling[adapter])
                         loras_B.append(current_adapter_lora_B.data)
-                    torch.cat(loras_A, dim=0, out=target_lora_A.data)
-                    torch.cat(loras_B, dim=1, out=target_lora_B.data)
+
+                    if len(loras_A) == 0:
+                        raise ValueError("No matching LoRAs found. Please raise an issue on Github.")
+                    loras_A = torch.cat(loras_A, dim=0)
+                    loras_B = torch.cat(loras_B, dim=1)
+                    target_lora_A.data[: loras_A.shape[0], :] = loras_A
+                    target_lora_B.data[:, : loras_B.shape[1]] = loras_B
                 elif combination_type == "svd":
                     target_lora_A.data, target_lora_B.data = self._svd_weighted_adapter(
                         adapters,
@@ -585,8 +616,19 @@ class LoraModel(BaseTuner):
         full_matrices=True,
         driver=None,
     ):
-        delta_weight = weights[0] * target.get_delta_weight(adapters[0])
-        for adapter, weight in zip(adapters[1:], weights[1:]):
+        valid_adapters = []
+        valid_weights = []
+        for adapter, weight in zip(adapters, weights):
+            if adapter in target.lora_A or adapter in target.lora_embedding_A:
+                valid_adapters.append(adapter)
+                valid_weights.append(weight)
+
+        # if no valid adapter, nothing to do
+        if len(valid_adapters) == 0:
+            raise ValueError("No matching LoRAs found. Please raise an issue on Github.")
+
+        delta_weight = valid_weights[0] * target.get_delta_weight(valid_adapters[0])
+        for adapter, weight in zip(valid_adapters[1:], valid_weights[1:]):
             delta_weight += weight * target.get_delta_weight(adapter)
         conv2d = isinstance(target, Conv2d)
         if conv2d:
