3db9c6818bd3c5d9b8d8cc20e90e0e4514a8536a
Benjamin Bossan <benjamin.bossan@gmail.com>
2023-10-05 16:15:17 +0200

    TST Add regression tests
    This is a first step towards adding regression tests to the project.
    These tests allow us to load old adapters with new PEFT versions and
    ensure that the output generated by the model does not change.
    
    The PR includes a framework for how to add regression artifacts and how
    to run regression tests based on those artifacts. Right now, only bnb +
    LoRA is covered, but it should be straightforward to add more tests.
    
    Description
    
    In general, for regression tests, we need two steps:
    
    1. Creating the regression artifacts, in this case the adapter
       checkpoint and the expected output of the model.
    2. Running the regression tests, i.e. loading the adapter and checking
       that the output of the model is the same as the expected output.
    
    My approach is to re-use as much code as possible betweeen those two
    steps. Therefore, the same test script can be used for both, with only
    an environment variable to distinguish between the two. Step 1 is
    invoked by calling:
    
    REGRESSION_CREATION_MODE=True pytest tests/regression/test_regression.py
    
    and to run the second step, we call:
    
    pytest tests/regression/test_regression.py
    
    Creating regression artifacts
    
    The first step will create an adapter checkpoint and an output for the
    given PEFT version and test setting in a new directory. E.g. it will
    create a directory tests/regression/lora_opt-125m_bnb_4bit/0.5.0/ that
    contains adapter_model.bin and output.pt.
    
    Before this step runs, there is a check that the git repo is clean (no
    dirty worktree) and that the commit is tagged (i.e. corresponds to a
    release version of PEFT). Otherwise, we may accidentally create
    regression artifacts that do not correspond to any PEFT release.
    
    The easiest way to get such a clean state (say, for PEFT v0.5.0) is by
    checking out a tagged commit, e.g:
    
    git checkout v0.5.0
    
    before running the first step.
    
    The first step will also skip the creation of regression artifacts if
    they already exist.
    
    It is possible to circumvent all the aforementioned checks by setting
    the environment variable REGRESSION_FORCE_MODE to True like so:
    
    REGRESSION_FORCE_MODE=True REGRESSION_CREATION_MODE=True pytest tests/regression/test_regression.py
    
    You should only do this if you know exactly what you're doing.
    
    Running regression tests
    
    The second step is much simpler. It will load the adapters and the
    output created in the first step, and compare the output to the output
    from a new PEFT model using the loaded adapter. The outputs should be
    the same.
    
    If more than one version is discovered for a given test setting, all of
    them are tested.
    
    Notes
    
    As is, the adapters are stored in the git repo itself. Since they're
    relatively small, the total size of the repo is still reasonable.
    However, it could be better to store those adapters on HF Hub instead.
    This would, however, make things a bit more complicated (not sure how to
    parse directories etc. on Hub).

---
+++ tests/regression/test_regression.py
@@ -0,0 +1,227 @@
+# coding=utf-8
+# Copyright 2023-present the HuggingFace Inc. team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Regression testing: check that checkpoints from previous PEFT versions still return the same values.
+#
+# For normal regression testing, just run:
+#
+# `pytest tests/regression/test_regression.py -s`.
+#
+# Add `-s` to show potentially useful debugging information.
+#
+# To create new regression tests, run:
+# `REGRESSION_CREATION_MODE=True pytest tests/regression/test_regression.py -s`.
+#
+# This will *fail* if:
+#
+# 1. the git worktree is dirty
+# 2. the git commit is not tagged
+#
+# This is important to ensure that the regression artifacts correspond to a specific released version of PEFT.
+# Therefore, it is recommended to checkout the tag before running the regression tests, e.g. by running:
+#
+# `git checkout v0.1.0`
+#
+# To override these checks, run:
+# `REGRESSION_CREATION_MODE=True REGRESSION_FORCE_MODE=True pytest tests/regression/test_regression.py -s`.
+#
+# In REGRESSION_CREATION_MODE, one directory will be created in tests/regression/<TEST_NAME>/<PEFT_VERSION>/ for each
+# test. This will contain the saved adapter, as well as the output of the test of the model for that version.
+#
+# In normal testing mode, the saved adapter and output for each version found in the directory
+# tests/regression/<TEST_NAME>/ will be loaded and compared to the current output.
+
+import os
+import subprocess
+import sys
+import unittest
+
+import torch
+from transformers import AutoModelForCausalLM, BitsAndBytesConfig
+
+import peft
+from peft import LoraConfig, PeftModel, get_peft_model
+from peft.utils import infer_device
+
+
+PEFT_VERSION = peft.__version__
+REGRESSION_DIR = os.path.join(os.getcwd(), "tests", "regression")
+
+
+def strtobool(val):
+    """Copied from distutils.util"""
+    val = val.lower()
+    if val in ("y", "yes", "t", "true", "on", "1"):
+        return 1
+    elif val in ("n", "no", "f", "false", "off", "0"):
+        return 0
+    else:
+        raise ValueError("invalid truth value {!r}".format(val))
+
+
+def save_output(output, name, force=False):
+    path = os.path.join(REGRESSION_DIR, name, PEFT_VERSION)
+    filename = os.path.join(path, "output.pt")
+    if os.path.exists(filename) and not force:
+        return
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    if os.path.exists(filename) and force:
+        print(f"Overriding existing output in {filename}", file=sys.stderr)
+
+    torch.save(output, filename)
+
+
+def save_model(model, name, force=False):
+    path = os.path.join(REGRESSION_DIR, name, PEFT_VERSION)
+    if os.path.exists(os.path.join(path, "adapter_model.bin")) and not force:
+        return
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    if os.path.exists(path) and force:
+        print(f"Overriding existing model in {path}", file=sys.stderr)
+
+    model.save_pretrained(path)
+
+
+def load_output(name):
+    filename = os.path.join(REGRESSION_DIR, name, "output.pt")
+    return torch.load(filename)
+
+
+class RegressionTester(unittest.TestCase):
+    """Base class for regression testing
+
+    Child classes must call assert_results_equal_or_store and pass the model outtput, as well as a unique name that
+    describes the setting (e.g. "lora_opt-125m_bnb_4bit"). They also need to implement get_output(model) to get the
+    model output, and load_base_model(name) to load the base model.
+    """
+
+    torch_device = infer_device()
+
+    def setUp(self):
+        self.creation_mode = strtobool(os.environ.get("REGRESSION_CREATION_MODE", "False"))
+        self.force_mode = strtobool(os.environ.get("REGRESSION_FORCE_MODE", "False"))
+        if self.force_mode and not self.creation_mode:
+            raise RuntimeError("REGRESSION_FORCE_MODE can only be used together with REGRESSION_CREATION_MODE")
+        if self.creation_mode:
+            self.check_clean_git_status(self.force_mode)
+
+    def fix_seed(self):
+        torch.manual_seed(0)
+
+    def check_clean_git_status(self, force):
+        """Ensure that worktree is not dirty and version tag is checked out"""
+        # check that the worktree is clean
+        try:
+            subprocess.check_output(["git", "diff", "--quiet", "HEAD"])
+        except subprocess.CalledProcessError as exc:
+            if force:
+                print("Overriding despite dirty git worktree", file=sys.stderr)
+            else:
+                raise RuntimeError("Git worktree is dirty") from exc
+
+        # check that the commit is tagged
+        try:
+            subprocess.check_output(["git", "describe", "--exact-match", "HEAD"])
+        except subprocess.CalledProcessError as exc:
+            if force:
+                print("Overriding despite non-tagged commit", file=sys.stderr)
+            else:
+                raise RuntimeError("Git commit is not tagged") from exc
+
+    def assert_results_equal_or_store(self, model, name):
+        """Check if the outputs are the same or save the outputs if in creation mode."""
+        if not self.creation_mode:  # normal regression testing mode
+            self._assert_results_equal(name)
+        else:
+            output = self.get_output(model)
+            save_output(output, name, force=self.force_mode)
+            save_model(model, name)
+
+    def _assert_results_equal(self, name):
+        # TODO: abstract
+        path = os.path.join(REGRESSION_DIR, name)
+        versions = os.listdir(path)
+        for version in versions:  # each directory corresponds to a version
+            output_loaded = load_output(os.path.join(name, version))
+            base_model = self.load_base_model()
+            model = PeftModel.from_pretrained(base_model, os.path.join(path, version))
+            output = self.get_output(model)
+            self.assertTrue(torch.allclose(output_loaded, output))
+
+    def get_output(self, model):
+        raise NotImplementedError
+
+    def load_base_model(self):
+        raise NotImplementedError
+
+
+class TestLora8bitBnb(RegressionTester):
+    def get_output(self, model):
+        input = torch.LongTensor([[1, 0, 1, 0, 1, 2]]).to(self.torch_device)
+        with torch.inference_mode():
+            output = model(input).logits
+        return output
+
+    def load_base_model(self):
+        return AutoModelForCausalLM.from_pretrained(
+            "facebook/opt-125m",
+            load_in_8bit=True,
+        )
+
+    def test_lora_linear_8bit(self):
+        self.fix_seed()
+        base_model = self.load_base_model()
+        config = LoraConfig(
+            r=8,
+            init_lora_weights=False,
+        )
+        model = get_peft_model(base_model, config)
+        self.assert_results_equal_or_store(model, "lora_opt-125m_bnb_8bit")
+
+
+class TestLora4bitBnb(RegressionTester):
+    def get_output(self, model):
+        input = torch.LongTensor([[1, 0, 1, 0, 1, 2]]).to(self.torch_device)
+        with torch.inference_mode():
+            output = model(input).logits
+        return output
+
+    def load_base_model(self):
+        bnb_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_use_double_quant=False,
+            bnb_4bit_compute_type=torch.float32,
+        )
+        return AutoModelForCausalLM.from_pretrained(
+            "facebook/opt-125m",
+            quantization_config=bnb_config,
+            torch_dtype=torch.float32,
+        )
+
+    def test_lora_linear_4bit(self):
+        self.fix_seed()
+        base_model = self.load_base_model()
+        config = LoraConfig(
+            r=8,
+            init_lora_weights=False,
+        )
+        model = get_peft_model(base_model, config)
+        self.assert_results_equal_or_store(model, "lora_opt-125m_bnb_4bit")
