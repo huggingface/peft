453c8966bb58af8ce25d6dbe6315c1dd2bbe9d5a
blbadger <blbadger10@gmail.com>
2023-10-27 14:28:20 -0400

    debugged hook call

--- src/peft/tuners/lora/model.py
+++ src/peft/tuners/lora/model.py
@@ -381,8 +381,7 @@ class LoraModel(BaseTuner):
             if getattr(self.model, "quantization_method", None) == "gptq":
                 raise ValueError("Cannot merge LORA layers when the model is gptq quantized")
 
-        # load tensors on aligned devices if required
-        model._hf_hook.pre_forward()
+        
         key_list = [key for key, _ in self.model.named_modules() if "lora" not in key]
         desc = "Unloading " + ("and merging " if merge else "") + "model"
         for key in tqdm(key_list, disable=not progressbar, desc=desc):
@@ -390,6 +389,8 @@ class LoraModel(BaseTuner):
                 parent, target, target_name = _get_submodules(self.model, key)
             except AttributeError:
                 continue
+            # load tensors on aligned devices if required
+            self.model._hf_hook.pre_forward(target)
             if isinstance(target, LoraLayer):
                 if isinstance(target, nn.Embedding):
                     new_module = torch.nn.Embedding(target.in_features, target.out_features)
