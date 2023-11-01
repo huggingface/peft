4fbdc766e2395bdf3ffd9e8a7ed9953d8137a74b
Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
2023-11-01 10:53:13 +0100

    Update log_reports.py

--- scripts/log_reports.py
+++ scripts/log_reports.py
@@ -15,6 +15,7 @@ for log in Path().glob("*.log"):
     with open(log, "r") as f:
         for i, line in enumerate(f):
             line = json.loads(line)
+            empty_file = False
             if line.get("nodeid", "") != "":
                 test = line["nodeid"]
                 if line.get("duration", None) is not None:
@@ -25,8 +26,6 @@ for log in Path().glob("*.log"):
                         total_num_failed += 1
                     else:
                         passed.append([test, duration, log.name.split('_')[0]])
-        if i == 0:
-            empty_file = True
     group_info.append([str(log), section_num_failed, failed])
     os.remove(log)
     failed = []
