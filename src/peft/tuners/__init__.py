279a6fc3c98488d1437ccd3e3ebe10ab0b83a9f7
Yixiao Li, Macbook Air <yxli2123@gmail.com>
2023-11-08 17:53:46 -0500

    add LoftQModel and LoftQConfig

--- src/peft/tuners/__init__.py
+++ src/peft/tuners/__init__.py
@@ -27,6 +27,7 @@ from .p_tuning import PromptEncoder, PromptEncoderConfig, PromptEncoderReparamet
 from .prefix_tuning import PrefixEncoder, PrefixTuningConfig
 from .prompt_tuning import PromptEmbedding, PromptTuningConfig, PromptTuningInit
 from .multitask_prompt_tuning import MultitaskPromptEmbedding, MultitaskPromptTuningConfig, MultitaskPromptTuningInit
+from .loftq import LoftQConfig, LoftQModel
 
 # Mapping of tuners that support direct plugging
 TUNERS_MAPPING = {
