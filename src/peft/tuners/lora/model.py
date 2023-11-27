ca04f3acbe1e83a9e11be4f2a5bd66c71a7007f2
blbadger <blbadger10@gmail.com>
2023-11-27 13:24:31 -0500

    removed traces and tp post forwards

--- src/peft/tuners/lora/model.py
+++ src/peft/tuners/lora/model.py
@@ -395,15 +395,8 @@ class LoraModel(BaseTuner):
                     parent._hf_hook.pre_forward(parent)
                 if hasattr(target, "_hf_hook") and isinstance(target._hf_hook, AlignDevicesHook):
                     target._hf_hook.pre_forward(target)
-
                 self._replace_module(parent, target_name, target.get_base_layer(), target)
-                print ([i for i in parent.parameters()])
-                print ([i for i in target.parameters()])
 
-                if hasattr(parent, "_hf_hook") and isinstance(parent._hf_hook, AlignDevicesHook):
-                    parent._hf_hook.pre_forward(parent)
-                if hasattr(target, "_hf_hook") and isinstance(target._hf_hook, AlignDevicesHook):
-                    target._hf_hook.pre_forward(target)
             elif isinstance(target, ModulesToSaveWrapper):
                 # save any additional trainable modules part of `modules_to_save`
                 setattr(parent, target_name, target.modules_to_save[target.active_adapter])
