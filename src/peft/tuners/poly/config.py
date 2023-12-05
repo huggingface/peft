8d3843c28e7aced547ff37874792ccd201358aeb
洛和 <suntao.sun@antgroup.com>
2023-12-05 10:44:46 +0800

    refine config

--- src/peft/tuners/poly/config.py
+++ src/peft/tuners/poly/config.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import List, Optional, Union
+from typing import List, Optional, Union, Literal
 
 from peft.config import PeftConfig
 from peft.utils import PeftType
@@ -7,6 +7,25 @@ from peft.utils import PeftType
 
 @dataclass
 class PolyConfig(PeftConfig):
+    """
+    This is the configuration class to store the configuration of a [`PolyModel`].
+        - [Polytropon (Poly)](https://arxiv.org/abs/2202.13914)
+        - [Multi-Head Routing (MHR)](https://arxiv.org/abs/2211.03831)
+
+    Args:
+        r (`int`): Attention dimension of each Lora in Poly.
+        target_modules (`Union[List[str],str]`): The names of the modules to apply Poly to.
+        modules_to_save (`List[str]`): List of modules apart from Poly layers to be set as trainable
+            and saved in the final checkpoint.
+        init_weights (bool): Whether to perform initialization of Poly weights.
+        poly_type (`Literal["poly"]`): The variant of the Poly module to use. Currently, only "poly"
+            is supported.
+        n_tasks (`int`): The number of tasks in a multitasking scenario.
+        n_skills (`int`): The number of skills (LoRA) in each Poly layer.
+        n_splits (`int`): The number of splits within each LoRA of a Poly layer. A value greater
+            than 1 indicates the use of Multi-Head Routing (MHR).
+    """
+
     r: int = field(default=8, metadata={"help": "Lora attention dimension"})
     target_modules: Optional[Union[List[str], str]] = field(
         default=None,
@@ -32,22 +51,21 @@ class PolyConfig(PeftConfig):
             ),
         },
     )
-
-    poly_type: str = field(
+    poly_type: Literal["poly"] = field(
         default="poly",
-        metadata={"help": "Type of Poly module to be used."},
+        metadata={"help": 'Type of Poly modules to be used. Currently only "poly" is supported.'},
     )
     n_tasks: int = field(
-        default=None,
+        default=1,
         metadata={"help": "Number of tasks in multitasking scenario."},
     )
     n_skills: int = field(
         default=4,
-        metadata={"help": "Number of skills (experts) in each Poly layer."},
+        metadata={"help": "Number of skills (LoRA) in each Poly layer."},
     )
     n_splits: int = field(
         default=1,
-        metadata={"help": "Number of splits (splits) in each LoRA in the Poly layer."},
+        metadata={"help": "Number of splits within each LoRA of a Poly layer."},
     )
 
     def __post_init__(self):
