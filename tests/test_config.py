225d9c0335273c48611da2dd33fa1e449a84a46a
Benjamin Bossan <BenjaminBossan@users.noreply.github.com>
2023-12-06 15:11:00 +0100

    Release: 0.7.0 (#1214)
    In preparation for the 0.7.0 release. Also remove obsolete TODO
    comments.

--- tests/test_config.py
+++ tests/test_config.py
@@ -24,7 +24,6 @@ from parameterized import parameterized
 
 from peft import (
     AdaLoraConfig,
-    # TODO: uncomment once PEFT works again with transformers
     AdaptionPromptConfig,
     IA3Config,
     LoHaConfig,
@@ -42,7 +41,6 @@ from peft import (
 PEFT_MODELS_TO_TEST = [("lewtun/tiny-random-OPTForCausalLM-delta", "v1")]
 
 ALL_CONFIG_CLASSES = (
-    # TODO: uncomment once PEFT works again with transformers
     AdaptionPromptConfig,
     AdaLoraConfig,
     IA3Config,
