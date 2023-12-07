62a632d51fb874a9d87f2ddf954f8d2f4525c8bf
Benjamin Bossan <benjamin.bossan@gmail.com>
2023-12-07 12:36:17 +0100

    Add back guards in bnb.py
    Should not be necessary but better safe than sorry.

--- src/peft/tuners/adalora/bnb.py
+++ src/peft/tuners/adalora/bnb.py
@@ -17,122 +17,128 @@ from typing import Any
 
 import torch
 
+from peft.import_utils import is_bnb_4bit_available, is_bnb_available
+
 from .layer import AdaLoraLayer
 
 
-class SVDLinear8bitLt(torch.nn.Module, AdaLoraLayer):
-    # Low-rank matrix for SVD-based adaptation
-    def __init__(
-        self,
-        base_layer: torch.nn.Module,
-        adapter_name: str,
-        r: int = 0,
-        lora_alpha: int = 1,
-        lora_dropout: float = 0.0,
-        init_lora_weights: bool = True,
-        **kwargs,
-    ) -> None:
-        super().__init__()
-        AdaLoraLayer.__init__(self, base_layer)
-        # Freezing the pre-trained weight matrix
-        self.get_base_layer().weight.requires_grad = False
+if is_bnb_available():
 
-        self.update_layer(adapter_name, r, lora_alpha, lora_dropout, init_lora_weights)
+    class SVDLinear8bitLt(torch.nn.Module, AdaLoraLayer):
+        # Low-rank matrix for SVD-based adaptation
+        def __init__(
+            self,
+            base_layer: torch.nn.Module,
+            adapter_name: str,
+            r: int = 0,
+            lora_alpha: int = 1,
+            lora_dropout: float = 0.0,
+            init_lora_weights: bool = True,
+            **kwargs,
+        ) -> None:
+            super().__init__()
+            AdaLoraLayer.__init__(self, base_layer)
+            # Freezing the pre-trained weight matrix
+            self.get_base_layer().weight.requires_grad = False
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        # note: no check for self.merged because merging is not supported (yet)
-        result = self.base_layer(x)
+            self.update_layer(adapter_name, r, lora_alpha, lora_dropout, init_lora_weights)
 
-        if self.disable_adapters:
+        def forward(self, x: torch.Tensor) -> torch.Tensor:
+            # note: no check for self.merged because merging is not supported (yet)
+            result = self.base_layer(x)
+
+            if self.disable_adapters:
+                return result
+
+            for active_adapter in self.active_adapters:
+                if active_adapter not in self.lora_A.keys():
+                    continue
+                requires_conversion = not torch.is_autocast_enabled()
+                if requires_conversion:
+                    expected_dtype = result.dtype
+                    if x.dtype != torch.float32:
+                        x = x.float()
+
+                lora_A = self.lora_A[active_adapter]
+                lora_B = self.lora_B[active_adapter]
+                lora_E = self.lora_E[active_adapter]
+                dropout = self.lora_dropout[active_adapter]
+                scaling = self.scaling[active_adapter]
+                ranknum = self.ranknum[active_adapter] + 1e-5
+
+                output = dropout(x) @ (lora_A * lora_E).T @ lora_B.T
+                if requires_conversion:
+                    output = output.to(expected_dtype)
+                output = output * scaling / ranknum
+                # inplace operation on view is forbidden for MatMul8bitLtBackward, so avoid it
+                result = result + output
             return result
 
-        for active_adapter in self.active_adapters:
-            if active_adapter not in self.lora_A.keys():
-                continue
-            requires_conversion = not torch.is_autocast_enabled()
-            if requires_conversion:
-                expected_dtype = result.dtype
-                if x.dtype != torch.float32:
-                    x = x.float()
-
-            lora_A = self.lora_A[active_adapter]
-            lora_B = self.lora_B[active_adapter]
-            lora_E = self.lora_E[active_adapter]
-            dropout = self.lora_dropout[active_adapter]
-            scaling = self.scaling[active_adapter]
-            ranknum = self.ranknum[active_adapter] + 1e-5
-
-            output = dropout(x) @ (lora_A * lora_E).T @ lora_B.T
-            if requires_conversion:
-                output = output.to(expected_dtype)
-            output = output * scaling / ranknum
-            # inplace operation on view is forbidden for MatMul8bitLtBackward, so avoid it
-            result = result + output
-        return result
-
-    def __repr__(self) -> str:
-        rep = super().__repr__()
-        return "adalora." + rep
+        def __repr__(self) -> str:
+            rep = super().__repr__()
+            return "adalora." + rep
 
 
-class SVDLinear4bit(torch.nn.Module, AdaLoraLayer):
-    # Low-rank matrix for SVD-based adaptation
-    def __init__(
-        self,
-        base_layer: torch.nn.Module,
-        adapter_name: str,
-        r: int = 0,
-        lora_alpha: int = 1,
-        lora_dropout: float = 0.0,
-        init_lora_weights: bool = True,
-        **kwargs,
-    ) -> None:
-        super().__init__()
-        AdaLoraLayer.__init__(self, base_layer)
-        # Freezing the pre-trained weight matrix
-        self.get_base_layer().weight.requires_grad = False
+if is_bnb_4bit_available():
 
-        self.update_layer(adapter_name, r, lora_alpha, lora_dropout, init_lora_weights)
+    class SVDLinear4bit(torch.nn.Module, AdaLoraLayer):
+        # Low-rank matrix for SVD-based adaptation
+        def __init__(
+            self,
+            base_layer: torch.nn.Module,
+            adapter_name: str,
+            r: int = 0,
+            lora_alpha: int = 1,
+            lora_dropout: float = 0.0,
+            init_lora_weights: bool = True,
+            **kwargs,
+        ) -> None:
+            super().__init__()
+            AdaLoraLayer.__init__(self, base_layer)
+            # Freezing the pre-trained weight matrix
+            self.get_base_layer().weight.requires_grad = False
 
-    def forward(self, x: torch.Tensor, *args: Any, **kwargs: Any) -> torch.Tensor:
-        # note: no check for self.merged because merging is not supported (yet)
-        result = self.base_layer(x, *args, **kwargs)
+            self.update_layer(adapter_name, r, lora_alpha, lora_dropout, init_lora_weights)
 
-        if self.disable_adapters:
+        def forward(self, x: torch.Tensor, *args: Any, **kwargs: Any) -> torch.Tensor:
+            # note: no check for self.merged because merging is not supported (yet)
+            result = self.base_layer(x, *args, **kwargs)
+
+            if self.disable_adapters:
+                return result
+
+            # As per Tim Dettmers, for 4bit, we need to defensively clone here.
+            # The reason is that in some cases, an error can occur that backprop
+            # does not work on a manipulated view. This issue may be solved with
+            # newer PyTorch versions but this would need extensive testing to be
+            # sure.
+            result = result.clone()
+
+            for active_adapter in self.active_adapters:
+                if active_adapter not in self.lora_A.keys():
+                    continue
+
+                lora_A = self.lora_A[active_adapter]
+                lora_B = self.lora_B[active_adapter]
+                lora_E = self.lora_E[active_adapter]
+                dropout = self.lora_dropout[active_adapter]
+                scaling = self.scaling[active_adapter]
+                ranknum = self.ranknum[active_adapter] + 1e-5
+
+                requires_conversion = not torch.is_autocast_enabled()
+                if requires_conversion:
+                    expected_dtype = result.dtype
+                    compute_dtype = lora_A.dtype
+                    if x.dtype != compute_dtype:
+                        x = x.to(compute_dtype)
+
+                output = dropout(x) @ (lora_A * lora_E).T @ lora_B.T
+                if requires_conversion:
+                    output = output.to(expected_dtype)
+                output = output * scaling / ranknum
+                result += output
             return result
 
-        # As per Tim Dettmers, for 4bit, we need to defensively clone here.
-        # The reason is that in some cases, an error can occur that backprop
-        # does not work on a manipulated view. This issue may be solved with
-        # newer PyTorch versions but this would need extensive testing to be
-        # sure.
-        result = result.clone()
-
-        for active_adapter in self.active_adapters:
-            if active_adapter not in self.lora_A.keys():
-                continue
-
-            lora_A = self.lora_A[active_adapter]
-            lora_B = self.lora_B[active_adapter]
-            lora_E = self.lora_E[active_adapter]
-            dropout = self.lora_dropout[active_adapter]
-            scaling = self.scaling[active_adapter]
-            ranknum = self.ranknum[active_adapter] + 1e-5
-
-            requires_conversion = not torch.is_autocast_enabled()
-            if requires_conversion:
-                expected_dtype = result.dtype
-                compute_dtype = lora_A.dtype
-                if x.dtype != compute_dtype:
-                    x = x.to(compute_dtype)
-
-            output = dropout(x) @ (lora_A * lora_E).T @ lora_B.T
-            if requires_conversion:
-                output = output.to(expected_dtype)
-            output = output * scaling / ranknum
-            result += output
-        return result
-
-    def __repr__(self) -> str:
-        rep = super().__repr__()
-        return "adalora." + rep
+        def __repr__(self) -> str:
+            rep = super().__repr__()
+            return "adalora." + rep
--- src/peft/tuners/ia3/bnb.py
+++ src/peft/tuners/ia3/bnb.py
@@ -17,106 +17,112 @@ from typing import Any
 
 import torch
 
+from peft.import_utils import is_bnb_4bit_available, is_bnb_available
+
 from .layer import IA3Layer
 
 
-class Linear8bitLt(torch.nn.Module, IA3Layer):
-    # (IA)^3 implemented in a dense layer
-    def __init__(
-        self,
-        base_layer: torch.nn.Module,
-        adapter_name: str,
-        is_feedforward: bool,
-        init_ia3_weights: bool = True,
-        **kwargs,
-    ) -> None:
-        super().__init__()
-        IA3Layer.__init__(self, base_layer, is_feedforward=is_feedforward)
+if is_bnb_available():
 
-        # Freezing the pre-trained weight matrix
-        self.get_base_layer().weight.requires_grad = False
-        self.update_layer(adapter_name, init_ia3_weights)
+    class Linear8bitLt(torch.nn.Module, IA3Layer):
+        # (IA)^3 implemented in a dense layer
+        def __init__(
+            self,
+            base_layer: torch.nn.Module,
+            adapter_name: str,
+            is_feedforward: bool,
+            init_ia3_weights: bool = True,
+            **kwargs,
+        ) -> None:
+            super().__init__()
+            IA3Layer.__init__(self, base_layer, is_feedforward=is_feedforward)
 
-    def forward(self, x: torch.Tensor, *args: Any, **kwargs: Any) -> torch.Tensor:
-        # note: no check for self.merged because merging is not supported (yet)
-        if self.disable_adapters:
-            return self.base_layer(x)
+            # Freezing the pre-trained weight matrix
+            self.get_base_layer().weight.requires_grad = False
+            self.update_layer(adapter_name, init_ia3_weights)
 
-        ia3_scaling = 1
-        for active_adapter in self.active_adapters:
-            if active_adapter not in self.ia3_l.keys():
-                continue
-            ia3_scaling *= self.ia3_l[active_adapter].flatten()
+        def forward(self, x: torch.Tensor, *args: Any, **kwargs: Any) -> torch.Tensor:
+            # note: no check for self.merged because merging is not supported (yet)
+            if self.disable_adapters:
+                return self.base_layer(x)
 
-        requires_conversion = (not torch.is_autocast_enabled()) and (x.dtype != torch.float32)
-        if requires_conversion:
-            x = x.float()
-        if self.is_feedforward:
-            result = self.base_layer(x * ia3_scaling)
-            expected_dtype = result.dtype
-        else:
-            result = self.base_layer(x)
-            expected_dtype = result.dtype
-            result = result * ia3_scaling
+            ia3_scaling = 1
+            for active_adapter in self.active_adapters:
+                if active_adapter not in self.ia3_l.keys():
+                    continue
+                ia3_scaling *= self.ia3_l[active_adapter].flatten()
 
-        if requires_conversion:
-            result = result.to(expected_dtype)
+            requires_conversion = (not torch.is_autocast_enabled()) and (x.dtype != torch.float32)
+            if requires_conversion:
+                x = x.float()
+            if self.is_feedforward:
+                result = self.base_layer(x * ia3_scaling)
+                expected_dtype = result.dtype
+            else:
+                result = self.base_layer(x)
+                expected_dtype = result.dtype
+                result = result * ia3_scaling
 
-        return result
+            if requires_conversion:
+                result = result.to(expected_dtype)
 
-    def __repr__(self) -> str:
-        rep = super().__repr__()
-        return "ia3." + rep
+            return result
+
+        def __repr__(self) -> str:
+            rep = super().__repr__()
+            return "ia3." + rep
 
 
-class Linear4bit(torch.nn.Module, IA3Layer):
-    # IA3 implemented in a dense layer
-    def __init__(
-        self,
-        base_layer: torch.nn.Module,
-        adapter_name: str,
-        is_feedforward: bool,
-        init_ia3_weights: bool = True,
-        **kwargs,
-    ) -> None:
-        super().__init__()
-        IA3Layer.__init__(self, base_layer, is_feedforward=is_feedforward)
+if is_bnb_4bit_available():
 
-        # Freezing the pre-trained weight matrix
-        self.get_base_layer().weight.requires_grad = False
-        self.update_layer(adapter_name, init_ia3_weights)
+    class Linear4bit(torch.nn.Module, IA3Layer):
+        # IA3 implemented in a dense layer
+        def __init__(
+            self,
+            base_layer: torch.nn.Module,
+            adapter_name: str,
+            is_feedforward: bool,
+            init_ia3_weights: bool = True,
+            **kwargs,
+        ) -> None:
+            super().__init__()
+            IA3Layer.__init__(self, base_layer, is_feedforward=is_feedforward)
 
-    def forward(self, x: torch.Tensor, *args: Any, **kwargs: Any) -> torch.Tensor:
-        # note: no check for self.merged because merging is not supported (yet)
-        if self.disable_adapters:
-            return self.base_layer(x)
+            # Freezing the pre-trained weight matrix
+            self.get_base_layer().weight.requires_grad = False
+            self.update_layer(adapter_name, init_ia3_weights)
 
-        ia3_scaling = 1
-        for active_adapter in self.active_adapters:
-            if active_adapter not in self.ia3_l.keys():
-                continue
-            ia3_scaling *= self.ia3_l[active_adapter].flatten()
+        def forward(self, x: torch.Tensor, *args: Any, **kwargs: Any) -> torch.Tensor:
+            # note: no check for self.merged because merging is not supported (yet)
+            if self.disable_adapters:
+                return self.base_layer(x)
 
-        requires_conversion = (not torch.is_autocast_enabled()) and (x.dtype != torch.float32)
-        if requires_conversion:
-            x = x.float()
-        if self.is_feedforward:
-            result = self.base_layer(x * ia3_scaling)
-            expected_dtype = result.dtype
-        else:
-            result = self.base_layer(x)
-            expected_dtype = result.dtype
-            result = result * ia3_scaling
+            ia3_scaling = 1
+            for active_adapter in self.active_adapters:
+                if active_adapter not in self.ia3_l.keys():
+                    continue
+                ia3_scaling *= self.ia3_l[active_adapter].flatten()
 
-        result = result.clone()
-        # adalora.py and lora.py both suggest that this is necessary for 4-bit training on older versions of Pytorch.
-        # This has been duplicated here.
+            requires_conversion = (not torch.is_autocast_enabled()) and (x.dtype != torch.float32)
+            if requires_conversion:
+                x = x.float()
+            if self.is_feedforward:
+                result = self.base_layer(x * ia3_scaling)
+                expected_dtype = result.dtype
+            else:
+                result = self.base_layer(x)
+                expected_dtype = result.dtype
+                result = result * ia3_scaling
 
-        if requires_conversion:
-            result = result.to(expected_dtype)
+            result = result.clone()
+            # adalora.py and lora.py both suggest that this is necessary for 4-bit training on older versions of Pytorch.
+            # This has been duplicated here.
 
-        return result
+            if requires_conversion:
+                result = result.to(expected_dtype)
 
-    def __repr__(self) -> str:
-        rep = super().__repr__()
-        return "ia3." + rep
+            return result
+
+        def __repr__(self) -> str:
+            rep = super().__repr__()
+            return "ia3." + rep
