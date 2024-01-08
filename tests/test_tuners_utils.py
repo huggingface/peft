60c6bd80007269078c2fdcc1792007294baacac4
Benjamin Bossan <benjamin.bossan@gmail.com>
2024-01-08 17:12:52 +0100

    ENH Add attribute to show targeted module names
    This is just a tiny convenience feature to help users understand
    which modules are being targeted by the adapter. This can be useful
    to quickly check if a complex regex works for `target_modules`.
    
    Note: This should work for all adapters that use BaseTuner, so not only
    LoRA but also IA³, LoHa, etc. Only the first two were tested but that
    should be enough.

--- tests/test_tuners_utils.py
+++ tests/test_tuners_utils.py
@@ -17,7 +17,8 @@
 import unittest
 
 from parameterized import parameterized
-from transformers import AutoModel
+from torch import nn
+from transformers import AutoModel, AutoModelForCausalLM
 
 from peft import IA3Config, LoraConfig, get_peft_model
 from peft.tuners.tuners_utils import check_target_module_exists, inspect_matched_modules
@@ -175,3 +176,59 @@ class PeftCustomKwargsTester(unittest.TestCase):
                 self.assertTrue(module.is_feedforward)
             else:  # other IA3 modules should not be marked as feedforward
                 self.assertFalse(module.is_feedforward)
+
+
+class MLP(nn.Module):
+    def __init__(self, bias=True):
+        super().__init__()
+        self.lin0 = nn.Linear(10, 20, bias=bias)
+        self.relu = nn.ReLU()
+        self.drop = nn.Dropout(0.5)
+        self.lin1 = nn.Linear(20, 2, bias=bias)
+        self.sm = nn.LogSoftmax(dim=-1)
+
+
+class TestTargetedModuleNames(unittest.TestCase):
+    """Check that the attribute targeted_module_names is correctly set.
+
+    This checks LoRA and IA³, but this should be sufficient, testing all other tuners is not necessary.
+    """
+
+    def test_one_targeted_module_regex(self):
+        model = MLP()
+        model = get_peft_model(model, LoraConfig(target_modules="lin0"))
+        self.assertEqual(model.targeted_module_names, ["lin0"])
+
+    def test_two_targeted_module_regex(self):
+        model = MLP()
+        model = get_peft_model(model, LoraConfig(target_modules="lin.*"))
+        self.assertEqual(model.targeted_module_names, ["lin0", "lin1"])
+
+    def test_one_targeted_module_list(self):
+        model = MLP()
+        model = get_peft_model(model, LoraConfig(target_modules=["lin0"]))
+        self.assertEqual(model.targeted_module_names, ["lin0"])
+
+    def test_two_targeted_module_list(self):
+        model = MLP()
+        model = get_peft_model(model, LoraConfig(target_modules=["lin0", "lin1"]))
+        self.assertEqual(model.targeted_module_names, ["lin0", "lin1"])
+
+    def test_ia3_targeted_module_regex(self):
+        model = MLP()
+        model = get_peft_model(model, IA3Config(target_modules=".*lin.*", feedforward_modules=".*lin.*"))
+        self.assertEqual(model.targeted_module_names, ["lin0", "lin1"])
+
+    def test_ia3_targeted_module_list(self):
+        model = MLP()
+        model = get_peft_model(model, IA3Config(target_modules=["lin0", "lin1"], feedforward_modules=["lin0", "lin1"]))
+        self.assertEqual(model.targeted_module_names, ["lin0", "lin1"])
+
+    def test_realistic_example(self):
+        model = AutoModelForCausalLM.from_pretrained("hf-internal-testing/tiny-random-BloomForCausalLM")
+        config = LoraConfig(task_type="CAUSAL_LM")
+        model = get_peft_model(model, config)
+        expected = [
+            f"transformer.h.{i}.self_attention.query_key_value" for i in range(len(model.base_model.transformer.h))
+        ]
+        self.assertEqual(model.targeted_module_names, expected)
