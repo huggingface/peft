82fe28152344a795dea367b93b713d8b4f338521
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2024-01-17 12:56:22 +0530

    fixes

--- src/peft/utils/merge_utils.py
+++ src/peft/utils/merge_utils.py
@@ -28,10 +28,10 @@ def magnitude_based_pruning(tensor: torch.Tensor, density: float) -> torch.Tenso
     density (`float`):The fraction of values to preserve. Should be in [0,1].
     """
     mask = torch.zeros_like(tensor).reshape(-1)
-    k = int(density * tensor.view(-1).shape[0])
-    top_k = torch.topk(tensor.abs().view(-1), k=k, largest=True)
+    k = int(density * tensor.reshape(-1).shape[0])
+    top_k = torch.topk(tensor.abs().reshape(-1), k=k, largest=True)
     mask[top_k[1]] = 1
-    return tensor * mask.view(tensor.shape)
+    return tensor * mask.reshape(tensor.shape)
 
 
 def random_pruning(tensor: torch.Tensor, density: float, rescale: bool) -> torch.Tensor:
