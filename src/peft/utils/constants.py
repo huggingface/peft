8044e581c3ce80501b153418eee61a271a143bbe
Arnav Garg <106701836+arnavgarg1@users.noreply.github.com>
2024-01-24 13:24:38 -0500

    Fix LoRA module mapping for Phi models (#1375)

--- src/peft/utils/constants.py
+++ src/peft/utils/constants.py
@@ -76,7 +76,7 @@ TRANSFORMERS_MODELS_TO_LORA_TARGET_MODULES_MAPPING = {
     "mistral": ["q_proj", "v_proj"],
     "mixtral": ["q_proj", "v_proj"],
     "stablelm": ["q_proj", "v_proj"],
-    "phi": ["Wqkv", "out_proj", "fc1", "fc2"],
+    "phi": ["q_proj", "v_proj", "fc1", "fc2"],
 }
 
 TRANSFORMERS_MODELS_TO_IA3_TARGET_MODULES_MAPPING = {
