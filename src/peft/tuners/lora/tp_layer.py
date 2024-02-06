6ff43c066ab6e163cf8445106ec027cc0b7027af
younesbelkada <younesbelkada@gmail.com>
2024-02-06 18:15:55 +0000

    add todo

--- src/peft/tuners/lora/tp_layer.py
+++ src/peft/tuners/lora/tp_layer.py
@@ -83,6 +83,7 @@ class LoraParallelLinear(nn.Module, LoraLayer):
 
     @property
     def is_paralle_a(self):
+        # TODO: remove it in PEFT 0.10.0
         # See https://github.com/huggingface/peft/pull/1439 for more details
         warnings.warn(
             "`is_paralle_a` is going to be deprecated in a future release. Please use `is_parallel_a`", FutureWarning
