6785380b540db82df8dd43d15f2061b7bd122644
Aarni Koskela <akx@iki.fi>
2024-02-07 09:13:25 +0200

    Noqa `raise OSError()` in `auto.py`

--- src/peft/auto.py
+++ src/peft/auto.py
@@ -49,7 +49,7 @@ class _BaseAutoPeftModel:
 
     def __init__(self, *args, **kwargs):
         # For consistency with transformers: https://github.com/huggingface/transformers/blob/91d7df58b6537d385e90578dac40204cb550f706/src/transformers/models/auto/auto_factory.py#L400
-        raise OSError(
+        raise EnvironmentError(  # noqa: UP024
             f"{self.__class__.__name__} is designed to be instantiated "
             f"using the `{self.__class__.__name__}.from_pretrained(pretrained_model_name_or_path)` or "
             f"`{self.__class__.__name__}.from_config(config)` methods."
