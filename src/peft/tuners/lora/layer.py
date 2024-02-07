Fix bug with incorrectly set gradient

src/peft/tuners/lora/layer.py
@@ -905,15 +905,17 @@ class MultiheadAttention(nn.Module, LoraLayer):
         # in_proj
         # TODO work with separate weights
         base_layer = self.get_base_layer()
-        weight = base_layer.in_proj_weight.data
+        weight = base_layer.in_proj_weight
         del base_layer.in_proj_weight
-        base_layer.register_parameter("in_proj_weight", nn.Parameter(weight))
+        base_layer.register_parameter("in_proj_weight", nn.Parameter(weight.data))
+        base_layer.in_proj_weight.requires_grad = weight.requires_grad
 
         # out_proj
         base_layer = base_layer.out_proj.get_base_layer()
-        weight = base_layer.weight.data
+        weight = base_layer.weight
         del base_layer.weight
-        base_layer.register_parameter("weight", nn.Parameter(weight))
+        base_layer.register_parameter("weight", nn.Parameter(weight.data))
+        base_layer.weight.requires_grad = weight.requires_grad
 
     def state_dict(self, *args, **kwargs):
         self._restore_weights()