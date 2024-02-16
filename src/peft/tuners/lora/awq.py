2f93c823a0900f7faa4dd774fa94eecc47896879
younesbelkada <younesbelkada@gmail.com>
2024-02-16 01:10:27 +0000

    raise error only at the dispatch level

--- src/peft/tuners/lora/awq.py
+++ src/peft/tuners/lora/awq.py
@@ -11,9 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import importlib.metadata as importlib_metadata
 from typing import Any, Optional
 
+import packaging.version
 import torch
 
 from peft.import_utils import is_auto_awq_available
@@ -91,6 +92,16 @@ def dispatch_awq(
         target_base_layer = target
 
     if is_auto_awq_available() and isinstance(target_base_layer, WQLinear_GEMM):
+        # Raise the error only at the dispatch level
+        AUTOAWQ_MINIMUM_VERSION = packaging.version.parse("0.2.0")
+        version_autoawq = packaging.version.parse(importlib_metadata.version("autoawq"))
+
+        if AUTOAWQ_MINIMUM_VERSION > version_autoawq:
+            raise ImportError(
+                f"Found an incompatible version of auto-awq. Found version {version_autoawq}, "
+                f"but only versions above {AUTOAWQ_MINIMUM_VERSION} are supported for PEFT."
+            )
+
         new_module = AwqLoraLinear(target, adapter_name, **kwargs)
         target.qweight = target_base_layer.qweight
 
