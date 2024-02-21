c6df82c5e250b67f08dc7ce2e8d807c853e54a05
EricLBuehler <ericlbuehler@gmail.com>
2024-02-21 12:54:51 -0500

    Update with EricLBuehler/xlora#20

--- src/peft/tuners/xlora/model.py
+++ src/peft/tuners/xlora/model.py
@@ -104,8 +104,8 @@ class xLoRAModel(LoraModel):
 
                 model_peft.internal_xlora_scalings = torch.full(  # type: ignore
                     (payload.batch_size, payload.seq_len, xlora_classifier.n_layers, xlora_classifier.n_classes),
-                    payload.override_scaling_pass_value,  # requires_grad=True
-                )  # TODO(EricLBuehler): is the requires_grad=True necessary?
+                    payload.override_scaling_pass_value,
+                )
 
                 return
 
