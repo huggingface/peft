b599bfedb793cb47fb73eceaeb1f38586cf5bd57
sayakpaul <spsayakpaul@gmail.com>
2024-03-12 17:14:44 +0530

    documentation

--- src/peft/tuners/lora/config.py
+++ src/peft/tuners/lora/config.py
@@ -105,7 +105,7 @@ class LoraConfig(PeftConfig):
             Enable 'Weight-Decomposed Low-Rank Adaptation' (DoRA). This technique decomposes the updates of the weights
             into two parts, magnitude and direction. Direction is handled by normal LoRA, whereas the magnitude is
             handled by a separate learnable parameter. This can improve the performance of LoRA, especially at low
-            ranks. Right now, DoRA only supports non-quantized linear layers. DoRA introduces a bigger overhead than
+            ranks. Right now, DoRA only supports non-quantized linear and Conv2D layers. DoRA introduces a bigger overhead than
             pure LoRA, so it is recommended to merge weights for inference. For more information, see
             https://arxiv.org/abs/2402.09353.
     """
@@ -238,7 +238,7 @@ class LoraConfig(PeftConfig):
                 "Enable 'Weight-Decomposed Low-Rank Adaptation' (DoRA). This technique decomposes the updates of the "
                 "weights into two parts, magnitude and direction. Direction is handled by normal LoRA, whereas the "
                 "magnitude is handled by a separate learnable parameter. This can improve the performance of LoRA, "
-                "especially at low ranks. Right now, DoRA only supports non-quantized linear layers. DoRA introduces "
+                "especially at low ranks. Right now, DoRA only supports non-quantized linear and Conv2D layers. DoRA introduces "
                 "a bigger overhead than pure LoRA, so it is recommended to merge weights for inference. For more "
                 "information, see  https://arxiv.org/abs/2402.09353."
             )
