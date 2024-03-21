209abd2a5b0d7b97d3a725a43da84a6c1dd5f092
Benjamin Bossan <benjamin.bossan@gmail.com>
2024-03-21 12:01:56 +0100

    Skip failing Deberta + Vera tests
    Same as for LoRA and IA3, these Deberta tests fail for some reason.

--- tests/test_feature_extraction_models.py
+++ tests/test_feature_extraction_models.py
@@ -45,8 +45,11 @@ def skip_non_prompt_tuning(test_list):
 def skip_deberta_lora_tests(test_list):
     r"""
     Skip tests that are checkpointing with lora/ia3 tests for Deberta models (couldn't find much info on the error)
+
+    Besides LoRA, also skip IA3 and VeRA.
     """
-    return [test for test in test_list if not (any(k in test[0] for k in ["lora", "ia3"]) and "Deberta" in test[0])]
+    to_skip = {"lora", "ia3", "vera"}
+    return [test for test in test_list if not (any(k in test[0] for k in to_skip) and "Deberta" in test[0])]
 
 
 def skip_deberta_pt_tests(test_list):
