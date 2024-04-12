677c64e792922ed58aa0e7e42ddba061ac382fa1
EricLBuehler <ericlbuehler@gmail.com>
2024-04-11 22:46:47 -0400

    Fix passing arg

--- src/peft/peft_model.py
+++ src/peft/peft_model.py
@@ -135,7 +135,7 @@ class PeftModel(PushToHubMixin, torch.nn.Module):
             cls = PEFT_TYPE_TO_MODEL_MAPPING[peft_config.peft_type]
             self.base_model = cls(model, {adapter_name: peft_config}, adapter_name)
             if isinstance(self.base_model, XLoraModel):
-                self.base_model.post_init_lora(model, peft_config, adapter_name, self)
+                self.base_model.post_init_lora(model, peft_config, self)
             self.set_additional_trainable_modules(peft_config, adapter_name)
 
         if getattr(model, "is_gradient_checkpointing", True):
