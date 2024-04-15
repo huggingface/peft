0e15216c314f61075e56106249d7eef43963d956
fxmeng <filtergrafting@gmail.com>
2024-04-15 10:21:19 +0800

    add pissa_utils

--- src/peft/utils/pissa_utils.py
+++ src/peft/utils/pissa_utils.py
@@ -21,12 +21,10 @@ import os
 import torch
 from safetensors import safe_open
 from safetensors.torch import save_file
-from peft import PeftModel
-from transformers import AutoTokenizer
 import json
 
 
-def pissa_pre_training_saving(peft_model: PeftModel, tokenizer: AutoTokenizer, save_path: str, push_to_hub_user: str = None):
+def pissa_pre_training_saving(peft_model, tokenizer, save_path, push_to_hub_user = None):
     # No need for SVD when we load the PiSSA and residual model saved locally.
     peft_model.peft_config['default'].init_lora_weights = True
     # Save PiSSA adapter.
