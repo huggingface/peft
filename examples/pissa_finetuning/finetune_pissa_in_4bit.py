db99cb357c4d186e5c60b370e45acfd5f37bb294
fxmeng <filtergrafting@gmail.com>
2024-04-15 10:46:48 +0800

    add pissa_utils

--- examples/pissa_finetuning/finetune_pissa_in_4bit.py
+++ examples/pissa_finetuning/finetune_pissa_in_4bit.py
@@ -3,19 +3,19 @@ from peft import LoraConfig, get_peft_model, PeftModel, prepare_model_for_kbit_t
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 from peft.utils.pissa_utils import pissa_pre_training_saving, pissa_post_training_saving
 
-if False:
-    # Download and load the llama-2-7b residual model in 4-bit format:
-    quantization_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_compute_dtype=torch.bfloat16,
-    )
-    res_model = AutoModelForCausalLM.from_pretrained("fxmeng/pissa-llama-2-7b-r16-alpha-16", quantization_config=quantization_config, low_cpu_mem_usage=True)
-    tokenizer = AutoTokenizer.from_pretrained('fxmeng/pissa-llama-2-7b-r16-alpha-16')
+quantization_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=torch.bfloat16,
+)
 
+DIY = True
+
+if not DIY:
+    # Download and load the llama-2-7b residual model in 4-bit format:
+    model_path = "fxmeng/pissa-llama-2-7b-r16-alpha-16"
 else:
-    #Or DIY one:
     res_model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf", torch_dtype=torch.float16, device_map='auto')
     tokenizer = AutoTokenizer.from_pretrained('meta-llama/Llama-2-7b-hf')
     tokenizer.pad_token_id = tokenizer.eos_token_id
@@ -23,11 +23,18 @@ else:
     model = get_peft_model(res_model, lora_config)
     model.print_trainable_parameters()
     pissa_pre_training_saving(res_model, tokenizer, save_path = "pissa-llama-2-7b-r16-alpha-16", push_to_hub = None)
-
+    model_path = "pissa-llama-2-7b-r16-alpha-16"
+    
+        
+res_model = AutoModelForCausalLM.from_pretrained(model_path, quantization_config=quantization_config, low_cpu_mem_usage=True)
+tokenizer = AutoTokenizer.from_pretrained(model_path)
 # Wrapping the residual model with PiSSA:
-peft_model = PeftModel.from_pretrained(res_model, "fxmeng/pissa-llama-2-7b-r16-alpha-16", subfolder="pissa_init", is_trainable=True)
+peft_model = PeftModel.from_pretrained(res_model, model_path, subfolder="pissa_init", is_trainable=True)
 peft_model.print_trainable_parameters()
 peft_model = prepare_model_for_kbit_training(peft_model)
+
+
+
 
 # Training PiSSA with trl on imdb dataset (using a subset for fast evaluation):
 from trl import SFTTrainer
