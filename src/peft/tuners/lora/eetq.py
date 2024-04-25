55347756fa5abc106d1d72c2b0f346a4e73e1850
Younes Belkada <younesbelkada@gmail.com>
2024-04-25 16:13:08 +0200

    indent

--- src/peft/tuners/lora/eetq.py
+++ src/peft/tuners/lora/eetq.py
@@ -23,64 +23,63 @@ from peft.tuners.tuners_utils import BaseTunerLayer
 if is_eetq_available():
     from eetq import EetqLinear
 
+    class EetqLoraLinear(torch.nn.Module, LoraLayer):
+        def __init__(
+            self,
+            base_layer,
+            adapter_name,
+            r: int = 0,
+            lora_alpha: int = 1,
+            lora_dropout: float = 0.0,
+            init_lora_weights: bool = True,
+            use_rslora: bool = False,
+            **kwargs,
+        ):
+            super().__init__()
+            LoraLayer.__init__(self, base_layer)
 
-class EetqLoraLinear(torch.nn.Module, LoraLayer):
-    def __init__(
-        self,
-        base_layer,
-        adapter_name,
-        r: int = 0,
-        lora_alpha: int = 1,
-        lora_dropout: float = 0.0,
-        init_lora_weights: bool = True,
-        use_rslora: bool = False,
-        **kwargs,
-    ):
-        super().__init__()
-        LoraLayer.__init__(self, base_layer)
+            # self.base_layer and self.quant_linear_module are the same; we need the former for consistency and the latter
+            # for backwards compatibility
+            self.quant_linear_module = base_layer
 
-        # self.base_layer and self.quant_linear_module are the same; we need the former for consistency and the latter
-        # for backwards compatibility
-        self.quant_linear_module = base_layer
+            self._active_adapter = adapter_name
+            self.update_layer(adapter_name, r, lora_alpha, lora_dropout, init_lora_weights, use_rslora)
 
-        self._active_adapter = adapter_name
-        self.update_layer(adapter_name, r, lora_alpha, lora_dropout, init_lora_weights, use_rslora)
+        def forward(self, x: torch.Tensor):
+            result = self.quant_linear_module(x)
 
-    def forward(self, x: torch.Tensor):
-        result = self.quant_linear_module(x)
+            if self.disable_adapters:
+                return result
 
-        if self.disable_adapters:
+            for active_adapter in self.active_adapters:
+                if active_adapter not in self.lora_A.keys():
+                    continue
+                lora_A = self.lora_A[active_adapter]
+                lora_B = self.lora_B[active_adapter]
+                dropout = self.lora_dropout[active_adapter]
+                scaling = self.scaling[active_adapter]
+
+                requires_conversion = not torch.is_autocast_enabled()
+                if requires_conversion:
+                    expected_dtype = result.dtype
+                    x = x.to(lora_A.weight.dtype)
+
+                output = lora_B(lora_A(dropout(x)))
+                if requires_conversion:
+                    output = output.to(expected_dtype)
+                output = output * scaling
+                result = result + output
             return result
 
-        for active_adapter in self.active_adapters:
-            if active_adapter not in self.lora_A.keys():
-                continue
-            lora_A = self.lora_A[active_adapter]
-            lora_B = self.lora_B[active_adapter]
-            dropout = self.lora_dropout[active_adapter]
-            scaling = self.scaling[active_adapter]
+        def merge(self, safe_merge: bool = False, adapter_names: Optional[List[str]] = None) -> None:
+            raise AttributeError("Merging LoRA layers is not supported for Eetq layers.")
 
-            requires_conversion = not torch.is_autocast_enabled()
-            if requires_conversion:
-                expected_dtype = result.dtype
-                x = x.to(lora_A.weight.dtype)
+        def unmerge(self) -> None:
+            raise AttributeError("Unmerging LoRA layers is not supported for Eetq layers.")
 
-            output = lora_B(lora_A(dropout(x)))
-            if requires_conversion:
-                output = output.to(expected_dtype)
-            output = output * scaling
-            result = result + output
-        return result
-
-    def merge(self, safe_merge: bool = False, adapter_names: Optional[List[str]] = None) -> None:
-        raise AttributeError("Merging LoRA layers is not supported for Eetq layers.")
-
-    def unmerge(self) -> None:
-        raise AttributeError("Unmerging LoRA layers is not supported for Eetq layers.")
-
-    def __repr__(self) -> str:
-        rep = super().__repr__()
-        return "lora." + rep
+        def __repr__(self) -> str:
+            rep = super().__repr__()
+            return "lora." + rep
 
 
 def dispatch_eetq(
