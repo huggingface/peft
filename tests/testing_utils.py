33781ce239e40d9efcf6715d320976ed9d0bfe6d
Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
2024-04-26 10:20:18 +0200

    FEAT: Add EETQ support in PEFT (#1675)
    * v1
    
    * fix tests'
    
    * fix unneeded change
    
    * fix unneeded change
    
    * fix unneeded change
    
    * fix
    
    * fix CI
    
    * fix docker image
    
    * fix docker image
    
    * add docs
    
    * lazy import
    
    * raise when merge
    
    * raise when merge
    
    * Update eetq.py
    
    * merge
    
    * style
    
    * add unmerge
    
    * indent
    
    * Update docs/source/developer_guides/quantization.md
    
    Co-authored-by: Benjamin Bossan <BenjaminBossan@users.noreply.github.com>
    
    * add details about transformers
    
    ---------
    
    Co-authored-by: Benjamin Bossan <BenjaminBossan@users.noreply.github.com>

--- tests/testing_utils.py
+++ tests/testing_utils.py
@@ -18,7 +18,13 @@ import numpy as np
 import pytest
 import torch
 
-from peft.import_utils import is_aqlm_available, is_auto_awq_available, is_auto_gptq_available, is_optimum_available
+from peft.import_utils import (
+    is_aqlm_available,
+    is_auto_awq_available,
+    is_auto_gptq_available,
+    is_eetq_available,
+    is_optimum_available,
+)
 
 
 def require_torch_gpu(test_case):
@@ -73,6 +79,13 @@ def require_auto_awq(test_case):
     Decorator marking a test that requires auto-awq. These tests are skipped when auto-awq isn't installed.
     """
     return unittest.skipUnless(is_auto_awq_available(), "test requires auto-awq")(test_case)
+
+
+def require_eetq(test_case):
+    """
+    Decorator marking a test that requires eetq. These tests are skipped when eetq isn't installed.
+    """
+    return unittest.skipUnless(is_eetq_available(), "test requires eetq")(test_case)
 
 
 def require_optimum(test_case):
