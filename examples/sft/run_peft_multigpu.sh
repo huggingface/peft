557a7da2949296d1e4878fad9569e912d86d92ce
Hongseok Oh <97136787+abzb1@users.noreply.github.com>
2024-05-10 18:47:52 +0900

    FIX Trailing ws in revise run_peft_multigpu.sh (#1722)

--- examples/sft/run_peft_multigpu.sh
+++ examples/sft/run_peft_multigpu.sh
@@ -28,7 +28,7 @@ torchrun --nproc_per_node 8 --nnodes 1 train.py \
 --per_device_eval_batch_size 8 \
 --gradient_accumulation_steps 8 \
 --gradient_checkpointing True \
---use_reentrant False \ 
+--use_reentrant False \
 --dataset_text_field "content" \
 --use_peft_lora True \
 --lora_r 8 \
