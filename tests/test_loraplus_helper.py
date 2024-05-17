556127d7a50772af0b439555a8326daaac52b7bd
moghadas76 <s.m.moghadas2012@gmail.com>
2024-05-17 02:23:49 +0200

    Add unit test

--- tests/test_loraplus_helper.py
+++ tests/test_loraplus_helper.py
@@ -16,7 +16,6 @@ class SimpleNet(nn.Module):
         self.lin1 = nn.Linear(20, 16, bias=bias)
 
     def forward(self, X):
-        X = X.float()
         X = self.lin0(self.layer_norm(self.embedding(X)))
         X = self.relu(X)
         X = self.lin1(X)
@@ -36,3 +35,23 @@ def test_lora_plus_helper_sucess():
     optim = create_loraplus_optimizer(model=model, optimizer_cls=optimizer_cls, optimizer_kwargs=optim_config, loraplus_lr_embedding=1e-6)
     assert optim is not None
     assert len(optim.param_groups) == 4
+
+def test_lora_plus_optimizer_sucess():
+    optimizer_cls = bnb.optim.Adam8bit
+    optim_config = {
+        'lr': 5e-5,
+        'eps': 1e-6,
+        'betas': (0.9, 0.999),
+        'weight_decay': 0.0,
+        "loraplus_lr_ratio": 0.2,
+    }
+    model: SimpleNet = SimpleNet().cuda()
+    optim = create_loraplus_optimizer(model=model, optimizer_cls=optimizer_cls, optimizer_kwargs=optim_config, loraplus_lr_embedding=1e-6)
+    loss = torch.nn.CrossEntropyLoss()
+    bnb.optim.GlobalOptimManager.get_instance().register_parameters(model.parameters())
+    x = torch.randint(100, (2, 4, 10)).cuda()
+    output = model(x).permute(0, 3, 1, 2)
+    label = torch.randint(16, (2,4,10,)).cuda()
+    loss_value = loss(output, label)
+    loss_value.backward()
+    optim.step()
