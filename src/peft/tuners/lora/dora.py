692487e80f2dd6e22c97a3677ac86f2c730b8415
Benjamin Bossan <benjamin.bossan@gmail.com>
2024-05-28 15:59:50 +0200

    Create defensive copy of base layer b4 dequant

--- src/peft/tuners/lora/dora.py
+++ src/peft/tuners/lora/dora.py
@@ -1,4 +1,18 @@
-#!/usr/bin/env python3
+# Copyright 2024-present the HuggingFace Inc. team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from copy import deepcopy
 
 import torch
 from torch import nn
@@ -28,6 +42,9 @@ class DoraLinearLayer(nn.Module):
         if dtype_is_fp16:
             lora_A = lora_A.float()
             lora_B = lora_B.float()
+
+        # we have to create a copy of the base layer, otherwise, FSDP will throw an error
+        base_layer = deepcopy(base_layer)
 
         with gather_params_ctx(base_layer.parameters()):
             weight = dequantize_module_weight(base_layer)
