03d2a09e1cf2fbb0566e4e00d35209cb626fa6af
ViliamvolosV <viliamvolosv@gmail.com>
2024-06-07 23:17:30 +0300

    addd glora

---
+++ src/peft/tuners/glora/config.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass, field
+from typing import List, Optional, Union
+from peft.utils import PeftType
+from peft.config import PeftConfig
+
+
+@dataclass
+class GLoraConfig(PeftConfig):
+    """
+    This is the configuration class to store the configuration of a [`GLoraModel`].
+
+    Args:
+        r (`int`): GLora attention dimension.
+        target_modules (`Union[List[str],str]`): The names of the modules to apply Lora to.
+        bias (`str`): Bias type for Lora. Can be 'none', 'all' or 'lora_only'
+        modules_to_save (`List[str]`):List of modules apart from LoRA layers to be set as trainable
+            and saved in the final checkpoint.
+        layers_to_transform (`Union[List[int],int]`):
+            The layer indexes to transform, if this argument is specified, it will apply the LoRA transformations on
+            the layer indexes that are specified in this list. If a single integer is passed, it will apply the LoRA
+            transformations on the layer at this index.
+        layers_pattern (`str`):
+            The layer pattern name, used only if `layers_to_transform` is different from `None` and if the layer
+            pattern is not in the common layers pattern.
+    """
+
+    r: int = field(default=4, metadata={"help": "Lora attention dimension"})
+    target_modules: Optional[Union[List[str], str]] = field(
+        default=None,
+        metadata={
+            "help": "List of module names or regex expression of the module names to replace with Lora."
+            "For example, ['q', 'v'] or '.*decoder.*(SelfAttention|EncDecAttention).*(q|v)$' "
+        },
+    )
+
+    def __post_init__(self):
+        self.peft_type = PeftType.GLORA
--- src/peft/utils/save_and_load.py
+++ src/peft/utils/save_and_load.py
@@ -176,6 +176,8 @@ def get_peft_model_state_dict(
                 )
             to_return["base_model.vera_A." + adapter_name] = state_dict["base_model.vera_A." + adapter_name]
             to_return["base_model.vera_B." + adapter_name] = state_dict["base_model.vera_B." + adapter_name]
+    elif config.peft_type == PeftType.GLORA:
+        to_return = {k: state_dict[k] for k in state_dict if "glora_" in k}
 
     else:
         raise ValueError(f"Unknown PEFT type passed: {config.peft_type}")
@@ -314,6 +316,7 @@ def set_peft_model_state_dict(
         PeftType.LN_TUNING,
         PeftType.BOFT,
         PeftType.VERA,
+        PeftType.GLORA
     ):
         peft_model_state_dict = {}
         parameter_prefix = {
@@ -327,6 +330,7 @@ def set_peft_model_state_dict(
             PeftType.BOFT: "boft_",
             PeftType.LN_TUNING: "ln_tuning_",
             PeftType.VERA: "vera_lambda_",
+            PeftType.GLORA: "glora_",
         }[config.peft_type]
         for k, v in state_dict.items():
             if parameter_prefix in k:
@@ -374,6 +378,9 @@ def set_peft_model_state_dict(
             peft_model_state_dict = {renamed_dora_weights(k): v for k, v in peft_model_state_dict.items()}
 
     elif config.is_prompt_learning or config.peft_type == PeftType.ADAPTION_PROMPT:
+        peft_model_state_dict = state_dict
+        
+    elif config.peft_type == PeftType.GLORA:
         peft_model_state_dict = state_dict
     else:
         raise NotImplementedError
