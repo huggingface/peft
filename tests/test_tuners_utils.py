b41cb6defc90d94e7674713f3db4f5a1c8ec5e81
JINO-ROHIT <find.jinorohit@gmail.com>
2024-09-27 23:45:38 +0530

    remove debug logs

--- tests/test_tuners_utils.py
+++ tests/test_tuners_utils.py
@@ -366,7 +366,7 @@ class MLP(nn.Module):
         self.lin0 = nn.Linear(10, 20, bias=bias)
         self.relu = nn.ReLU()
         self.drop = nn.Dropout(0.5)
-        self.lin1 = nn.Linear(20, 10, bias=bias)
+        self.lin1 = nn.Linear(20, 2, bias=bias)
         self.sm = nn.LogSoftmax(dim=-1)
 
 
@@ -378,7 +378,6 @@ class TestTargetedModuleNames(unittest.TestCase):
 
     def test_one_targeted_module_regex(self):
         model = MLP()
-        print(model)
         model = get_peft_model(model, LoraConfig(target_modules="lin0"))
         assert model.targeted_module_names == ["lin0"]
 
