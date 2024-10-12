43f86b40522005e2c8f3e0223007f0d7f3cd83e8
jiqing-feng <jiqing.feng@intel.com>
2024-10-12 09:05:14 -0400

    AWQ model with ipex linear can finetune on CPU

--- src/peft/tuners/lora/layer.py
+++ src/peft/tuners/lora/layer.py
@@ -80,7 +80,7 @@ class LoraLayer(BaseTunerLayer):
         elif hasattr(base_layer, "codebooks") and base_layer.__class__.__name__ == "QuantizedLinear":
             # AQLM QuantLinear
             in_features, out_features = base_layer.in_features, base_layer.out_features
-        elif hasattr(base_layer, "w_bit") and base_layer.__class__.__name__ == "WQLinear_GEMM":
+        elif hasattr(base_layer, "w_bit") and base_layer.__class__.__name__ in ("WQLinear_GEMM", "WQLinear_IPEX"):
             # Awq layers
             in_features, out_features = base_layer.in_features, base_layer.out_features
         elif base_layer.__class__.__name__ == "EetqLinear":
