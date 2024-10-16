fb762cccce6bd8544565a96a166e66dc69ee2b61
Aritra Roy Gosthipaty <aritra.born2fly@gmail.com>
2024-10-16 22:48:17 +0530

    ENH Faster DoRA in when no dropout/eval mode (#2122)

--- src/peft/tuners/lora/layer.py
+++ src/peft/tuners/lora/layer.py
@@ -585,13 +585,19 @@ class Linear(nn.Module, LoraLayer):
                 if not self.use_dora[active_adapter]:
                     result = result + lora_B(lora_A(dropout(x))) * scaling
                 else:
-                    x = dropout(x)
+                    if isinstance(dropout, nn.Identity) or not self.training:
+                        base_result = result
+                    else:
+                        x = dropout(x)
+                        base_result = None
+
                     result = result + self.lora_magnitude_vector[active_adapter](
                         x,
                         lora_A=lora_A,
                         lora_B=lora_B,
                         scaling=scaling,
                         base_layer=self.get_base_layer(),
+                        base_result=base_result,
                     )
 
             result = result.to(torch_result_dtype)
