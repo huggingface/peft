957907ac80ed76651a13ea9c41f75b39e3bdf688
Benjamin Bossan <BenjaminBossan@users.noreply.github.com>
2024-10-24 16:59:47 +0200

    MNT: Enable Python 3.12 on CI (#2173)
    Python 3.8 was removed recently, adding 3.12 now.

--- setup.py
+++ setup.py
@@ -78,7 +78,10 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
 )
