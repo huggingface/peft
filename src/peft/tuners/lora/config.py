FIX: logging, normalization behaviour, bugs in example

src/peft/tuners/lora/config.py
@@ -75,18 +75,41 @@ class CordaConfig:
     This is the sub-configuration class to store the configuration of a [`LoraModel`].
 
     Args:
-        run_model (`Callable[[], None]`): Callback to run the model when building covariance.
-        cache_file (`Optional[str]`): Cache file to store the SVD result.
-        covariance_file (`Optional[str]`): Covariance file to store the covariance matrix.
-        sample_count (`int`): Divisor for each hook call.
-        corda_method (`Literal["ipm", "kpm"]`): Method to build adapter.
+        cache_file (`Optional[str]`):
+            Cache file to store the SVD result.
+        covariance_file (`Optional[str]`):
+            Covariance file to store the covariance matrix.
+        run_model (`Callable[[], None]`):
+            Callback to run the model when building covariance.
+        hooked_model (`Optional[nn.Module]`):
+            Model to hook when building covariance. If none, original model will be hooked.
+        sample_count (`int`):
+            Divisor for each hook call.
+        corda_method (`Literal["ipm", "kpm"]`):
+            Method to build adapter.
+        run_svd_for_covariance (`bool`):
+            If true, SVD decomposition will be run for covariance matrix. Currently for debugging purposes only.
     """
 
-    cache_file: Optional[str] = field(default=None)
-    covariance_file: Optional[str] = field(default=None)
-    run_model: Optional[Callable[[], None]] = field(default=None)
-    sample_count: int = field(default=256)
-    corda_method: Literal["ipm", "kpm"] = field(default="ipm")
+    cache_file: Optional[str] = field(default=None, metadata={"help": "Cache file to store the SVD result"})
+    covariance_file: Optional[str] = field(
+        default=None, metadata={"help": "Covariance file to store the covariance matrix"}
+    )
+    run_model: Optional[Callable[[], None]] = field(
+        default=None, metadata={"help": "Callback to run the model when building covariance"}
+    )
+    hooked_model: Optional[nn.Module] = field(
+        default=None,
+        metadata={"help": "Model to hook when building covariance. If none, original model will be hooked."},
+    )
+    sample_count: int = field(default=256, metadata={"help": "Divisor for each hook call"})
+    corda_method: Literal["ipm", "kpm"] = field(default="ipm", metadata={"help": "Method to build adapter"})
+    run_svd_for_covariance: bool = field(
+        default=False,
+        metadata={
+            "help": "If true, SVD decomposition will be run for covariance matrix. Currently for debugging purposes only."
+        },
+    )
 
 
 @dataclass

src/peft/tuners/lora/layer.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 from __future__ import annotations
 
+import logging
 import math
 import warnings
 from typing import Any, Optional, Union
@@ -263,7 +264,6 @@ class LoraLayer(BaseTunerLayer):
         weight = weight.to(torch.float32)
         out_dim = weight.data.size(0)
         in_dim = weight.data.size(1)
-        min_dim = min(in_dim, out_dim)
 
         # Calculate WC from covariance matrix
         assert hasattr(linear, "eigens")
@@ -289,10 +289,12 @@ class LoraLayer(BaseTunerLayer):
             raise Exception("nan or inf in V")
 
         # Sanity check
+        logging.info(f"U.device = {U.device}, S.device = {S.device}, V.device = {V.device}")
+        logging.info(f"weight.data.device = {weight.data.device}")
         svd_error = torch.dist(U @ torch.diag(S) @ V.t(), weight.data)
-        scale_u = torch.linalg.norm(U) / math.sqrt(min_dim)
-        scale_v = torch.linalg.norm(V) / math.sqrt(min_dim)
-        print(f"scale_u: {scale_u:.2f}, scale_v: {scale_v:.2f}, svd_error: {svd_error:.2f}")
+        scale_u = torch.linalg.norm(U) / math.sqrt(r)
+        scale_v = torch.linalg.norm(V) / math.sqrt(r)
+        logging.info(f"scale_u: {scale_u:.2f}, scale_v: {scale_v:.2f}, svd_error: {svd_error:.2f}")
         assert U.size(0) == out_dim
         assert U.size(1) == r
         assert S.size(0) == r