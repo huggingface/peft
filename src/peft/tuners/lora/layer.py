7656b87c96c45c65d2f41de414210bc6c6d612c8
Georg Slamanig <georgslamanig@gmail.com>
2024-11-05 09:18:08 +0100

    fixed groups issue

--- src/peft/tuners/lora/layer.py
+++ src/peft/tuners/lora/layer.py
@@ -904,7 +904,11 @@ class _ConvNd(nn.Module, LoraLayer):
         conv_layer = type(base_layer)
         out_kernel = out_stride = (1,) * (self._kernel_dim - 2)
         self.lora_A[adapter_name] = conv_layer(self.in_features, r, kernel_size, stride, padding, bias=False)
-        self.lora_B[adapter_name] = conv_layer(r, self.out_features, out_kernel, out_stride, bias=False)
+        if self.base_layer.groups:
+            self.lora_B[adapter_name] = conv_layer(r, int(self.out_features / self.base_layer.groups), out_kernel,
+                                           out_stride, bias=False)
+        else:
+            self.lora_B[adapter_name] = conv_layer(r, self.out_features, out_kernel, out_stride, bias=False)
 
         if use_rslora:
             self.scaling[adapter_name] = lora_alpha / math.sqrt(r)
