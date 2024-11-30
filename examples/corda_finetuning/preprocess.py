782d1404fa0ccfc4f4eec6fdfc095d8b1458051d
5eqn <subcat2077@gmail.com>
2024-11-30 16:37:15 +0800

    FIX: various quick fixes according to comments

--- examples/corda_finetuning/preprocess.py
+++ examples/corda_finetuning/preprocess.py
@@ -1,3 +1,17 @@
+# Copyright 2024-present the HuggingFace Inc. team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import argparse
 import os
 
@@ -87,12 +101,6 @@ if __name__ == "__main__":
         type=str,
         default="meta-llama/Llama-2-7b-hf",
         help="Pretrained model ID",
-    )
-    parser.add_argument(
-        "--alpha",
-        type=float,
-        default=0.5,
-        help="hyper-parameter alpha for ASVD",
     )
     parser.add_argument(
         "--calib_loader_size",
