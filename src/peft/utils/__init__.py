cf0dfe56950a67a7bfceaac1e520fe579521beca
Fabian Keller <bluenote10@users.noreply.github.com>
2024-12-13 15:49:40 +0100

    ENH Apply sorting of imports (#2279)

--- src/peft/utils/__init__.py
+++ src/peft/utils/__init__.py
@@ -12,42 +12,43 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .integrations import map_cache_to_layer_device_map  # noqa: I001
+from .integrations import map_cache_to_layer_device_map
 from .loftq_utils import replace_lora_weights_loftq
-from .peft_types import PeftType, TaskType
 from .other import (
-    TRANSFORMERS_MODELS_TO_PREFIX_TUNING_POSTPROCESS_MAPPING,
-    TRANSFORMERS_MODELS_TO_LORA_TARGET_MODULES_MAPPING,
-    TRANSFORMERS_MODELS_TO_ADALORA_TARGET_MODULES_MAPPING,
-    TRANSFORMERS_MODELS_TO_IA3_TARGET_MODULES_MAPPING,
-    TRANSFORMERS_MODELS_TO_IA3_FEEDFORWARD_MODULES_MAPPING,
-    TRANSFORMERS_MODELS_TO_LNTUNING_TARGET_MODULES_MAPPING,
-    TRANSFORMERS_MODELS_TO_VERA_TARGET_MODULES_MAPPING,
-    TRANSFORMERS_MODELS_TO_FOURIERFT_TARGET_MODULES_MAPPING,
-    TRANSFORMERS_MODELS_TO_VBLORA_TARGET_MODULES_MAPPING,
     CONFIG_NAME,
-    WEIGHTS_NAME,
-    SAFETENSORS_WEIGHTS_NAME,
     INCLUDE_LINEAR_LAYERS_SHORTHAND,
-    _set_trainable,
-    bloom_model_postprocess_past_key_value,
-    prepare_model_for_kbit_training,
-    shift_tokens_right,
-    transpose,
+    SAFETENSORS_WEIGHTS_NAME,
+    TRANSFORMERS_MODELS_TO_ADALORA_TARGET_MODULES_MAPPING,
+    TRANSFORMERS_MODELS_TO_FOURIERFT_TARGET_MODULES_MAPPING,
+    TRANSFORMERS_MODELS_TO_IA3_FEEDFORWARD_MODULES_MAPPING,
+    TRANSFORMERS_MODELS_TO_IA3_TARGET_MODULES_MAPPING,
+    TRANSFORMERS_MODELS_TO_LNTUNING_TARGET_MODULES_MAPPING,
+    TRANSFORMERS_MODELS_TO_LORA_TARGET_MODULES_MAPPING,
+    TRANSFORMERS_MODELS_TO_PREFIX_TUNING_POSTPROCESS_MAPPING,
+    TRANSFORMERS_MODELS_TO_VBLORA_TARGET_MODULES_MAPPING,
+    TRANSFORMERS_MODELS_TO_VERA_TARGET_MODULES_MAPPING,
+    WEIGHTS_NAME,
+    ModulesToSaveWrapper,
+    _freeze_adapter,
     _get_batch_size,
     _get_submodules,
-    _set_adapter,
-    _freeze_adapter,
-    ModulesToSaveWrapper,
-    _prepare_prompt_learning_config,
     _is_valid_match,
-    infer_device,
+    _prepare_prompt_learning_config,
+    _set_adapter,
+    _set_trainable,
+    bloom_model_postprocess_past_key_value,
+    cast_mixed_precision_params,
     get_auto_gptq_quant_linear,
     get_quantization_config,
     id_tensor_storage,
-    cast_mixed_precision_params,
+    infer_device,
+    prepare_model_for_kbit_training,
+    shift_tokens_right,
+    transpose,
 )
-from .save_and_load import get_peft_model_state_dict, set_peft_model_state_dict, load_peft_weights
+from .peft_types import PeftType, TaskType
+from .save_and_load import get_peft_model_state_dict, load_peft_weights, set_peft_model_state_dict
+
 
 __all__ = [
     "CONFIG_NAME",
