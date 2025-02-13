8ccab8d47a8ce79ce086dee34067eca8f72f6108
nemo <git@ningu.net>
2025-02-13 14:21:18 +0100

    Fix missing import and add TODO

--- src/peft/tuners/lora/model.py
+++ src/peft/tuners/lora/model.py
@@ -37,6 +37,7 @@ from peft.tuners.tuners_utils import (
 from peft.utils import (
     TRANSFORMERS_MODELS_TO_LORA_TARGET_MODULES_MAPPING,
     AuxiliaryTrainingWrapper,
+    ModulesToSaveWrapper,
     _freeze_adapter,
     _get_submodules,
     get_peft_model_state_dict,
@@ -459,7 +460,7 @@ class LoraModel(BaseTuner):
 
         hook_handles = []
         for module in self.modules():
-            if isinstance(module, LoraLayer) or isinstance(module, ModulesToSaveWrapper):
+            if isinstance(module, LoraLayer) or isinstance(module, AuxiliaryTrainingWrapper):
                 pre_forward = partial(_adapter_names_pre_forward_hook, adapter_names=adapter_names)
                 handle = module.register_forward_pre_hook(pre_forward, with_kwargs=True)
                 hook_handles.append(handle)
@@ -468,7 +469,7 @@ class LoraModel(BaseTuner):
             # For encoder-decoder models, even when applying beam search, the encoder part of the model should not use
             # the extended adapter_names. This is because the encoder still uses the original, non-extended samples.
             for module in self.model.get_encoder().modules():
-                if isinstance(module, LoraLayer) or isinstance(module, ModulesToSaveWrapper):
+                if isinstance(module, LoraLayer) or isinstance(module, AuxiliaryTrainingWrapper):
                     # Add another hook to overwrite the kwargs with the original adapter names -- this is easier than
                     # trying to exclude the encoder.
                     pre_forward = partial(_adapter_names_pre_forward_hook, adapter_names=original_adapter_names)
@@ -530,6 +531,8 @@ class LoraModel(BaseTuner):
                         target.merge(safe_merge=safe_merge, adapter_names=adapter_names)
                     self._replace_module(parent, target_name, target.get_base_layer(), target)
                 elif isinstance(target, ModulesToSaveWrapper):
+                    # TODO is this still necessary with AuxiliaryTrainingWrapper save/load handling?
+
                     # save any additional trainable modules part of `modules_to_save`
                     new_module = target.modules_to_save[target.active_adapter]
                     if hasattr(new_module, "base_layer"):
