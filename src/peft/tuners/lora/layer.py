FIX Multiple issues with low precision base models
As a user, it should be possible to manually cast the base model to a
lower precision dtype, float16 or bfloat16, and still have the different
PEFT methods work correctly. Currently, this is not the case for many
PEFT methods, as can be replicated by the added tests.

To understand the problem, it helps to take a step back. By default,
PEFT will treat the adapter weights with high precision, i.e. with
float32. When the base model is lower precision, the user needs to pass
inputs in lower precision too, as otherwise self.base_layer(x) would
fail. However, this low precision input clashes with the high precision
adapter weights.

The solution implemented in this PR is to cast the input to a higher
dtype [1]. That way, the whole adapter operation is conducted in high
precision. Only once that has finished will the final result be cast to
the original dtype. This should lead to better results, but it may
require more memory. Note that this is how LoRA is implemented, so the
changes in this PR bring the other methods more in line with what LoRA
does.

If the user does not want the adapter to be in float32, they can always
pass autocast_adapter_dtype=False when calling get_peft_model or
PeftModel.from_pretrained. This is also tested.

Besides adjusting the forward method to account for these changes, the
merge and unmerge methods also often had to be adjusted, as they did not
correctly account for the base model dtype. Now, those methods should
always conserve the original dtype of the base model.

Note that if, for whatever reason, the input casting in [1] is not
desired, users can use the disable_input_dtype_casting context manager
to disable it (more context information on this feature can be found in
PR #2353). I updated the corresponding code to be agnostic to the
specific PEFT method (beforehand, it was only for LoRA).

src/peft/tuners/lora/layer.py
@@ -494,19 +494,6 @@ class LoraLayer(BaseTunerLayer):
 
         return result
 
-    def _cast_input_dtype(self, x: torch.Tensor, dtype: torch.dtype) -> torch.Tensor:
-        """
-        Whether to cast the dtype of the input to the forward method.
-
-        Usually, we want to enable this to align the input dtype with the dtype of the weight, but by setting
-        layer.cast_input_dtype=False, this can be disabled if necessary.
-
-        Enabling or disabling can be managed via the peft.helpers.disable_lora_input_dtype_casting context manager.
-        """
-        if (not self.cast_input_dtype_enabled) or (x.dtype == dtype):
-            return x
-        return x.to(dtype=dtype)
-
 
 # Below code is based on https://github.com/microsoft/LoRA/blob/main/loralib/layers.py
 # and modified to work with PyTorch FSDP
@@ -577,6 +564,7 @@ class Linear(nn.Module, LoraLayer):
                     # Note that safe_merge will be slower than the normal merge
                     # because of the copy operation.
                     orig_weights = base_layer.weight.data.clone()
+                    orig_dtype = orig_weights.dtype
                     delta_weight = self.get_delta_weight(active_adapter)
                     if not self.use_dora[active_adapter]:
                         orig_weights += delta_weight
@@ -601,7 +589,7 @@ class Linear(nn.Module, LoraLayer):
                             f"NaNs detected in the merged weights. The adapter {active_adapter} seems to be broken"
                         )
 
-                    base_layer.weight.data = orig_weights
+                    base_layer.weight.data = orig_weights.to(orig_dtype)
 
                     if self.lora_bias[active_adapter]:
                         new_bias = base_layer.bias + self.lora_B[active_adapter].bias
@@ -632,6 +620,7 @@ class Linear(nn.Module, LoraLayer):
                         dora_factor = self.lora_magnitude_vector[active_adapter].weight / weight_norm
                         dora_factor = transpose(dora_factor.view(-1, 1), self.fan_in_fan_out)
                         new_weight = dora_factor * (base_layer.weight.data + delta_weight)
+                        new_weight = new_weight.to(base_layer.weight.dtype)
                         base_layer.weight.data = new_weight
 
                     if self.lora_bias[active_adapter]:
@@ -650,14 +639,15 @@ class Linear(nn.Module, LoraLayer):
             active_adapter = self.merged_adapters.pop()
             if active_adapter in self.lora_A.keys():
                 weight = self.get_base_layer().weight
+                orig_dtype = weight.dtype
                 delta_weight = self.get_delta_weight(active_adapter)
                 if not self.use_dora[active_adapter]:
-                    weight.data -= delta_weight
+                    weight.data -= delta_weight.to(orig_dtype)
                 else:
                     weight_norm = self._cache_pop(f"{active_adapter}-weight_norm")
                     dora_factor = self.lora_magnitude_vector[active_adapter].weight / weight_norm
                     weight_orig = weight.data / dora_factor.view(-1, 1) - delta_weight
-                    weight.data = weight_orig
+                    weight.data = weight_orig.to(orig_dtype)
 
                 if self.lora_bias[active_adapter]:
                     self.get_base_layer().bias.data -= self.lora_B[active_adapter].bias
@@ -861,11 +851,12 @@ class Embedding(nn.Module, LoraLayer):
         for active_adapter in adapter_names:
             if active_adapter in self.lora_embedding_A.keys():
                 base_layer = self.get_base_layer()
+                orig_dtype = base_layer.weight.dtype
                 if safe_merge:
                     # Note that safe_merge will be slower than the normal merge
                     # because of the copy operation.
                     orig_weights = base_layer.weight.data.clone()
-                    orig_weights += self.get_delta_weight(active_adapter)
+                    orig_weights += self.get_delta_weight(active_adapter).to(orig_dtype)
 
                     if not torch.isfinite(orig_weights).all():
                         raise ValueError(
@@ -874,7 +865,7 @@ class Embedding(nn.Module, LoraLayer):
 
                     base_layer.weight.data = orig_weights
                 else:
-                    base_layer.weight.data += self.get_delta_weight(active_adapter)
+                    base_layer.weight.data += self.get_delta_weight(active_adapter).to(orig_dtype)
                 self.merged_adapters.append(active_adapter)
 
     def unmerge(self) -> None:
@@ -886,8 +877,9 @@ class Embedding(nn.Module, LoraLayer):
             return
         while len(self.merged_adapters) > 0:
             active_adapter = self.merged_adapters.pop()
+            orig_dtype = self.get_base_layer().weight.dtype
             if active_adapter in self.lora_embedding_A.keys():
-                self.get_base_layer().weight.data -= self.get_delta_weight(active_adapter)
+                self.get_base_layer().weight.data -= self.get_delta_weight(active_adapter).to(orig_dtype)
 
     def get_delta_weight(self, adapter) -> torch.Tensor:
         """
@@ -1129,6 +1121,7 @@ class _ConvNd(nn.Module, LoraLayer):
         for active_adapter in adapter_names:
             if active_adapter in self.lora_A.keys():
                 base_layer = self.get_base_layer()
+                orig_dtype = base_layer.weight.dtype
                 if safe_merge:
                     # Note that safe_merge will be slower than the normal merge
                     # because of the copy operation.
@@ -1136,7 +1129,7 @@ class _ConvNd(nn.Module, LoraLayer):
                     delta_weight = self.get_delta_weight(active_adapter)
 
                     if not self.use_dora[active_adapter]:
-                        orig_weights += delta_weight
+                        orig_weights += delta_weight.to(orig_dtype)
                     else:
                         # handle dora
                         # since delta_weight already includes scaling, set it to 1 here
@@ -1151,6 +1144,7 @@ class _ConvNd(nn.Module, LoraLayer):
                         self._cache_store(f"{active_adapter}-weight_norm", weight_norm)
                         dora_factor = self.lora_magnitude_vector[active_adapter].weight / weight_norm
                         orig_weights = dora_factor.view(*self._get_dora_factor_view()) * (orig_weights + delta_weight)
+                        orig_weights = orig_weights.to(orig_dtype)
 
                     if not torch.isfinite(orig_weights).all():
                         raise ValueError(
@@ -1169,7 +1163,7 @@ class _ConvNd(nn.Module, LoraLayer):
                 else:
                     delta_weight = self.get_delta_weight(active_adapter)
                     if not self.use_dora[active_adapter]:
-                        base_layer.weight.data += delta_weight
+                        base_layer.weight.data += delta_weight.to(orig_dtype)
                     else:
                         # handle dora
                         # since delta_weight already includes scaling, set it to 1 here
@@ -1186,7 +1180,7 @@ class _ConvNd(nn.Module, LoraLayer):
                         new_weight = dora_factor.view(*self._get_dora_factor_view()) * (
                             base_layer.weight.data + delta_weight
                         )
-                        base_layer.weight.data = new_weight
+                        base_layer.weight.data = new_weight.to(orig_dtype)
 
                     if self.lora_bias[active_adapter]:
                         base_layer.bias.data += self.lora_B[active_adapter].bias
@@ -1204,14 +1198,15 @@ class _ConvNd(nn.Module, LoraLayer):
             active_adapter = self.merged_adapters.pop()
             if active_adapter in self.lora_A.keys():
                 weight = self.get_base_layer().weight
+                orig_dtype = weight.dtype
                 delta_weight = self.get_delta_weight(active_adapter)
                 if not self.use_dora[active_adapter]:
-                    weight.data -= delta_weight
+                    weight.data -= delta_weight.to(orig_dtype)
                 else:
                     weight_norm = self._cache_pop(f"{active_adapter}-weight_norm")
                     dora_factor = self.lora_magnitude_vector[active_adapter].weight / weight_norm
                     weight_orig = weight.data / dora_factor.view(*self._get_dora_factor_view()) - delta_weight
-                    weight.data = weight_orig
+                    weight.data = weight_orig.to(orig_dtype)
 
                 if self.lora_bias[active_adapter]:
                     self.get_base_layer().bias.data -= self.lora_B[active_adapter].bias
@@ -1501,11 +1496,12 @@ class MultiheadAttention(nn.Module, LoraLayer):
         for active_adapter in adapter_names:
             if active_adapter in self.lora_A.keys():
                 base_layer = self.get_base_layer()
+                orig_dtype = base_layer.out_proj.weight.dtype
                 if safe_merge:
                     # TODO: work with separate weights
                     # merging in_proj (nn.Parameter)
                     orig_weights_in = base_layer.in_proj_weight.data.detach().clone()
-                    orig_weights_in += self.get_delta_weight(active_adapter)
+                    orig_weights_in += self.get_delta_weight(active_adapter).to(base_layer.in_proj_weight.dtype)
                     if not torch.isfinite(orig_weights_in).all():
                         raise ValueError(
                             f"NaNs detected in the merged weights. The adapter {active_adapter} seems to be broken"
@@ -1513,7 +1509,7 @@ class MultiheadAttention(nn.Module, LoraLayer):
 
                     # merging out_proj (subclass of nn.Linear)
                     orig_weights_out = base_layer.out_proj.weight.data.detach().clone()
-                    orig_weights_out += base_layer.out_proj.get_delta_weight(active_adapter)
+                    orig_weights_out += base_layer.out_proj.get_delta_weight(active_adapter).to(orig_dtype)
                     if not torch.isfinite(orig_weights_out).all():
                         raise ValueError(
                             f"NaNs detected in the merged weights. The adapter {active_adapter} seems to be broken"
@@ -1530,7 +1526,8 @@ class MultiheadAttention(nn.Module, LoraLayer):
                 else:
                     # merging in_proj (nn.Parameter)
                     # TODO: work with separate weights
-                    weight_merged = base_layer.in_proj_weight.data.detach() + self.get_delta_weight(active_adapter)
+                    delta_weight = self.get_delta_weight(active_adapter).to(base_layer.in_proj_weight.dtype)
+                    weight_merged = base_layer.in_proj_weight.data.detach() + delta_weight
 
                     # unregister parameter implicitly and overwrite using merged weights; gradients are computed after
                     # forward and, thus, after unmerging (see forward()), therefore this is safe to do.
@@ -1538,9 +1535,8 @@ class MultiheadAttention(nn.Module, LoraLayer):
                     base_layer.in_proj_weight = weight_merged
 
                     # merging out_proj (subclass of nn.Linear)
-                    weight_merged = base_layer.out_proj.weight.data.detach() + base_layer.out_proj.get_delta_weight(
-                        active_adapter
-                    )
+                    delta_weight = base_layer.out_proj.get_delta_weight(active_adapter).to(orig_dtype)
+                    weight_merged = base_layer.out_proj.weight.data.detach() + delta_weight
                     del base_layer.out_proj.get_base_layer().weight
                     base_layer.out_proj.get_base_layer().weight = weight_merged
                     base_layer.out_proj.merge(adapter_names=[active_adapter])
@@ -1556,6 +1552,7 @@ class MultiheadAttention(nn.Module, LoraLayer):
 
         # TODO work with separate weights
         base_layer = self.get_base_layer()
+        orig_dtype = base_layer.out_proj.base_layer.weight.dtype
         while len(self.merged_adapters) > 0:
             active_adapter = self.merged_adapters.pop()
             if active_adapter in self.lora_A.keys():
@@ -1563,14 +1560,14 @@ class MultiheadAttention(nn.Module, LoraLayer):
                 # requires_grad was False when the optimizer was initialized, but still let's try to be correct here.
 
                 # in_proj
-                old_weight = base_layer.in_proj_weight.data - self.get_delta_weight(active_adapter)
+                delta_weight = self.get_delta_weight(active_adapter).to(base_layer.in_proj_weight.dtype)
+                old_weight = base_layer.in_proj_weight.data - delta_weight
                 del base_layer.in_proj_weight
                 base_layer.register_parameter("in_proj_weight", nn.Parameter(old_weight, requires_grad=False))
 
                 # out_proj
-                old_weight = base_layer.out_proj.base_layer.weight.data - base_layer.out_proj.get_delta_weight(
-                    active_adapter
-                )
+                delta_weight = base_layer.out_proj.get_delta_weight(active_adapter).to(orig_dtype)
+                old_weight = base_layer.out_proj.base_layer.weight.data - delta_weight
                 del base_layer.out_proj.base_layer.weight
                 base_layer.out_proj.base_layer.register_parameter(
                     "weight", nn.Parameter(old_weight, requires_grad=False)

src/peft/tuners/tuners_utils.py
@@ -20,7 +20,7 @@ import textwrap
 import warnings
 from abc import ABC, abstractmethod
 from contextlib import contextmanager, nullcontext
-from typing import Any, Optional, Union
+from typing import Any, Optional, Union, overload
 
 import torch
 from accelerate.hooks import AlignDevicesHook
@@ -859,6 +859,29 @@ class BaseTunerLayer(ABC):
                 adapter_layer[adapter_name] = adapter_layer[adapter_name].to(device, dtype=dtype)
             else:
                 adapter_layer[adapter_name] = adapter_layer[adapter_name].to(device)
+
+    @overload
+    def _cast_input_dtype(self, x: None, dtype: torch.dtype) -> None: ...
+
+    @overload
+    def _cast_input_dtype(self, x: torch.Tensor, dtype: torch.dtype) -> torch.Tensor: ...
+
+    def _cast_input_dtype(self, x, dtype: torch.dtype):
+        """
+        Whether to cast the dtype of the input of the forward method.
+
+        Usually, we want to enable this to align the input dtype with the dtype of the weight, but by setting
+        layer.cast_input_dtype=False, this can be disabled if necessary.
+
+        Enabling or disabling can be managed via the peft.helpers.disable_lora_input_dtype_casting context manager.
+        """
+        if x is None:  # useful e.g. if x is the bias, which can be None
+            return None
+
+        cast_input_dtype_enabled = getattr(self, "cast_input_dtype_enabled", True)
+        if (not cast_input_dtype_enabled) or (x.dtype == dtype):
+            return x
+        return x.to(dtype=dtype)
 
 
 def _find_minimal_target_modules(