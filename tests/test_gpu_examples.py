c375f99ad4a7bde479589da77d5e02417c4db17d
jiqing-feng <jiqing.feng@intel.com>
2025-04-03 23:16:10 +0800

    TST Increase tolerance in some tests for xpu (#2475)
    Numerical stability is lower, increase tolerances.

--- tests/test_gpu_examples.py
+++ tests/test_gpu_examples.py
@@ -2199,8 +2199,8 @@ class TestLoftQ:
     # The error factor indicates by how much the quantization error should be decreased when using LoftQ compared to
     # quantization without LoftQ. Thus 1.03 means that the error should be decreased by 3% at least. This is a very
     # conservative value to prevent flakiness, in practice most gains are > 1.5
-    error_factor = 1.03
     device = infer_device()
+    error_factor = 1.005 if device in ("xpu", "cpu") else 1.03
 
     def get_input(self, model_id, device):
         tokenizer = AutoTokenizer.from_pretrained(model_id)
