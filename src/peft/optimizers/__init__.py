0c2bdbb11ab74cf061d6772320ea93873033df92
Aaron Chung <35474496+AaronZLT@users.noreply.github.com>
2025-04-10 16:53:19 +0800

    FEAT Add LoRA-FA to PEFT (#2468)
    Adds LoRA with frozen A (LoRA-FA) to PEFT.
    
    Paper: https://arxiv.org/abs/2308.03303

--- src/peft/optimizers/__init__.py
+++ src/peft/optimizers/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2024-present the HuggingFace Inc. team.
+# Copyright 2025-present the HuggingFace Inc. team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from .lorafa import create_lorafa_optimizer
 from .loraplus import create_loraplus_optimizer
 
 
-__all__ = ["create_loraplus_optimizer"]
+__all__ = ["create_lorafa_optimizer", "create_loraplus_optimizer"]
