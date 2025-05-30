aea21951b63a3f7d52184f584139ec538edf483f
Benjamin Bossan <benjamin.bossan@gmail.com>
2025-05-30 17:56:24 +0200

    Remove workaround for error 2, fix in transformers

--- tests/conftest.py
+++ tests/conftest.py
@@ -44,12 +44,8 @@ def pytest_runtest_makereport(item, call):
     """
     Plug into the pytest test report generation to skip a specific MacOS failure caused by transformers.
 
-    Failure 0 was introduced by https://github.com/huggingface/transformers/pull/37785, which results in torch.load
+    The error was introduced by https://github.com/huggingface/transformers/pull/37785, which results in torch.load
     failing when using torch < 2.6.
-
-    Failure 1 was introduced by https://github.com/huggingface/transformers/pull/37919, which results in a DTensor
-    import being triggered in the save_pretrained, which fails with MacOS and torch 2.2 (possibly also later MacOS
-    versions).
 
     Since the MacOS x86 runners need to use an older torch version, those steps are necessary to get the CI green.
     """
@@ -57,8 +53,7 @@ def pytest_runtest_makereport(item, call):
     rep = outcome.get_result()
     # ref:
     # https://github.com/huggingface/transformers/blob/858ce6879a4aa7fa76a7c4e2ac20388e087ace26/src/transformers/utils/import_utils.py#L1418
-    error_msg_0 = re.compile(r"Due to a serious vulnerability issue in `torch.load`")
-    error_msg_1 = re.compile(r"cannot import name 'DTensor' from 'torch.distributed.tensor'")
+    error_msg = re.compile(r"Due to a serious vulnerability issue in `torch.load`")
 
     # notes:
     # - pytest uses hard-coded strings, we cannot import and use constants
@@ -66,9 +61,9 @@ def pytest_runtest_makereport(item, call):
     # - errors can happen during call (running the test) but also setup (e.g. in fixtures)
     if rep.failed and (rep.when in ("setup", "call")) and (platform.system() == "Darwin"):
         exc_msg = str(call.excinfo.value)
-        if error_msg_0.search(exc_msg) or error_msg_1.search(exc_msg):
+        if error_msg.search(exc_msg):
             # turn this failure into an xfail:
             rep.outcome = "skipped"
             # for this attribute, see:
             # https://github.com/pytest-dev/pytest/blob/bd6877e5874b50ee57d0f63b342a67298ee9a1c3/src/_pytest/reports.py#L266C5-L266C13
-            rep.wasxfail = "Errors known to occur on MacOS with older torch versions, won't be fixed"
+            rep.wasxfail = "Error known to occur on MacOS with older torch versions, won't be fixed"
