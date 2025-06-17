fd3edea5faec924e17dae61721c429954dc1dbf5
Benjamin Bossan <benjamin.bossan@gmail.com>
2025-06-17 14:26:22 +0200

    Fix for non-prefix prompt learning
    Avoid regression, even though I'm not quite sure if the old behavior is
    technically correct.

--- src/peft/peft_model.py
+++ src/peft/peft_model.py
@@ -2018,15 +2018,19 @@ class PeftModelForCausalLM(PeftModel):
                 model_kwargs["input_ids"] = None
 
         # if cache_position exists and if we're in the prefill stage
-        if (model_kwargs.get("cache_position") is not None) and (model_kwargs["cache_position"][0] == 0):
-            if peft_config.peft_type == PeftType.PREFIX_TUNING:
-                # for prefix tuning, the past_key_values have been prefilled
-                model_kwargs["cache_position"] += peft_config.num_virtual_tokens
-            else:
-                # for other prompt learning methods, the inputs_embeds have been extended
-                device = model_kwargs["cache_position"].device
-                new_seq_length = model_kwargs["cache_position"].shape[0] + peft_config.num_virtual_tokens
-                model_kwargs["cache_position"] = torch.arange(new_seq_length).to(device)
+        if (
+            (model_kwargs.get("cache_position") is not None)
+            and (model_kwargs["cache_position"][0] == 0)
+            and (peft_config.peft_type == PeftType.PREFIX_TUNING)
+        ):
+            # for prefix tuning, the past_key_values have been prefilled
+            model_kwargs["cache_position"] += peft_config.num_virtual_tokens
+        else:
+            # For transformers>=4.38.0 - for some architectures such as Llama, `cache_position` is passed in the forward
+            # pass to keep track of the position ids of the cache. We have to pop that from `model_kwargs` as
+            # `cache_position` is properly created by the model, using the passed `inputs_embeds`:
+            # https://github.com/huggingface/transformers/blob/593230f0a1150ea9c0477b9d859f25daf73c8c33/src/transformers/models/llama/modeling_llama.py#L956
+            _ = model_kwargs.pop("cache_position", None)
 
         return model_kwargs
 
