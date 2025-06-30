e03fecb2b0b64e34a47e753ca706140c151c5dd9
kbhardwa <kbhardwa@qti.qualcomm.com>
2025-06-30 23:24:15 +0000

    Add the readme example into a file

---
+++ examples/shira_finetuning/shira_sft_finetuning.py
@@ -0,0 +1,21 @@
+import torch
+from peft import ShiraConfig, get_peft_model
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from trl import SFTConfig, SFTTrainer
+from datasets import load_dataset
+
+model = AutoModelForCausalLM.from_pretrained("facebook/opt-350m", torch_dtype=torch.bfloat16, device_map="auto")
+tokenizer = AutoTokenizer.from_pretrained("facebook/opt-350m")
+dataset = load_dataset("imdb", split="train[:1%]")
+shira_config = ShiraConfig(
+    r=32,
+)
+peft_model = get_peft_model(model, shira_config)
+training_args = SFTConfig(dataset_text_field="text", max_seq_length=128)
+trainer = SFTTrainer(
+    model=peft_model,
+    train_dataset=dataset,
+    tokenizer=tokenizer,
+)
+trainer.train()
+peft_model.save_pretrained("shira-opt-350m")
