3ce7c8200055a1b4b02b688dfe4dbe440e3c41ab
Ahmet Bilican <ahmet.bilican@codeway.co>
2025-08-03 04:16:52 +0000

    added copyright notices

--- src/peft/tuners/waveft/wavelet.py
+++ src/peft/tuners/waveft/wavelet.py
@@ -11,12 +11,33 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """
 Minimal wavelet implementation extracted from PyWavelets
-to avoid heavy dependency on pywt.
 
-This module provides a lightweight Wavelet class that implements 
-the most commonly used wavelets for signal processing.
+This code contains portions derived from PyWavelets:
+Copyright (c) 2006-2012 Filip Wasilewski <http://en.ig.ma/>
+Copyright (c) 2012-     The PyWavelets Developers <https://github.com/PyWavelets/pywt>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Original source: https://github.com/PyWavelets/pywt
 """
 
 import math
