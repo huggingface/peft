f125d4db95f2711b0d2e7c83d7312896a177f179
Tanuj Rai <tanujrai19@gmail.com>
2025-09-09 14:43:03 +0530

    Update test_scaling_rslora.py

--- tests/test_scaling_rslora.py
+++ tests/test_scaling_rslora.py
@@ -1,36 +1,37 @@
-def test_scaling_simple(self, model):
-    n_layers = 5
-    rank, lora_alpha = 8, 16
-    config = LoraConfig(
-        r=rank,
-        lora_alpha=lora_alpha,
-        target_modules=["k_proj"],
-    )
-    model = get_peft_model(model, config)
-    scalings = self.get_scalings(model)
-    expected = [lora_alpha / math.sqrt(rank)] * n_layers
-    assert scalings == expected
-
-    # double
-    self.scale_layer(model, 2)
-    scalings = self.get_scalings(model)
-    expected = [4.0] * n_layers
-    assert scalings == expected
-
-    # back to original
-    self.unscale_layer(model, None)
-    scalings = self.get_scalings(model)
-    expected = [2.0] * n_layers
-    assert scalings == expected
-
-    # triple
-    self.set_scale(model, "default", 3)
-    scalings = self.get_scalings(model)
-    expected = [6.0] * n_layers
-    assert scalings == expected
-
-    # back to original
-    self.unscale_layer(model, 3)
-    scalings = self.get_scalings(model)
-    expected = [2.0] * n_layers
-    assert scalings == expected
+class TestScaling:  
+    def test_scaling_simple(self, model):
+        n_layers = 5
+        rank, lora_alpha = 8, 16
+        config = LoraConfig(
+            r=rank,
+            lora_alpha=lora_alpha,
+            target_modules=["k_proj"],
+        )
+        model = get_peft_model(model, config)
+        scalings = self.get_scalings(model)
+        expected = [lora_alpha / math.sqrt(rank)] * n_layers
+        assert scalings == expected
+    
+        # double
+        self.scale_layer(model, 2)
+        scalings = self.get_scalings(model)
+        expected = [4.0] * n_layers
+        assert scalings == expected
+    
+        # back to original
+        self.unscale_layer(model, None)
+        scalings = self.get_scalings(model)
+        expected = [2.0] * n_layers
+        assert scalings == expected
+    
+        # triple
+        self.set_scale(model, "default", 3)
+        scalings = self.get_scalings(model)
+        expected = [6.0] * n_layers
+        assert scalings == expected
+    
+        # back to original
+        self.unscale_layer(model, 3)
+        scalings = self.get_scalings(model)
+        expected = [2.0] * n_layers
+        assert scalings == expected
