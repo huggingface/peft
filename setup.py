ed03787b2bbda2b9842535a06ad4ac91b0b9bc1f
Yuanyuan Chen <cyyever@outlook.com>
2025-09-19 11:50:28 +0800

    Add python3.13 classifier to setup.py
    Signed-off-by: Yuanyuan Chen <cyyever@outlook.com>

--- setup.py
+++ setup.py
@@ -82,6 +82,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
 )
