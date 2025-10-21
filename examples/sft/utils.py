659b724e4a059a7fc3bc52bd5d5744ce55694cf1
Benjamin Bossan <benjamin.bossan@gmail.com>
2025-10-21 13:01:13 +0200

    Undo unrelated change

--- examples/sft/utils.py
+++ examples/sft/utils.py
@@ -97,9 +97,7 @@ def create_and_prepare_model(args, data_args, training_args):
     ):
         raise NotImplementedError("Unsloth is not supported in distributed training")
 
-    if args.use_4bit_quantization and args.use_8bit_quantization:
-        raise ValueError("You configured 4bit and 8bit quantization at the same time, please choose only one of them.")
-    elif args.use_4bit_quantization:
+    if args.use_4bit_quantization:
         compute_dtype = getattr(torch, args.bnb_4bit_compute_dtype)
         quant_storage_dtype = getattr(torch, args.bnb_4bit_quant_storage_dtype)
 
@@ -117,8 +115,8 @@ def create_and_prepare_model(args, data_args, training_args):
                 print("=" * 80)
                 print("Your GPU supports bfloat16, you can accelerate training with the argument --bf16")
                 print("=" * 80)
-    elif args.use_8bit_quantization:
-        bnb_config = BitsAndBytesConfig(load_in_8bit=args.use_8bit_quantization)
+        elif args.use_8bit_quantization:
+            bnb_config = BitsAndBytesConfig(load_in_8bit=args.use_8bit_quantization)
 
     if args.use_unsloth:
         if torch.xpu.is_available():
