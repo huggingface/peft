62d4bed67a00b974c131e4d0e6f8a63fa649d050
Benjamin Bossan <benjamin.bossan@gmail.com>
2025-12-12 12:55:40 +0100

    Fix copyright notice

--- scripts/evaluate-lora-conversion.py
+++ scripts/evaluate-lora-conversion.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (c) 2025 Your Organization/Project. All rights reserved.
+# Copyright 2025-present the HuggingFace Inc. team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
