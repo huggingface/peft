huggingface/peft

commit 91aad9306e9d288f60ddf187014270d945c0eb83

Refactor layer initialization to use PEFT config directly (#2960)
There is a lot of code duplication in the way that adapter layers are
initialized. This means that if, say, a new LoRA config argument is
added, we need updates in the following places:

1. LoraModel._create_and_replace: kwargs dict
2. LoraModel._create_and_replace: update_layer arguments
3. LoraModel._create_and_replace: update_layer signature
4. Linear.__init__: signature (same for each LoRA layer type)
5. Linear.__init__: update_layer arguments
6. resolve_lora_variant: signature if the argument is tied to a LoRA
   variant

It is far too easy to forget something and it's in fact a common mistake
we see, especially by outside contributors.

This PR aims at removing this duplication as much as possible. The crux
is that almost all of these arguments derive from the LoraConfig.
Therefore, the duplication can be reduced by passing the LoraConfig
directly instead of each of its individual arguments.

As is, this change is only implemented by LoRA, which is by far the most
affected by this duplication. However, an argument could be made that
for consistency, the other PEFT methods should be refactored
accordingly.

src/peft/tuners/lora/model.py
@@ -205,19 +205,6 @@ class LoraModel(BaseTuner):
         kwargs = {
             "r": r,
             "lora_alpha": alpha,
-            "lora_dropout": lora_config.lora_dropout,
-            "fan_in_fan_out": lora_config.fan_in_fan_out,
-            "init_lora_weights": lora_config.init_lora_weights,
-            "use_rslora": lora_config.use_rslora,
-            "use_dora": lora_config.use_dora,
-            "use_alora": lora_config.alora_invocation_tokens is not None,
-            "use_qalora": lora_config.use_qalora,
-            "qalora_group_size": lora_config.qalora_group_size,
-            "ephemeral_gpu_offload": lora_config.runtime_config.ephemeral_gpu_offload,
-            "lora_bias": lora_config.lora_bias,
-            "arrow_config": lora_config.arrow_config,
-            "lora_ga_config": lora_config.lora_ga_config,
-            "use_bdlora": lora_config.use_bdlora,
             "target_name": current_key,
             "loaded_in_8bit": getattr(self.model, "is_loaded_in_8bit", False),
             "loaded_in_4bit": getattr(self.model, "is_loaded_in_4bit", False),
@@ -248,16 +235,8 @@ class LoraModel(BaseTuner):
                 adapter_name,
                 r,
                 lora_alpha=alpha,
-                lora_dropout=lora_config.lora_dropout,
-                init_lora_weights=lora_config.init_lora_weights,
-                use_rslora=lora_config.use_rslora,
-                use_dora=lora_config.use_dora,
-                lora_bias=lora_config.lora_bias,
-                arrow_config=lora_config.arrow_config,
-                lora_ga_config=lora_config.lora_ga_config,
-                use_bdlora=lora_config.use_bdlora,
-                inference_mode=lora_config.inference_mode,
                 target_name=current_key,
+                config=lora_config,
             )
         else:
             if isinstance(target, ParamWrapper) and (parameter_name == target.parameter_name):
@@ -309,7 +288,7 @@ class LoraModel(BaseTuner):
         if lora_config._custom_modules:
             # Experimental custom LoRA module support. Allows users to pass a custom mapping for unsupported layer
             # types by impelementing their own LoRA layers.
-            def dynamic_dispatch_func(target, adapter_name, lora_config, **kwargs):
+            def dynamic_dispatch_func(target, adapter_name, config, **kwargs):
                 new_module = None
 
                 if isinstance(target, BaseTunerLayer):
@@ -317,9 +296,9 @@ class LoraModel(BaseTuner):
                 else:
                     target_base_layer = target
 
-                for key, custom_cls in lora_config._custom_modules.items():
+                for key, custom_cls in config._custom_modules.items():
                     if isinstance(target_base_layer, key):
-                        new_module = custom_cls(target, adapter_name, **kwargs)
+                        new_module = custom_cls(target, adapter_name, config=config, **kwargs)
                         break
 
                 return new_module
@@ -353,7 +332,7 @@ class LoraModel(BaseTuner):
 
         new_module = None
         for dispatcher in dispatchers:
-            new_module = dispatcher(target, adapter_name, lora_config=lora_config, **kwargs)
+            new_module = dispatcher(target, adapter_name, config=lora_config, **kwargs)
             if new_module is not None:  # first match wins
                 break
 

src/peft/tuners/adalora/model.py
@@ -119,9 +119,6 @@ class AdaLoraModel(LoraModel):
         kwargs = {
             "r": lora_config.init_r,
             "lora_alpha": lora_config.lora_alpha,
-            "lora_dropout": lora_config.lora_dropout,
-            "fan_in_fan_out": lora_config.fan_in_fan_out,
-            "init_lora_weights": lora_config.init_lora_weights,
             "loaded_in_8bit": getattr(self.model, "is_loaded_in_8bit", False),
             "loaded_in_4bit": getattr(self.model, "is_loaded_in_4bit", False),
         }
@@ -148,8 +145,7 @@ class AdaLoraModel(LoraModel):
                 adapter_name,
                 lora_config.init_r,
                 lora_config.lora_alpha,
-                lora_config.lora_dropout,
-                lora_config.init_lora_weights,
+                config=lora_config,
             )
 
     @staticmethod
@@ -200,25 +196,25 @@ class AdaLoraModel(LoraModel):
             new_module = SVDQuantLinear(target, adapter_name, **kwargs)
         else:
             if isinstance(target_base_layer, torch.nn.Linear):
-                if kwargs["fan_in_fan_out"]:
+                if lora_config.fan_in_fan_out:
                     warnings.warn(
                         "fan_in_fan_out is set to True but the target module is `torch.nn.Linear`. "
                         "Setting fan_in_fan_out to False."
                     )
-                    kwargs["fan_in_fan_out"] = lora_config.fan_in_fan_out = False
+                    lora_config.fan_in_fan_out = False
             elif isinstance(target_base_layer, Conv1D):
-                if not kwargs["fan_in_fan_out"]:
+                if not lora_config.fan_in_fan_out:
                     warnings.warn(
                         "fan_in_fan_out is set to False but the target module is `Conv1D`. "
                         "Setting fan_in_fan_out to True."
                     )
-                    kwargs["fan_in_fan_out"] = lora_config.fan_in_fan_out = True
+                    lora_config.fan_in_fan_out = True
             else:
                 raise ValueError(
                     f"Target module {target} is not supported. "
                     f"Currently, only `torch.nn.Linear` and `Conv1D` are supported."
                 )
-            new_module = SVDLinear(target, adapter_name, **kwargs)
+            new_module = SVDLinear(target, adapter_name, config=lora_config, **kwargs)
 
         return new_module
 

src/peft/tuners/adalora/bnb.py
@@ -18,6 +18,7 @@ import torch
 
 from peft.import_utils import is_bnb_4bit_available, is_bnb_available
 
+from .config import AdaLoraConfig
 from .layer import AdaLoraLayer
 
 
@@ -29,10 +30,9 @@ if is_bnb_available():
             self,
             base_layer: torch.nn.Module,
             adapter_name: str,
+            config: AdaLoraConfig,
             r: int = 0,
             lora_alpha: int = 1,
-            lora_dropout: float = 0.0,
-            init_lora_weights: bool = True,
             **kwargs,
         ) -> None:
             super().__init__()
@@ -41,7 +41,7 @@ if is_bnb_available():
             self.get_base_layer().weight.requires_grad = False
 
             self._active_adapter = adapter_name
-            self.update_layer(adapter_name, r, lora_alpha, lora_dropout, init_lora_weights)
+            self.update_layer(adapter_name, r, lora_alpha, config=config)
 
         def forward(self, x: torch.Tensor) -> torch.Tensor:
             # note: no check for self.merged because merging is not supported (yet)
@@ -87,10 +87,8 @@ if is_bnb_4bit_available():
             self,
             base_layer: torch.nn.Module,
             adapter_name: str,
+            config: AdaLoraConfig,
             r: int = 0,
-            lora_alpha: int = 1,
-            lora_dropout: float = 0.0,
-            init_lora_weights: bool = True,
             **kwargs,
         ) -> None:
             super().__init__()
@@ -99,7 +97,7 @@ if is_bnb_4bit_available():
             self.get_base_layer().weight.requires_grad = False
 
             self._active_adapter = adapter_name
-            self.update_layer(adapter_name, r, lora_alpha, lora_dropout, init_lora_weights)
+            self.update_layer(adapter_name, r, config=config)
 
         def forward(self, x: torch.Tensor, *args: Any, **kwargs: Any) -> torch.Tensor:
             # note: no check for self.merged because merging is not supported (yet)

src/peft/tuners/adalora/gptq.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 import torch
 
+from .config import AdaLoraConfig
 from .layer import AdaLoraLayer
 
 
@@ -21,10 +22,8 @@ class SVDQuantLinear(torch.nn.Module, AdaLoraLayer):
         self,
         base_layer,
         adapter_name,
+        config: AdaLoraConfig,
         r: int = 0,
-        lora_alpha: int = 1,
-        lora_dropout: float = 0.0,
-        init_lora_weights: bool = True,
         **kwargs,
     ) -> None:
         super().__init__()
@@ -34,7 +33,7 @@ class SVDQuantLinear(torch.nn.Module, AdaLoraLayer):
         # for backwards compatibility
         self.quant_linear_module = base_layer
         self._active_adapter = adapter_name
-        self.update_layer(adapter_name, r, lora_alpha, lora_dropout, init_lora_weights)
+        self.update_layer(adapter_name, r, config=config)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         result = self.quant_linear_module(x)

src/peft/tuners/adalora/layer.py
@@ -24,6 +24,8 @@ from peft.tuners.lora import LoraLayer
 from peft.tuners.tuners_utils import check_adapters_to_merge
 from peft.utils import transpose
 
+from .config import AdaLoraConfig
+
 
 if packaging.version.parse(transformers.__version__) >= packaging.version.parse("4.33.0"):
     from transformers.integrations import deepspeed_config
@@ -45,9 +47,10 @@ class AdaLoraLayer(LoraLayer):
         self.lora_B = nn.ParameterDict({})
         self.ranknum = nn.ParameterDict({})
 
-    def update_layer(
-        self, adapter_name, r, lora_alpha, lora_dropout, init_lora_weights, inference_mode: bool = False, **kwargs
-    ):
+    def update_layer(self, adapter_name: str, r: int, lora_alpha: int, config: AdaLoraConfig, **kwargs) -> None:
+        lora_dropout = config.lora_dropout
+        init_lora_weights = config.init_lora_weights
+        inference_mode = config.inference_mode
         if r < 0:
             # note: r == 0 is allowed for AdaLora, see #1539
             raise ValueError(f"`r` should be a positive integer or 0, but the value passed is {r}")
@@ -91,11 +94,9 @@ class SVDLinear(nn.Module, AdaLoraLayer):
         self,
         base_layer: nn.Module,
         adapter_name: str,
+        config: AdaLoraConfig,
         r: int = 0,
         lora_alpha: int = 1,
-        lora_dropout: float = 0.0,
-        fan_in_fan_out: bool = False,
-        init_lora_weights: bool = True,
         **kwargs,
     ) -> None:
         super().__init__()
@@ -103,9 +104,9 @@ class SVDLinear(nn.Module, AdaLoraLayer):
         # Freezing the pre-trained weight matrix
         self.get_base_layer().weight.requires_grad = False
 
-        self.fan_in_fan_out = fan_in_fan_out
+        self.fan_in_fan_out = config.fan_in_fan_out
         self._active_adapter = adapter_name
-        self.update_layer(adapter_name, r, lora_alpha, lora_dropout, init_lora_weights)
+        self.update_layer(adapter_name, r, lora_alpha, config=config)
 
     def merge(self, safe_merge: bool = False, adapter_names: Optional[list[str]] = None) -> None:
         """

src/peft/tuners/lora/aqlm.py
@@ -20,6 +20,8 @@ from peft.import_utils import is_aqlm_available
 from peft.tuners.lora.layer import LoraLayer
 from peft.tuners.tuners_utils import BaseTunerLayer
 
+from .config import LoraConfig
+
 
 if is_aqlm_available():
     from aqlm import QuantizedLinear
@@ -30,16 +32,12 @@ class AqlmLoraLinear(torch.nn.Module, LoraLayer):
         self,
         base_layer,
         adapter_name: str,
+        config: LoraConfig,
         r: int = 0,
         lora_alpha: int = 1,
-        lora_dropout: float = 0.0,
-        init_lora_weights: bool = True,
-        use_rslora: bool = False,
-        use_dora: bool = False,
-        lora_bias: bool = False,
         **kwargs,
     ):
-        if use_dora:
+        if config.use_dora:
             raise ValueError(f"{self.__class__.__name__} does not support DoRA yet, please set it to False")
 
         super().__init__()
@@ -50,11 +48,7 @@ class AqlmLoraLinear(torch.nn.Module, LoraLayer):
             adapter_name,
             r,
             lora_alpha=lora_alpha,
-            lora_dropout=lora_dropout,
-            init_lora_weights=init_lora_weights,
-            use_rslora=use_rslora,
-            use_dora=use_dora,
-            lora_bias=lora_bias,
+            config=config,
         )
 
     def forward(self, x: torch.Tensor):
@@ -98,6 +92,7 @@ class AqlmLoraLinear(torch.nn.Module, LoraLayer):
 def dispatch_aqlm(
     target: torch.nn.Module,
     adapter_name: str,
+    config: LoraConfig,
     **kwargs: Any,
 ) -> Optional[torch.nn.Module]:
     new_module = None
@@ -108,7 +103,7 @@ def dispatch_aqlm(
         target_base_layer = target
 
     if is_aqlm_available() and isinstance(target_base_layer, QuantizedLinear):
-        new_module = AqlmLoraLinear(target, adapter_name, **kwargs)
+        new_module = AqlmLoraLinear(target, adapter_name, config=config, **kwargs)
         target.qweight = target_base_layer.codes
 
     return new_module

src/peft/tuners/lora/awq.py
@@ -21,22 +21,20 @@ from peft.import_utils import is_auto_awq_available
 from peft.tuners.lora.layer import LoraLayer
 from peft.tuners.tuners_utils import BaseTunerLayer
 
+from .config import LoraConfig
+
 
 class AwqLoraLinear(torch.nn.Module, LoraLayer):
     def __init__(
         self,
         base_layer,
         adapter_name,
+        config: LoraConfig,
         r: int = 0,
         lora_alpha: int = 1,
-        lora_dropout: float = 0.0,
-        init_lora_weights: bool = True,
-        use_rslora: bool = False,
-        use_dora: bool = False,
-        lora_bias: bool = False,
         **kwargs,
     ):
-        if use_dora:
+        if config.use_dora:
             raise ValueError(f"{self.__class__.__name__} does not support DoRA yet, please set it to False")
 
         super().__init__()
@@ -51,11 +49,7 @@ class AwqLoraLinear(torch.nn.Module, LoraLayer):
             adapter_name,
             r,
             lora_alpha=lora_alpha,
-            lora_dropout=lora_dropout,
-            init_lora_weights=init_lora_weights,
-            use_rslora=use_rslora,
-            use_dora=use_dora,
-            lora_bias=lora_bias,
+            config=config,
         )
 
     def forward(self, x: torch.Tensor):
@@ -92,6 +86,7 @@ class AwqLoraLinear(torch.nn.Module, LoraLayer):
 def dispatch_awq(
     target: torch.nn.Module,
     adapter_name: str,
+    config: LoraConfig,
     **kwargs: Any,
 ) -> Optional[torch.nn.Module]:
     new_module = None
@@ -115,7 +110,7 @@ def dispatch_awq(
                     f"but only versions above {AUTOAWQ_MINIMUM_VERSION} are supported for PEFT."
                 )
 
-            new_module = AwqLoraLinear(target, adapter_name, **kwargs)
+            new_module = AwqLoraLinear(target, adapter_name, config=config, **kwargs)
             target.qweight = target_base_layer.qweight
 
     return new_module

src/peft/tuners/lora/bnb.py
@@ -24,7 +24,7 @@ from peft.tuners.tuners_utils import BaseTunerLayer, check_adapters_to_merge
 from peft.utils.integrations import dequantize_bnb_weight
 from peft.utils.other import transpose
 
-from .config import ArrowConfig
+from .config import LoraConfig
 from .layer import LoraLayer, LoraVariant
 
 
@@ -38,15 +38,9 @@ if is_bnb_available():
             self,
             base_layer: torch.nn.Module,
             adapter_name: str,
+            config: LoraConfig,
             r: int = 0,
             lora_alpha: int = 1,
-            lora_dropout: float = 0.0,
-            init_lora_weights: bool = True,
-            use_rslora: bool = False,
-            use_alora: bool = False,
-            use_dora: bool = False,
-            arrow_config: ArrowConfig = None,
-            lora_bias: bool = False,
             **kwargs,
         ) -> None:
             super().__init__()
@@ -58,24 +52,17 @@ if is_bnb_available():
                 adapter_name,
                 r,
                 lora_alpha=lora_alpha,
-                lora_dropout=lora_dropout,
-                init_lora_weights=init_lora_weights,
-                use_rslora=use_rslora,
-                use_dora=use_dora,
-                use_alora=use_alora,
-                lora_bias=lora_bias,
-                arrow_config=arrow_config,
+                config=config,
             )
 
-        def resolve_lora_variant(
-            self, *, arrow_config: ArrowConfig, use_dora: bool, use_alora: bool, **kwargs
-        ) -> Optional[LoraVariant]:
-            if arrow_config is not None:
+        def resolve_lora_variant(self, *, config, **kwargs) -> Optional[LoraVariant]:
+            if config.arrow_config is not None:
                 from .variants import ArrowLinearVariant
 
                 return ArrowLinearVariant()
 
-            if not use_dora and not use_alora:
+            use_alora = config.alora_invocation_tokens is not None
+            if not config.use_dora and not use_alora:
                 return None
 
             from .variants import ALoraLinearVariant, DoraLinearVariant
@@ -296,7 +283,7 @@ if is_bnb_available():
             rep = super().__repr__()
             return "lora." + rep
 
-    def dispatch_bnb_8bit(target: torch.nn.Module, adapter_name: str, **kwargs):
+    def dispatch_bnb_8bit(target: torch.nn.Module, adapter_name: str, config: LoraConfig, **kwargs):
         new_module = None
 
         if isinstance(target, BaseTunerLayer):
@@ -314,7 +301,7 @@ if is_bnb_available():
                     "index": target.index,
                 }
             )
-            new_module = Linear8bitLt(target, adapter_name, **eightbit_kwargs)
+            new_module = Linear8bitLt(target, adapter_name, config=config, **eightbit_kwargs)
 
         return new_module
 
@@ -327,14 +314,9 @@ if is_bnb_4bit_available():
             self,
             base_layer: torch.nn.Module,
             adapter_name: str,
+            config: LoraConfig,
             r: int = 0,
             lora_alpha: int = 1,
-            lora_dropout: float = 0.0,
-            init_lora_weights: bool = True,
-            use_rslora: bool = False,
-            use_dora: bool = False,
-            arrow_config: ArrowConfig = None,
-            lora_bias: bool = False,
             **kwargs,
         ) -> None:
             super().__init__()
@@ -346,23 +328,17 @@ if is_bnb_4bit_available():
                 adapter_name,
                 r,
                 lora_alpha=lora_alpha,
-                lora_dropout=lora_dropout,
-                init_lora_weights=init_lora_weights,
-                use_rslora=use_rslora,
-                use_dora=use_dora,
-                lora_bias=lora_bias,
-                arrow_config=arrow_config,
+                config=config,
             )
 
-        def resolve_lora_variant(
-            self, *, arrow_config: ArrowConfig, use_dora: bool, use_alora: bool, **kwargs
-        ) -> Optional[LoraVariant]:
-            if arrow_config is not None:
+        def resolve_lora_variant(self, *, config: LoraConfig, **kwargs) -> Optional[LoraVariant]:
+            if config.arrow_config is not None:
                 from .variants import ArrowLinearVariant
 
                 return ArrowLinearVariant()
 
-            if not use_dora and not use_alora:
+            use_alora = config.alora_invocation_tokens is not None
+            if not config.use_dora and not use_alora:
                 return None
 
             from .variants import ALoraLinearVariant, DoraLinearVariant
@@ -588,7 +564,7 @@ if is_bnb_4bit_available():
             rep = super().__repr__()
             return "lora." + rep
 
-    def dispatch_bnb_4bit(target: torch.nn.Module, adapter_name: str, **kwargs):
+    def dispatch_bnb_4bit(target: torch.nn.Module, adapter_name: str, config: LoraConfig, **kwargs):
         new_module = None
 
         if isinstance(target, BaseTunerLayer):
@@ -606,6 +582,6 @@ if is_bnb_4bit_available():
                     "quant_type": target_base_layer.weight.quant_type,
                 }
             )
-            new_module = Linear4bit(target, adapter_name, **fourbit_kwargs)
+            new_module = Linear4bit(target, adapter_name, config=config, **fourbit_kwargs)
 
         return new_module

commit 376e2ac3e70ecc63f23f931521e7cd865b8f030b

Fix various test errors in the single GPU case
This addresses some of the errors reported by running the tests
on a single GPU machine.

I will list the error messages and a short explanation of the fix.

> `FAILED tests/test_common_gpu.py::PeftGPUCommonTests::test_lora_gptq_quantization_from_pretrained_safetensors - NameError: name 'BACKEND' is not defined`

The test was using GPTQModel without marking the test as requiring it leading to an error. This is fixed
by marking the test with `requires_gptqmodel`.

> `FAILED tests/test_custom_models.py::TestPeftCustomModel::test_only_params_are_updated[Embedding + transformers Conv1D 1 trainable_tokens-EmbConv1D-TrainableTokensConfig-config_kwargs180] - AssertionError: assert not True`
> `FAILED tests/test_custom_models.py::TestPeftCustomModel::test_disable_adapters_with_merging[Embedding + transformers Conv1D 1 trainable_tokens-EmbConv1D-TrainableTokensConfig-config_kwargs180] - AssertionError: assert not True`

This test fails because sometimes the gradients of the trainable tokens delta is 0 but only when training on CUDA,
CPU is fine.

This is a weird one and I'm not sure if this is a good fix or not. I encountered this error on two machines
(1xL40S and 4xA10G) and I was not able to pinpoint this to something particular in the environment, i.e.
PEFT version (tested v0.17 to main), transformers version (tested 4.5{5,6,7}, 5.0), CUDA version (tested 12.6, 12.8)
or torch version (tested 2.7, 2.8, 2.9, 2.10). I also set `LD_LIBRARY_PATH=` before running pytest to exclude
cuDNN libraries that come preinstalled on the EC2 instance.

Removing the ReLU in `EmbConv1DModel` as well as boosting the Conv1D weights will fix the error. Replacing
the ReLU with `Threshold(0, 0)` has the same behavior. It depends on the seed, i.e. if the initialization of
`Conv1D` is favorable the bug will not trigger.

I tried pinpointing it on `index_copy` but it is not `index_copy` by itself that is the problem. Maybe we will just
have to live with this?

> `FAILED tests/test_common_gpu.py::PeftGPUCommonTests::test_dora_ephemeral_gpu_offload_multigpu - RuntimeError: Expected all tensors to be on the same device, but got mat2 is on cpu, different from other tensors on cuda:0 (when checking argument in method wrapper_CUDA_mm)`

This is caused by a bug introduced in #2960 - `ephemeral_gpu_offload` is not passed to the variant and therefore

src/peft/tuners/lora/model.py
@@ -209,6 +209,7 @@ class LoraModel(BaseTuner):
             "target_name": current_key,
             "loaded_in_8bit": getattr(self.model, "is_loaded_in_8bit", False),
             "loaded_in_4bit": getattr(self.model, "is_loaded_in_4bit", False),
+            "ephemeral_gpu_offload": lora_config.runtime_config.ephemeral_gpu_offload,
             "parameter_name": parameter_name,
         }
 

tests/test_common_gpu.py
@@ -64,6 +64,7 @@ from .testing_utils import (
     load_cat_image,
     require_bitsandbytes,
     require_deterministic_for_xpu,
+    require_gptqmodel,
     require_non_cpu,
     require_torch_multi_accelerator,
 )
@@ -519,6 +520,7 @@ class PeftGPUCommonTests(unittest.TestCase):
             assert "default" in model.base_model.model.model.decoder.layers[0].self_attn.q_proj.ia3_l
             assert "adapter2" in model.base_model.model.model.decoder.layers[0].self_attn.q_proj.ia3_l
 
+    @require_gptqmodel
     @pytest.mark.single_gpu_tests
     def test_lora_gptq_quantization_from_pretrained_safetensors(self):
         r"""

tests/test_custom_models.py
@@ -1365,6 +1365,10 @@ class ModelEmbConv1D(nn.Module):
         super().__init__()
         self.emb = nn.Embedding(emb_size, 5)
         self.conv1d = Conv1D(1, 5)
+        # make sure that we have a good signal-to-noise ratio
+        # since apparently CUDA ReLU clips the gradient at a
+        # certain point.
+        self.conv1d.weight.data += 10
         self.relu = nn.ReLU()
         self.flat = nn.Flatten()
         self.lin0 = nn.Linear(10, 2)

tests/test_gpu_examples.py
@@ -592,7 +592,8 @@ class PeftBnbGPUExampleTests(unittest.TestCase):
                 device_map={"": 0},
             )
 
-            assert set(model.hf_device_map.values()) == {0}
+            # note: transformers v5 doesn't set the device map if there's only one device
+            assert not hasattr(model.hf_device_map) or set(model.hf_device_map.values()) == {0}
 
             tokenizer = AutoTokenizer.from_pretrained(self.seq2seq_model_id)
             model = prepare_model_for_kbit_training(model)