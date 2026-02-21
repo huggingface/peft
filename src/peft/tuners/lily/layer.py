648fda7f048d6f6fbad8104184f19b9bb796a2d2
yibozhong <yibozhong657@gmail.com>
2026-02-22 01:38:17 +0800

    update lily layer related changes

--- src/peft/tuners/lily/layer.py
+++ src/peft/tuners/lily/layer.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 import math
-from typing import Any
+from typing import Any, Optional
 
 import torch
 import torch.nn as nn
@@ -27,7 +27,7 @@ class LilyLayer(BaseTunerLayer):
     # All names of layers that may contain (trainable) adapter weights
     adapter_layer_names: tuple[str, ...] = ("lily_A", "lily_B", "lily_router")
     # All names of other parameters that may contain adapter-related parameters
-    other_param_names: tuple[str, ...] = ("r", "lily_scaling", "lily_dropout", "num_A", "num_B")
+    other_param_names: tuple[str, ...] = ("r", "scaling", "num_A", "num_B")
     def __init__(
         self,
         base_layer: nn.Module, 
@@ -35,8 +35,7 @@ class LilyLayer(BaseTunerLayer):
     ) -> None:
         self.base_layer = base_layer
         self.r = {}
-        self.lily_scaling = {}
-        self.lily_dropout = nn.ModuleDict({})
+        self.scaling = {}
         self.num_A = {}
         self.num_B = {}
         self.lily_A = nn.ModuleDict({})
@@ -46,43 +45,6 @@ class LilyLayer(BaseTunerLayer):
 
         base_layer = self.get_base_layer()
         if isinstance(base_layer, nn.Linear):
-            in_features, out_features = base_layer.in_features, base_layer.out_features
-        elif isinstance(base_layer, nn.Conv1d):
-            in_features, out_features = base_layer.in_channels, base_layer.out_channels
-        elif isinstance(base_layer, nn.Conv2d):
-            in_features, out_features = base_layer.in_channels, base_layer.out_channels
-        elif isinstance(base_layer, nn.Conv3d):
-            in_features, out_features = base_layer.in_channels, base_layer.out_channels
-        elif isinstance(base_layer, nn.Embedding):
-            in_features, out_features = base_layer.num_embeddings, base_layer.embedding_dim
-        elif isinstance(base_layer, Conv1D):
-            in_features, out_features = (
-                base_layer.weight.ds_shape if hasattr(base_layer.weight, "ds_shape") else base_layer.weight.shape
-            )
-        elif isinstance(base_layer, nn.MultiheadAttention):
-            if not base_layer._qkv_same_embed_dim:
-                raise ValueError(f"Only same dim for query/key/value is supported as of now for {self.__class__}.")
-            in_features, out_features = base_layer.embed_dim, 3 * base_layer.embed_dim
-        elif hasattr(base_layer, "infeatures") and hasattr(base_layer, "outfeatures"):
-            # QuantLinear
-            in_features, out_features = base_layer.infeatures, base_layer.outfeatures
-        elif hasattr(base_layer, "input_size") and hasattr(base_layer, "output_size"):
-            # Megatron ColumnParallelLinear,RowParallelLinear
-            in_features, out_features = base_layer.input_size, base_layer.output_size
-        elif hasattr(base_layer, "codebooks") and base_layer.__class__.__name__ == "QuantizedLinear":
-            # AQLM QuantLinear
-            in_features, out_features = base_layer.in_features, base_layer.out_features
-        elif hasattr(base_layer, "w_bit") and base_layer.__class__.__name__ == "WQLinear_GEMM":
-            # Awq layers
-            in_features, out_features = base_layer.in_features, base_layer.out_features
-        elif base_layer.__class__.__name__ == "EetqLinear":
-            # Eetq layers
-            in_features, out_features = base_layer.in_features, base_layer.out_features
-        elif hasattr(base_layer, "W_q") and base_layer.__class__.__name__ == "HQQLinear":
-            # HQQ layers
-            in_features, out_features = base_layer.in_features, base_layer.out_features
-        elif base_layer.__class__.__name__ == "PatchedLinear":
-            # INC layers
             in_features, out_features = base_layer.in_features, base_layer.out_features
         else:
             # possibly support user provided custom layer types using dynamic dispatch
@@ -101,8 +63,7 @@ class LilyLayer(BaseTunerLayer):
         self,
         adapter_name,
         r,
-        lily_scaling,
-        lily_dropout,
+        scaling,
         num_A,
         num_B,
         lily_A=None,
@@ -116,19 +77,14 @@ class LilyLayer(BaseTunerLayer):
             raise ValueError(f"`r` should be a positive integer value but the value passed is {r}")
         
         self.r[adapter_name] = r
-        self.lily_scaling[adapter_name] = lily_scaling
-        if lily_dropout > 0.0:
-            lily_dropout_layer = nn.Dropout(p=lily_dropout)
-        else:
-            lily_dropout_layer = nn.Identity()
+        self.scaling[adapter_name] = scaling
         
-        self.lily_dropout.update(nn.ModuleDict({adapter_name: lily_dropout_layer}))
         # Actual trainble parameters
         self.lily_A[adapter_name] = lily_A if lily_A is not None else nn.Linear(self.in_features, r, bias=False)
         self.lily_B[adapter_name] = lily_B if lily_B is not None else nn.Linear(r, self.out_features, bias=False)
         self.lily_router[adapter_name] = nn.Linear(r, num_B, bias=False)
-        self.num_A = num_A
-        self.num_B = num_B
+        self.num_A[adapter_name] = num_A  
+        self.num_B[adapter_name] = num_B  
         self.reset_lily_parameters(adapter_name) # initialize the parameters
         self.set_adapter(self.active_adapters)
 
@@ -146,8 +102,7 @@ class Linear(nn.Module, LilyLayer):
         base_layer,
         adapter_name: str,
         r: int = 0,
-        lily_scaling: float = 1.0,
-        lily_dropout: float = 0.0,
+        scaling: float = 1.0,
         num_A: int = 4,
         num_B: int = 4,
         lily_A: nn.Linear = None,
@@ -162,8 +117,7 @@ class Linear(nn.Module, LilyLayer):
         self.update_layer(
             adapter_name,
             r,
-            lily_scaling=lily_scaling,
-            lily_dropout=lily_dropout,
+            scaling=scaling,
             lily_A=lily_A,
             lily_B=lily_B,
             num_A=num_A,
@@ -178,39 +132,13 @@ class Linear(nn.Module, LilyLayer):
             adapter (str):
                 The name of the adapter for which the delta weight should be computed.
         """
-        device = self.lily_B[adapter].weight.device
-        dtype = self.lily_B[adapter].weight.dtype
+        raise NotImplementedError("This method is not supported for Lily.")
+    
+    def merge(self, safe_merge: bool = False, adapter_names: Optional[list[str]] = None) -> None:
+        raise NotImplementedError("This method is not supported for Lily.")
 
-        # In case users wants to merge the adapter weights that are in
-        # float16 while being on CPU, we need to cast the weights to float32, perform the merge and then cast back to
-        # float16 because the `@` and matmul operation in general is not supported in torch + cpu + fp16.
-        cast_to_fp32 = device.type == "cpu" and (dtype == torch.float16 or dtype == torch.bfloat16)
-
-        weight_A = self.lily_A[adapter].weight
-        weight_B = self.lily_B[adapter].weight
-        router = self.lily_router[adapter].weight
-
-        if cast_to_fp32:
-            weight_A = weight_A.float()
-            weight_B = weight_B.float()
-        
-        # the delta weight is dynamically computed based on the input tensor
-        hidden = self.lily_A(x)
-        router_logits = torch.matmul(hidden, router.t())
-        router_probability = F.softmax(router_logits, dim=-1) 
-        expert_probabilities = router_probability.mean(dim=(0, 1)) 
-        weight_B = einops.rearrange(weight_B, "(e i) o -> e i o", e=self.num_B)
-        combined_B = torch.einsum("e,eio->io", expert_probabilities, weight_B)
-        output_tensor = torch.matmul(weight_A.t(), combined_B) * self.lily_scaling # get the approximated weight update
-
-        if cast_to_fp32:
-            output_tensor = output_tensor.to(dtype=dtype)
-
-            # cast back the weights
-            self.lily_A[adapter].weight.data = weight_A.to(dtype)
-            self.lily_B[adapter].weight.data = weight_B.to(dtype)
-
-        return output_tensor
+    def unmerge(self) -> None:
+        raise NotImplementedError("This method is not supported for Lily.")
 
     def forward(self, x: torch.Tensor, *args: Any, **kwargs: Any) -> torch.Tensor:
         result = self.base_layer(x, *args, **kwargs)
@@ -226,8 +154,7 @@ class Linear(nn.Module, LilyLayer):
             lily_B = self.lily_B[active_adapter]
             router = self.lily_router[active_adapter]
             B = einops.rearrange(lily_B.weight, "(e i) o -> e i o", e=self.num_B)
-            dropout = self.lily_dropout
-            scaling = self.lily_scaling[active_adapter]
+            scaling = self.scaling[active_adapter]
             x = x.to(lily_A.weight.dtype)
             hidden = lily_A(x)
             router_logits = router(hidden) # [B, N, num_of_experts]
