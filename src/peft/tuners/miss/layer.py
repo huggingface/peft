8f7171b28ef58addd4e088eb7cecd10de474167f
Joluck <997529190@qq.com>
2026-04-30 14:38:44 +0800

    make style

--- src/peft/tuners/miss/layer.py
+++ src/peft/tuners/miss/layer.py
@@ -18,8 +18,8 @@ import warnings
 from typing import Any, Optional
 
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
+from torch import nn
 
 from peft.tuners.tuners_utils import BaseTunerLayer, check_adapters_to_merge
 
