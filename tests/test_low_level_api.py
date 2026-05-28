c503cfcd592a4109f2de867299b51000b3bdd2ad
Taksh <takshkothari09@gmail.com>
2026-05-28 11:06:02 +0530

    Address review feedback: check keys instead of values, and cover embedding layer

--- tests/test_low_level_api.py
+++ tests/test_low_level_api.py
@@ -520,20 +520,18 @@ class TestPeftStateDict:
             assert ("v_proj" in key) or ("q_proj" in key) or ("lm_head") in key
 
     def test_get_peft_model_state_dict_matches_adapter_name_exactly(self):
-        model = get_peft_model(DummyModel(), LoraConfig(target_modules=["linear"]))
-        model.add_adapter("default_extended", LoraConfig(target_modules=["linear"]))
-
-        for module in model.modules():
-            if hasattr(module, "lora_A"):
-                module.lora_A["default"].weight.data.fill_(1.0)
-                module.lora_B["default"].weight.data.fill_(1.0)
-                module.lora_A["default_extended"].weight.data.fill_(2.0)
-                module.lora_B["default_extended"].weight.data.fill_(2.0)
+        model = get_peft_model(DummyModel(), LoraConfig(target_modules=["linear", "embedding"]))
+        model.add_adapter("default_extended", LoraConfig(target_modules=["linear", "embedding"]))
 
         sd = get_peft_model_state_dict(model, adapter_name="default")
-        assert all(
-            torch.allclose(value, torch.ones_like(value)) for key, value in sd.items() if "lora_" in key
-        )
+        
+        lora_keys = [k for k in sd.keys() if "lora_" in k]
+        assert len(lora_keys) > 0
+        
+        for key in lora_keys:
+            # "default" is stripped by get_peft_model_state_dict.
+            # "default_extended" should not be picked up by the state dict filter.
+            assert "default_extended" not in key
 
     def check_peft_model_weights_loaded_correctly(self, inner_model_cls, config, nested, adapter_name="default"):
         # Runs checks that a roundtrip of get_peft_model_state_dict and set_peft_model_state_dict results in the same
