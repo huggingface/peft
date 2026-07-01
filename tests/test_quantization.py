a7c19166e3c753f2c095b9576ac850e93fdb4ec6
Benjamin Bossan <benjamin.bossan@gmail.com>
2026-07-01 18:19:23 +0200

    FIX MiSS with bat init error with some quantization backends
    When using MiSS with the init_weights='bat', the forward pass requires
    getting the non-quantized base weights. When the quantization backend
    does not support dequantization, we now detect this and raise a
    helpful error message. Tests have been updated to check for this error
    message.
    
    Normally, we should have caught this error. The reason why we didn't
    is that we have a helper function for testing that changes PEFT config
    kwargs to be non-zero initialization. When passing init_weights='bat'
    to that function, we would override 'bat' with False. Thus, the 'bat'
    path was not actually being tested. The helper function has now been
    updated not to override 'bat'.

--- tests/test_quantization.py
+++ tests/test_quantization.py
@@ -250,6 +250,9 @@ class TestQuantization:
     @pytest.mark.parametrize("config_cls,config_kwargs", TEST_CASES, ids=_peft_id)
     def test_forward_changes_output(self, config_cls, config_kwargs, quant, dummy_input):
         """Check that the forward pass works, also check if the results are affected"""
+        if (config_cls == MissConfig) and (config_kwargs.get("init_weights") == "bat"):
+            pytest.skip(reason="Test requires non-zero init but MiSS is using 'bat' init")
+
         config_kwargs = set_init_weights_false(config_cls, config_kwargs)
         model = quant.load_model()
 
@@ -258,6 +261,11 @@ class TestQuantization:
 
         config = config_cls(**config_kwargs)
         model = get_peft_model(model, config)
+
+        if (config_cls == MissConfig) and (config.init_weights == "bat") and (not quant.supports_merge):
+            with pytest.raises(ValueError, match="is not supported for quantization with"), torch.inference_mode():
+                model(dummy_input).logits
+            return
 
         with torch.inference_mode():
             out_peft = model(dummy_input).logits
@@ -283,6 +291,11 @@ class TestQuantization:
         config = config_cls(**config_kwargs)
         torch.manual_seed(SEED)
         model = get_peft_model(model, config).eval()
+
+        if (config_cls == MissConfig) and (config.init_weights == "bat") and (not quant.supports_merge):
+            with pytest.raises(ValueError, match="is not supported for quantization with"), torch.inference_mode():
+                model(dummy_input).logits
+            return
 
         with torch.inference_mode():
             out_quant = model(dummy_input).logits
