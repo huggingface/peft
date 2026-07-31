a5e689189fd2dd79f67ad2adee7f2e9e6328c82f
Benjamin Bossan <benjamin.bossan@gmail.com>
2026-07-31 19:27:44 +0200

    Fix VeRA and PVeRA too

--- tests/test_unilora.py
+++ tests/test_unilora.py
@@ -58,9 +58,7 @@ def _make_unilora_config(**kwargs):
 def _get_unilora_index_state(model):
     # note: the indices are non-persistent buffers unless save_indices is set, so they need to be collected from the
     # modules instead of from the state_dict
-    return {
-        name: buffer.detach().cpu().clone() for name, buffer in model.named_buffers() if "unilora_indices" in name
-    }
+    return {name: buffer.detach().cpu().clone() for name, buffer in model.named_buffers() if "unilora_indices" in name}
 
 
 class TestUniLora:
